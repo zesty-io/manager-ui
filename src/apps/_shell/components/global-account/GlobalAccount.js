741062ad3196965b593962c5bad2854e734acdd9
stuart runyan <shrunyan@gmail.com>
2017-06-16 18:41:12 -0700

    new component structure

---
+++ src/apps/_shell/components/global-account/GlobalAccount.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import cx from 'classnames'
+import css from './GlobalAccount.less'
+
+import {toggleAccountsMenu} from '../../store/accounts-menu'
+import {subMenuLoad} from '../../store/global-sub-menu'
+
+export default class GlobalAccount extends Component {
+  constructor(props) {
+    super(props)
+    this.showAccountsMenu = this.showAccountsMenu.bind(this)
+    this.hideAccountsMenu = this.hideAccountsMenu.bind(this)
+  }
+  render() {
+    return (
+      <div
+        className={css.GlobalAccount}
+        onMouseEnter={this.showAccountsMenu}
+        onMouseLeave={this.hideAccountsMenu}>
+        <i className="fa fa-globe" aria-hidden="true"></i>
+      </div>
+    )
+  }
+  showAccountsMenu() {
+    this.props.dispatch(subMenuLoad(''))
+    this.props.dispatch(toggleAccountsMenu(true))
+  }
+  hideAccountsMenu() {
+    this.props.dispatch(toggleAccountsMenu(false))
+  }
+}
