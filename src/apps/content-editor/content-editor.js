8e4c171ae7d0dcdcb49e09afedb92ec2e0e96ddb
stuart runyan <shrunyan@gmail.com>
2017-08-07 15:48:39 -0700

    WIP: grid based layout

---
+++ src/apps/content-editor/content-editor.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {Switch, Redirect, Route} from 'react-router-dom'
+import styles from './content-editor.less'
+
+import ContentEditorMenu from './content-editor-menu'
+import ContentEditorView from './content-editor-view'
+import ContentEditorActions from './content-editor-actions'
+
+class ContentEditor extends Component {
+  constructor(props) {
+    super(props)
+    console.log('ContentEditor:constructor', this)
+  }
+  componentWillMount() {
+    console.log('ContentEditor:componentWillMount', this)
+  }
+  render() {
+    return (
+      <section className={styles.ContentEditor}>
+        <ContentEditorMenu sets={this.props.sets} />
+        <ContentEditorView />
+        <ContentEditorActions />
+      </section>
+    )
+  }
+}
+
+export default connect(state => state)(ContentEditor)
