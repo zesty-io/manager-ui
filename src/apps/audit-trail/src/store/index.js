ef322a0360b0304c025dd94ea91e4b2d677b09ed
Stuart Runyan <shrunyan@gmail.com>
2019-05-22 16:36:56 -0700

    comment out unused store

--- src/apps/audit-trail/src/store/index.js
+++ src/apps/audit-trail/src/store/index.js
@@ -1,29 +1,29 @@
-import thunkMiddleware from 'redux-thunk'
-import {createLogger} from 'redux-logger'
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+// import thunkMiddleware from 'redux-thunk'
+// import {createLogger} from 'redux-logger'
+// import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 
-import {logs} from './logs'
-import {loadingLogs} from './loadingLogs'
-import {inViewLogs} from './inViewLogs'
-import {settings} from './settings'
+// import {logs} from './logs'
+// import {loadingLogs} from './loadingLogs'
+// import {inViewLogs} from './inViewLogs'
+// import {settings} from './settings'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const loggerMiddleware = createLogger({
-    collapsed: true,
-    diff: true
-})
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const loggerMiddleware = createLogger({
+//     collapsed: true,
+//     diff: true
+// })
 
-export const store = createStore(
-  combineReducers({
-    logs,
-    loadingLogs,
-    inViewLogs,
-    settings
-  }),
-  composeEnhancers(
-    applyMiddleware(
-      thunkMiddleware, // lets us dispatch() functions
-      loggerMiddleware // neat middleware that logs actions
-    )
-  )
-)
+// export const store = createStore(
+//   combineReducers({
+//     logs,
+//     loadingLogs,
+//     inViewLogs,
+//     settings
+//   }),
+//   composeEnhancers(
+//     applyMiddleware(
+//       thunkMiddleware, // lets us dispatch() functions
+//       loggerMiddleware // neat middleware that logs actions
+//     )
+//   )
+// )
