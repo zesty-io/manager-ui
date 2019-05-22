82570549e921c72edc24538d575bee7c9a7430cd
Stuart Runyan <shrunyan@gmail.com>
2019-05-22 16:46:44 -0700

    removed: social app nav link

--- src/shell/components/global-menu/index.js
+++ src/shell/components/global-menu/index.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import styles from './styles.less'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles.less";
 
-import {subMenuLoad, subMenuTimer} from 'shell/store/ui/global-sub-menu'
+import { subMenuLoad, subMenuTimer } from "shell/store/ui/global-sub-menu";
 
 export default class GlobalMenu extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.hideMenu = this.hideMenu.bind(this)
-    this.showMenu = this.showMenu.bind(this)
+    this.hideMenu = this.hideMenu.bind(this);
+    this.showMenu = this.showMenu.bind(this);
 
     // this.state = {
     //   reveal: undefined
     // }
   }
   componentWillMount() {
-    console.log('GlobalMenu:componentWillMount', this)
+    console.log("GlobalMenu:componentWillMount", this);
   }
   render() {
     return (
-      <menu className={styles.GlobalMenu} >
+      <menu className={styles.GlobalMenu}>
         {/*this.props.products.map(product => {
           return <Link
                   className={styles.control}
@@ -39,8 +39,9 @@ export default class GlobalMenu extends Component {
           to="/content"
           title="Content Editor"
           onClick={this.hideMenu}
-          onMouseEnter={this.showMenu}>
-          <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-pencil-square-o" aria-hidden="true" />
           <span className={styles.title}>Content</span>
         </Link>
         <Link
@@ -48,30 +49,38 @@ export default class GlobalMenu extends Component {
           to="/media"
           title="Media Manager"
           onClick={this.hideMenu}
-          onMouseEnter={this.showMenu}>
-          <i className="fa fa-picture-o" aria-hidden="true"></i>
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-picture-o" aria-hidden="true" />
           <span className={styles.title}>Media</span>
         </Link>
-        <Link
+        {/* <Link
           className={styles.control}
           to="/forms"
           onClick={this.hideMenu}
-          onMouseEnter={this.showMenu}>
-          <i className="fa fa-envelope-o" aria-hidden="true"></i>
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-envelope-o" aria-hidden="true" />
           <span className={styles.title}>Forms</span>
-        </Link>
-        <Link className={styles.control} to="/leads" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-address-card-o" aria-hidden="true"></i>
+        </Link> */}
+        <Link
+          className={styles.control}
+          to="/leads"
+          onClick={this.hideMenu}
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-address-card-o" aria-hidden="true" />
           <span className={styles.title}>Leads</span>
         </Link>
-        <Link className={styles.control} to="/social" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-share-alt" aria-hidden="true"></i>
-          <span className={styles.title}>Social</span>
-        </Link>
 
-
-        <Link className={styles.build} to="/schema" title="Schema Editor" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-puzzle-piece" aria-hidden="true"></i>
+        <Link
+          className={styles.build}
+          to="/schema"
+          title="Schema Editor"
+          onClick={this.hideMenu}
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-puzzle-piece" aria-hidden="true" />
           <span className={styles.title}>Schema</span>
         </Link>
         <Link
@@ -79,39 +88,59 @@ export default class GlobalMenu extends Component {
           to="/code"
           title="Code Editor"
           onClick={this.hideMenu}
-          onMouseEnter={this.showMenu}>
-          <i className="fa fa-code-fork" aria-hidden="true"></i>
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-code-fork" aria-hidden="true" />
           <span className={styles.title}>Code</span>
         </Link>
 
-
-        <Link className={styles.optimize} to="/analytics" title="Analytics" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-pie-chart" aria-hidden="true"></i>
+        <Link
+          className={styles.optimize}
+          to="/analytics"
+          title="Analytics"
+          onClick={this.hideMenu}
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-pie-chart" aria-hidden="true" />
           <span className={styles.title}>Analytics</span>
         </Link>
-        <Link className={styles.optimize} to="/seo" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-line-chart" aria-hidden="true"></i>
+        <Link
+          className={styles.optimize}
+          to="/seo"
+          onClick={this.hideMenu}
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-line-chart" aria-hidden="true" />
           <span className={styles.title}>SEO</span>
         </Link>
-        <Link className={styles.optimize} to="/audit-trail" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-check-square-o" aria-hidden="true"></i>
+        <Link
+          className={styles.optimize}
+          to="/audit-trail"
+          onClick={this.hideMenu}
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-check-square-o" aria-hidden="true" />
           <span className={styles.title}>AuditTrail</span>
         </Link>
 
-        <Link className={styles.settings} to="/settings" onClick={this.hideMenu} onMouseEnter={this.showMenu}>
-          <i className="fa fa-cog" aria-hidden="true"></i>
+        <Link
+          className={styles.settings}
+          to="/settings"
+          onClick={this.hideMenu}
+          onMouseEnter={this.showMenu}
+        >
+          <i className="fa fa-cog" aria-hidden="true" />
           <span className={styles.title}>Settings</span>
         </Link>
-
       </menu>
-    )
+    );
   }
   hideMenu() {
     // if (this.state.reveal) {
     //   clearTimeout(this.state.reveal)
     // }
 
-    this.props.dispatch(subMenuLoad(''))
+    this.props.dispatch(subMenuLoad(""));
   }
   showMenu(evt) {
     // if (this.state.reveal) {
@@ -119,52 +148,51 @@ export default class GlobalMenu extends Component {
     // }
 
     if (evt.target.href) {
-        const currentHref = window.location.pathname
-        const hoveredHref = evt.target.href.split('/')[3]
+      const currentHref = window.location.pathname;
+      const hoveredHref = evt.target.href.split("/")[3];
 
-        if (currentHref.indexOf(hoveredHref) === -1) {
-          const timeout = setTimeout(() => {
-            console.log('setTimeout')
-            switch(hoveredHref) {
-              case 'content':
-                this.props.dispatch(subMenuLoad('content'))
-                break;
-              case 'media':
-                this.props.dispatch(subMenuLoad('media'))
-                break;
-              case 'code':
-                this.props.dispatch(subMenuLoad('code'))
-                break;
-            }
-          }, 500)
+      if (currentHref.indexOf(hoveredHref) === -1) {
+        const timeout = setTimeout(() => {
+          console.log("setTimeout");
+          switch (hoveredHref) {
+            case "content":
+              this.props.dispatch(subMenuLoad("content"));
+              break;
+            case "media":
+              this.props.dispatch(subMenuLoad("media"));
+              break;
+            case "code":
+              this.props.dispatch(subMenuLoad("code"));
+              break;
+          }
+        }, 500);
 
-          this.props.dispatch(subMenuTimer(timeout))
+        this.props.dispatch(subMenuTimer(timeout));
 
+        // const reveal = setTimeout(() => {
+        //   switch(hoveredHref) {
+        //     case 'content':
+        //       this.props.dispatch(subMenuLoad('content'))
+        //       break;
+        //     case 'media':
+        //       this.props.dispatch(subMenuLoad('media'))
+        //       break;
+        //     case 'code':
+        //       this.props.dispatch(subMenuLoad('code'))
+        //       break;
+        //     // case 'analytics':
+        //     //   this.props.dispatch(subMenuLoad('analytics'))
+        //     // case 'seo':
+        //     //   this.props.dispatch(subMenuLoad('seo'))
+        //     // case 'social':
+        //     //   this.props.dispatch(subMenuLoad('social'))
+        //     // case 'leads':
+        //     //   this.props.dispatch(subMenuLoad('leads'))
+        //   }
+        // }, 500)
 
-          // const reveal = setTimeout(() => {
-          //   switch(hoveredHref) {
-          //     case 'content':
-          //       this.props.dispatch(subMenuLoad('content'))
-          //       break;
-          //     case 'media':
-          //       this.props.dispatch(subMenuLoad('media'))
-          //       break;
-          //     case 'code':
-          //       this.props.dispatch(subMenuLoad('code'))
-          //       break;
-          //     // case 'analytics':
-          //     //   this.props.dispatch(subMenuLoad('analytics'))
-          //     // case 'seo':
-          //     //   this.props.dispatch(subMenuLoad('seo'))
-          //     // case 'social':
-          //     //   this.props.dispatch(subMenuLoad('social'))
-          //     // case 'leads':
-          //     //   this.props.dispatch(subMenuLoad('leads'))
-          //   }
-          // }, 500)
-
-          // this.setState({reveal})
-        }
+        // this.setState({reveal})
       }
+    }
   }
 }
