07f0c4c0a5d965c55cf49d5c0b536452ab7e2542
Stuart Runyan <shrunyan@gmail.com>
2019-05-22 18:06:08 -0700

    updated global action icons

--- src/shell/components/global-actions/index.js
+++ src/shell/components/global-actions/index.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react'
-import cx from 'classnames'
-import styles from './styles.less'
+import React, { Component } from "react";
+import cx from "classnames";
+import styles from "./styles.less";
 
 export default class GlobalActions extends Component {
   componentWillMount() {
-    console.log('GlobalActions:componentWillMount')
+    console.log("GlobalActions:componentWillMount");
   }
   render() {
     return (
       <div className={styles.GlobalActions}>
-        {/*<span className={styles.action}>
-                  <i className={cx(styles.preview, "fa fa-life-ring")} aria-hidden="true" title="Visual Sitemap"></i>
-                </span>*/}
         <span className={styles.action}>
-          <i className={cx(styles.preview, "fa fa-map")} aria-hidden="true" title="Visual Sitemap"></i>
+          <i
+            className={cx(styles.preview, "fa fa-map")}
+            aria-hidden="true"
+            title="Visual Sitemap"
+          />
         </span>
         <span className={styles.action}>
-          <i className={cx(styles.preview, "fa fa-clone")} aria-hidden="true" title="Live Preview"></i>
+          <i
+            className={cx(styles.preview, "fa fa-eye")}
+            aria-hidden="true"
+            title="Live Preview"
+          />
         </span>
-        <span className={styles.action}>
-          <i className={cx(styles.chat, "fa fa-comments-o")} aria-hidden="true" title="Chat">
+        {/* <span className={styles.action}>
+          <i
+            className={cx(styles.chat, "fa fa-comments-o")}
+            aria-hidden="true"
+            title="Chat"
+          >
             <span className={styles.notificationCount}>3</span>
           </i>
-        </span>
-
-
-
-
-
-        {/*<button title="Live Preview">
-                  <i className="fa fa-clone" aria-hidden="true"></i>
-                </button>*/}
+        </span> */}
       </div>
-    )
+    );
   }
 }
