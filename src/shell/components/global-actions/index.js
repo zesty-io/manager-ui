b21d184b9c2c841ac687a6fe0341a41e65a5a6fc
Stuart Runyan <shrunyan@gmail.com>
2020-01-29 12:23:46 -0800

    fixed: global sub menu icon alignment

--- src/shell/components/global-actions/index.js
+++ src/shell/components/global-actions/index.js
@@ -1,23 +1,19 @@
 import React from "react";
 import cx from "classnames";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faMap,
   faBell,
   faComment,
   faQuestion,
   faEye
 } from "@fortawesome/free-solid-svg-icons";
-import { ButtonGroup } from "@zesty-io/core/ButtonGroup";
 
 import styles from "./styles.less";
 export default React.memo(function GlobalActions() {
-  // TODO load version number from package.json on build
-  const version = "0.0.0";
-
   return (
-    <div className={styles.GlobalActions}>
-      <ButtonGroup>
+    <div className={styles.GlobalSubMenu}>
+      <div className={styles.GlobalActions}>
         <span className={styles.action} title="Live preview">
           <FontAwesomeIcon icon={faEye} />
         </span>
@@ -30,7 +26,7 @@ export default React.memo(function GlobalActions() {
         <span className={styles.action} title="Help">
           <FontAwesomeIcon icon={faQuestion} />
         </span>
-      </ButtonGroup>
+      </div>
 
       <span className={cx(styles.AppVersion)}>
         <img
@@ -41,21 +37,6 @@ export default React.memo(function GlobalActions() {
         />
         <span className={styles.VersionNumber}>{CONFIG.VERSION}</span>
       </span>
-      {/* <span className={styles.action}>
-        <FontAwesomeIcon icon={faMap} title="Visual Sitemap" />
-      </span>
-      <span className={styles.action}>
-        <FontAwesomeIcon icon={faEye} title="Live Preview" />
-      </span> */}
-      {/* <span className={styles.action}>
-        <i
-          className={cx(styles.chat, "fa fa-comments-o")}
-          aria-hidden="true"
-          title="Chat"
-        >
-          <span className={styles.notificationCount}>3</span>
-        </i>
-      </span> */}
     </div>
   );
 });
