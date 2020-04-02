f59f339cf61119555984af487886edc8f5d3cb4f
Hector Sanchez <hola@hectorsanchez.mx>
2020-04-02 14:26:22 -0600

    adds instance head tags to Preview

--- src/apps/settings/src/app/views/Head/Head.js
+++ src/apps/settings/src/app/views/Head/Head.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-// import { notify } from "../../../../../shell/store/notifications";
 import { addHeadTag } from "../../../store/headTags";
 
 import { Button } from "@zesty-io/core/Button";
 import { ButtonGroup } from "@zesty-io/core/ButtonGroup";
 import { Card, CardHeader, CardContent } from "@zesty-io/core/Card";
-
-// import { SubHeader } from "../components/SubHeader";
 
 import { HeadTag } from "./HeadTag";
 import { Preview } from "./Preview";
@@ -22,7 +19,6 @@ export default connect(state => {
     item: state.instance
   };
 })(function Head(props) {
-  console.log(Object.values(props.tags));
   function onAdd() {
     const idTag = Object.values(props.tags).length;
     props.dispatch(
@@ -48,78 +44,61 @@ export default connect(state => {
         sort: Object.values(props.tags).length || 0
       })
     );
-
-    // props
-    //   .dispatch(
-    //     createHeadTag({
-    //       type: "meta",
-    //       resourceZUID: props.itemZUID,
-    //       custom: true,
-    //       attributes: {
-    //         name: "",
-    //         content: ""
-    //       },
-    //       sort: props.tags.length || 0
-    //     })
-    //   )
-    //   .then(res => {
-    //     props.dispatch(
-    //       notify({
-    //         message: res.data.error ? res.data.error : "New head tag created",
-    //         kind: res.data.error ? "warn" : "success"
-    //       })
-    //     );
-    //   });
   }
 
+  const filteredTags = Object.values(props.tags).filter(item => {
+    if (item.attributes.some(el => el.key === "custom")) {
+      return item;
+    }
+  });
+
+  const sortedTags = filteredTags.sort(
+    (a, b) =>
+      Object.values(props.tags).indexOf(b) -
+      Object.values(props.tags).indexOf(a)
+  );
+
+  const instanceTags = Object.values(props.tags).filter(item => {
+    if (
+      !item.attributes.some(el => el.key === "custom") &&
+      item.resourceZUID === window.zesty.site.zuid
+    ) {
+      return item;
+    }
+  });
+
   return (
-    <>
-      <div className={styles.HeadWrap}>
-        <div className={styles.Head}>
-          <main className={styles.Tags}>
-            <h1 className={styles.Warn}>
-              <Button kind="secondary" onClick={onAdd}>
-                <i className="fa fa-plus" />
-                New head tag
-              </Button>
-              <i className="fa fa-exclamation-triangle" aria-hidden="true" />
-              &nbsp; Head tags are not versioned or published. Changes to head
-              tags take effect immediately on your live instance.
-            </h1>
-            {Object.values(props.tags).filter(item => {
-              if (item.attributes.some(el => el.key === "custom")) {
-                return item;
-              }
-            }).length ? (
-              Object.values(props.tags)
-                .filter(item => {
-                  if (item.attributes.some(el => el.key === "custom")) {
-                    return item;
-                  }
-                })
-                .sort(
-                  (a, b) =>
-                    Object.values(props.tags).indexOf(b) -
-                    Object.values(props.tags).indexOf(a)
-                )
-                .map((tag, index) => {
-                  return (
-                    <HeadTag key={index} tag={tag} dispatch={props.dispatch} />
-                  );
-                })
-            ) : (
-              <h3 className={styles.NoTags}>
-                No head tags have been created for this item.
-              </h3>
-            )}
-          </main>
-          <Preview
-            instanceName={props.item.name}
-            item={props.item}
-            tags={Object.values(props.tags)}
-          />
-        </div>
+    <div className={styles.HeadWrap}>
+      <div className={styles.Head}>
+        <main className={styles.Tags}>
+          <h1 className={styles.Warn}>
+            <Button kind="secondary" onClick={onAdd}>
+              <i className="fa fa-plus" />
+              New head tag
+            </Button>
+            <i className="fa fa-exclamation-triangle" aria-hidden="true" />
+            &nbsp; Head tags are not versioned or published. Changes to head
+            tags take effect immediately on your live instance.
+          </h1>
+          {filteredTags.length ? (
+            sortedTags.map((tag, index) => {
+              return (
+                <HeadTag key={index} tag={tag} dispatch={props.dispatch} />
+              );
+            })
+          ) : (
+            <h3 className={styles.NoTags}>
+              No head tags have been created for this item.
+            </h3>
+          )}
+        </main>
+        <Preview
+          instanceName={props.item.name}
+          item={props.item}
+          instanceTags={instanceTags}
+          tags={sortedTags}
+        />
       </div>
-    </>
+    </div>
   );
 });
--- src/apps/settings/src/app/views/Head/Preview/Preview.js
+++ src/apps/settings/src/app/views/Head/Preview/Preview.js
@@ -10,19 +10,30 @@ export const Preview = props => (
         {props.item &&
           props.item.web &&
           `  <!-- Auto-generated Head Tags -->
-  <title>${props.item.web.metaTitle}</title>
-  <meta name="description" content="${props.item.web.metaDescription}" />
-  <meta  http-equiv="Content-Type" content="text/html;charset=utf-8">
-  <link rel="canonical" href="${props.domain}${props.item.web.path}" />
-  <meta property="og:type" content="website" />
-  <meta name="twitter:card" content="summary">
-  <meta property="og:title" content="${props.item.web.metaTitle}" />
-  <meta name="twitter:title" content="${props.item.web.metaTitle}">
-  <meta property="og:description" content="${props.item.web.metaDescription}" />
-  <meta property="twitter:description" content="${props.item.web.metaDescription}" />
-  <meta property="og:url" content="${props.domain}${props.item.web.path}" />
-  <meta property="og:site_name" content="${props.instanceName}" />\n
-`}
+          <title>${props.item.web.metaTitle}</title>
+          <meta name="description" content="${props.item.web.metaDescription}" />
+          <meta  http-equiv="Content-Type" content="text/html;charset=utf-8">
+          <link rel="canonical" href="${props.domain}${props.item.web.path}" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary">
+          <meta property="og:title" content="${props.item.web.metaTitle}" />
+          <meta name="twitter:title" content="${props.item.web.metaTitle}">
+          <meta property="og:description" content="${props.item.web.metaDescription}" />
+          <meta property="twitter:description" content="${props.item.web.metaDescription}" />
+          <meta property="og:url" content="${props.domain}${props.item.web.path}" />
+          <meta property="og:site_name" content="${props.instanceName}" />\n
+        `}
+        <p>
+          {props.instanceTags &&
+            props.instanceTags
+              .map(
+                tag =>
+                  `  <${tag.type} ${tag.attributes
+                    .map(attr => `${attr.key}="${attr.value}"`)
+                    .join(" ")} />`
+              )
+              .join("\n")}
+        </p>
       </p>
       {`  <!-- Custom Item Head Tags -->\n`}
       {props.tags
