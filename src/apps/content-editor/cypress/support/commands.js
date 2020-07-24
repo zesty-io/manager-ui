ec6388a89aa5fb2ef349c9b3a4c971127ef995fe
Stuart Runyan <shrunyan@gmail.com>
2020-07-24 09:37:12 -0700

    fixed: make cookie available to test environment

--- src/apps/content-editor/cypress/support/commands.js
+++ src/apps/content-editor/cypress/support/commands.js
@@ -1,16 +1,27 @@
+// TODO we may need this as Cypress seems to indicate cookies are not
+// preserved across tests.
+// @see https://docs.cypress.io/api/cypress-api/cookies.html#Set-global-default-cookies
+// Cypress.Cookies.defaults({
+//   whitelist: Cypress.env("COOKIE_NAME")
+// });
+
 Cypress.Commands.add("login", (eml, pass) => {
   const formBody = new FormData();
 
   formBody.append("email", eml || Cypress.env("validEmail"));
   formBody.append("password", pass || Cypress.env("validPassword"));
 
-  return fetch(`${Cypress.env("API_AUTH")}/login`, {
+  fetch(`${Cypress.env("API_AUTH")}/login`, {
     method: "POST",
     credentials: "include",
     body: formBody
   })
     .then(res => res.json())
-    .then(json => json.data.data);
+    .then(json => {
+      // We need the cookie value returned reset so it is unsecure and
+      // accessible by javascript
+      cy.setCookie(Cypress.env("COOKIE_NAME"), json.meta.token);
+    });
 });
 
 Cypress.Commands.add("goHome", () => {
