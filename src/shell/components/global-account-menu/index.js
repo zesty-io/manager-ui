fed320c27cadb2bbb49b86e565a89a2e1945313f
Stuart Runyan <shrunyan@gmail.com>
2020-09-17 14:49:27 -0700

    added: styling to global account menu

--- src/shell/components/global-account-menu/index.js
+++ src/shell/components/global-account-menu/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import cx from "classnames";
 import { connect } from "react-redux";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faExternalLinkAlt,
+  faSignOutAlt
+} from "@fortawesome/free-solid-svg-icons";
+
 import { Select, Option } from "@zesty-io/core/Select";
+import { Button } from "@zesty-io/core/Button";
+import { Url } from "@zesty-io/core/Url";
 
 import styles from "./styles.less";
-
 export default connect(state => {
   return {
     ui: state.ui,
@@ -23,7 +30,16 @@ export default connect(state => {
     >
       <header className={styles.user}>
         <h1>
-          {props.user.firstName} {props.user.lastName}
+          <img
+            alt={`${props.user.firstName} ${props.user.lastName} Avatar`}
+            src={`https://www.gravatar.com/avatar/${props.user.emailHash}?d=mm&s=40`}
+          />
+          <div className={styles.meta}>
+            <span className={styles.name}>
+              {props.user.firstName} {props.user.lastName}
+            </span>
+            <span className={styles.subheadline}>{props.user.email}</span>
+          </div>
         </h1>
       </header>
 
@@ -42,14 +58,33 @@ export default connect(state => {
         </Select>
       </main>
 
-      <menu className={styles.accountActions}>
-        <a
-          href={`${CONFIG.URL_ACCOUNTS}/logout`}
-          className={cx(styles.link, styles.logout)}
-        >
+      <main className={styles.Domains}>
+        {props.instance.screenshotURL && (
+          <img src={props.instance.screenshotURL} width="275px" />
+        )}
+
+        <ul className={styles.bodyText}>
+          {props.instance.domains.map(domain => (
+            <li key={domain.domain}>
+              <Url href={`http://${domain.domain}`}>
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+                &nbsp;
+                {domain.domain}
+              </Url>
+            </li>
+          ))}
+        </ul>
+      </main>
+
+      <Url
+        href={`${CONFIG.URL_ACCOUNTS}/logout`}
+        className={cx(styles.link, styles.logout)}
+      >
+        <Button kind="alt">
+          <FontAwesomeIcon icon={faSignOutAlt} />
           Logout
-        </a>
-      </menu>
+        </Button>
+      </Url>
     </section>
   );
 });
