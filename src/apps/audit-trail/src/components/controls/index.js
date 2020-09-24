780db780370ac56cb975ea465327501c44ecfa05
Joshua Hanson <joshua.hanson@zesty.io>
2020-09-24 11:53:26 -0500

    Audit Trail: filter by date and search term

--- src/apps/audit-trail/src/components/controls/index.js
+++ src/apps/audit-trail/src/components/controls/index.js
@@ -1,45 +1,60 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import cx from "classnames";
 
 import { Search } from "@zesty-io/core/Search";
 import { Button } from "@zesty-io/core/Button";
 import { ButtonGroup } from "@zesty-io/core/ButtonGroup";
 
-import { searchInViewLogs, filterInViewLogs } from "../../store/logsInView";
-
 import styles from "./styles.less";
-export default React.memo(function AuditControls() {
-  const dispatch = useDispatch();
+export default React.memo(function AuditControls(props) {
   return (
     <header className={styles.auditControls}>
       <Search
         className={styles.SearchLogs}
         placeholder="Search AuditTrail Logs"
         onChange={value => {
-          dispatch(searchInViewLogs(value.trim()));
+          props.setSearch(value.trim().toLowerCase());
         }}
       />
       <ButtonGroup className={styles.btnGroup}>
         <Button
-          className={styles.child}
+          className={cx(styles.child, {
+            [styles.selected]: props.filter === 1
+          })}
           onClick={() => {
-            dispatch(filterInViewLogs("1"));
+            if (props.filter === 1) {
+              props.setFilter(-1);
+            } else {
+              props.setFilter(1);
+            }
           }}
         >
           Today
         </Button>
         <Button
-          className={styles.child}
+          className={cx(styles.child, {
+            [styles.selected]: props.filter === 7
+          })}
           onClick={() => {
-            dispatch(filterInViewLogs("7"));
+            if (props.filter === 7) {
+              props.setFilter(-1);
+            } else {
+              props.setFilter(7);
+            }
           }}
         >
           Last Week
         </Button>
         <Button
-          className={styles.child}
+          className={cx(styles.child, {
+            [styles.selected]: props.filter === 30
+          })}
           onClick={() => {
-            dispatch(filterInViewLogs("30"));
+            if (props.filter === 30) {
+              props.setFilter(-1);
+            } else {
+              props.setFilter(30);
+            }
           }}
         >
           Last Month
