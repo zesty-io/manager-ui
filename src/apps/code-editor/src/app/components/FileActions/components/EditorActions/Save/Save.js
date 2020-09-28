db4bcf879464a28f9e6a10c8f4fca841ddba0b4f
David Naimi <d88naimi@gmail.com>
2020-09-28 14:27:29 -0700

    fixed stackeed button on window size below 1280px

--- src/apps/code-editor/src/app/components/FileActions/components/EditorActions/Save/Save.js
+++ src/apps/code-editor/src/app/components/FileActions/components/EditorActions/Save/Save.js
@@ -5,6 +5,8 @@ import { Button } from "@zesty-io/core/Button";
 
 import { saveFile } from "../../../../../../store/files";
 
+import styles from "../EditorActions.less";
+
 export default connect(state => {
   return {
     platform: state.platform
@@ -48,7 +50,11 @@ export default connect(state => {
         ) : (
           <i className="fas fa-save"></i>
         )}
-        Save ({props.platform.isMac ? "CMD" : "CTRL"} + S)
+        Save&nbsp;
+        <span className={styles.HideSmall}>
+          {" "}
+          ({props.platform.isMac ? "CMD" : "CTRL"} + S)
+        </span>
       </Button>
     );
   })
