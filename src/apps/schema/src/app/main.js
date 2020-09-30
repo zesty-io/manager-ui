f93892fdd389ba44283e0b337634c996060f0026
Joshua Hanson <joshua.hanson@zesty.io>
2020-09-30 16:48:41 -0500

    Revert "Schema: invalid modelZUID and invalid fieldZUID handler"
    This reverts commit 0a50037d399b4fcbe1985bfaa8b9e6cb139720ed.

--- src/apps/schema/src/app/main.js
+++ src/apps/schema/src/app/main.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Switch, Route } from "react-router-dom";
-import { faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { WithLoader } from "@zesty-io/core/WithLoader";
 
-import Missing from "shell/components/missing";
 import { SchemaNav } from "./components/Nav";
 // import { Dashboard } from "./views/Dashboard";
 import { SchemaCreate } from "./views/SchemaCreate";
@@ -13,11 +11,7 @@ import { SchemaEdit } from "./views/SchemaEdit";
 
 import styles from "./main.less";
 export default connect(state => {
-  return {
-    navSchema: state.navSchema,
-    fields: state.fields,
-    models: state.models
-  };
+  return { navSchema: state.navSchema };
 })(function SchemaBuilder(props) {
   return (
     <WithLoader
@@ -33,47 +27,9 @@ export default connect(state => {
             <Route exact path="/schema/new" component={SchemaCreate} />
             <Route
               path="/schema/:modelZUID/field/:fieldZUID"
-              render={routeProps => {
-                const modelExists =
-                  props.models[routeProps.match.params.modelZUID];
-                const fieldExists =
-                  props.fields[routeProps.match.params.fieldZUID];
-
-                return modelExists && fieldExists ? (
-                  <SchemaEdit {...routeProps} />
-                ) : (
-                  <Missing
-                    missingText={
-                      modelExists
-                        ? "Field does not exist"
-                        : "Model does not exist"
-                    }
-                    buttonText={modelExists ? "Edit Model" : "Create Model"}
-                    buttonPath={
-                      modelExists
-                        ? `/schema/${routeProps.match.params.modelZUID}`
-                        : "/schema"
-                    }
-                    buttonIcon={modelExists ? faEdit : faPlus}
-                  />
-                );
-              }}
+              component={SchemaEdit}
             />
-            <Route
-              path="/schema/:modelZUID"
-              render={routeProps => {
-                return props.models[routeProps.match.params.modelZUID] ? (
-                  <SchemaEdit {...routeProps} />
-                ) : (
-                  <Missing
-                    missingText="Model does not exist"
-                    buttonText="Create Model"
-                    buttonPath="/schema"
-                    buttonIcon={faPlus}
-                  />
-                );
-              }}
-            />
+            <Route path="/schema/:modelZUID" component={SchemaEdit} />
             <Route path="/schema" component={SchemaCreate} />
           </Switch>
         </div>
