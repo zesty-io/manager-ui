0a50037d399b4fcbe1985bfaa8b9e6cb139720ed
Joshua Hanson <joshua.hanson@zesty.io>
2020-09-30 14:36:59 -0500

    Schema: invalid modelZUID and invalid fieldZUID handler

--- src/apps/schema/src/app/main.js
+++ src/apps/schema/src/app/main.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Switch, Route } from "react-router-dom";
+import { faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { WithLoader } from "@zesty-io/core/WithLoader";
 
+import Missing from "shell/components/missing";
 import { SchemaNav } from "./components/Nav";
 // import { Dashboard } from "./views/Dashboard";
 import { SchemaCreate } from "./views/SchemaCreate";
@@ -11,7 +13,11 @@ import { SchemaEdit } from "./views/SchemaEdit";
 
 import styles from "./main.less";
 export default connect(state => {
-  return { navSchema: state.navSchema };
+  return {
+    navSchema: state.navSchema,
+    fields: state.fields,
+    models: state.models
+  };
 })(function SchemaBuilder(props) {
   return (
     <WithLoader
@@ -27,9 +33,47 @@ export default connect(state => {
             <Route exact path="/schema/new" component={SchemaCreate} />
             <Route
               path="/schema/:modelZUID/field/:fieldZUID"
-              component={SchemaEdit}
+              render={routeProps => {
+                const modelExists =
+                  props.models[routeProps.match.params.modelZUID];
+                const fieldExists =
+                  props.fields[routeProps.match.params.fieldZUID];
+
+                return modelExists && fieldExists ? (
+                  <SchemaEdit {...routeProps} />
+                ) : (
+                  <Missing
+                    missingText={
+                      modelExists
+                        ? "Field does not exist"
+                        : "Model does not exist"
+                    }
+                    buttonText={modelExists ? "Edit Model" : "Create Model"}
+                    buttonPath={
+                      modelExists
+                        ? `/schema/${routeProps.match.params.modelZUID}`
+                        : "/schema"
+                    }
+                    buttonIcon={modelExists ? faEdit : faPlus}
+                  />
+                );
+              }}
             />
-            <Route path="/schema/:modelZUID" component={SchemaEdit} />
+            <Route
+              path="/schema/:modelZUID"
+              render={routeProps => {
+                return props.models[routeProps.match.params.modelZUID] ? (
+                  <SchemaEdit {...routeProps} />
+                ) : (
+                  <Missing
+                    missingText="Model does not exist"
+                    buttonText="Create Model"
+                    buttonPath="/schema"
+                    buttonIcon={faPlus}
+                  />
+                );
+              }}
+            />
             <Route path="/schema" component={SchemaCreate} />
           </Switch>
         </div>
--- src/shell/components/missing/index.js
+++ src/shell/components/missing/index.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { connect } from "react-redux";
+import { Button } from "@zesty-io/core/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useHistory } from "react-router-dom";
 
-import { Url } from "@zesty-io/core/Url";
-
-export default connect()(function Welcome() {
+export default function Missing(props) {
+  let history = useHistory();
   return (
     <section>
       <h1 style={{ textAlign: "center", fontSize: "38px", marginTop: "100px" }}>
-        This URL doesn't seem to have have anything here.
+        {props.missingText
+          ? props.missingText
+          : "This URL doesn't seem to have have anything here."}
       </h1>
-      <h2 style={{ textAlign: "center", fontSize: "26px", marginTop: "16px" }}>
-        If it seems like something is missing please let us know at{" "}
-        <Url href="mailto:support@zesty.io">support@zesty.io</Url>
-      </h2>
+      {props.buttonText && props.buttonPath && props.buttonIcon && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "8px"
+          }}
+        >
+          <Button
+            kind="secondary"
+            onClick={() => history.push(props.buttonPath)}
+            type="save"
+          >
+            <FontAwesomeIcon icon={props.buttonIcon} />
+            &nbsp;
+            {props.buttonText}
+          </Button>
+        </div>
+      )}
     </section>
   );
-});
+}
