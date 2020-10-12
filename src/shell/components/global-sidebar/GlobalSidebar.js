1326c6511c259e8a02710f54afeca578c901f870
Stuart Runyan <stuart@zesty.io>
2020-10-12 15:26:03 -0700

    Feature/favicon (#216)
    * fixed: render media app modal above shell
    
    * added: global favicon editing experience

--- src/shell/components/global-sidebar/GlobalSidebar.js
+++ src/shell/components/global-sidebar/GlobalSidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import styles from "./GlobalSidebar.less";
 
+import Favicon from "shell/components/favicon";
 import GlobalMenu from "shell/components/global-menu";
 import GlobalActions from "shell/components/global-actions";
 
@@ -13,6 +14,7 @@ export default connect(state => {
   return (
     <aside className={styles.GlobalSidebar}>
       <div className={styles.topMenu}>
+        <Favicon />
         <GlobalMenu />
         <GlobalActions />
       </div>
