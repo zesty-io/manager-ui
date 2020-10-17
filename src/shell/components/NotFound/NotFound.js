30f0f578b3c3d1e947f7864bf2451c657c475fff
Joshua Hanson <joshua.hanson@zesty.io>
2020-10-16 19:42:41 -0500

    use typography display

--- src/shell/components/NotFound/NotFound.js
+++ src/shell/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import styles from "./NotFound.less";
 export default function NotFound(props) {
   return (
     <section className={styles.NotFound}>
-      <h1 className={styles.Display}>{props.message}</h1>
+      <h1 className={styles.display}>{props.message}</h1>
     </section>
   );
 }
