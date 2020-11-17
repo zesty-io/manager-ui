4e280e09314fa7217395b0b0c20c880d911a8717
David Naimi <d88naimi@gmail.com>
2020-11-17 12:07:59 -0800

    side nav content and colors loaded in

--- src/apps/media-2/src/app/MediaApp.js
+++ src/apps/media-2/src/app/MediaApp.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import cx from "classnames";
 
 import { WithLoader } from "@zesty-io/core/WithLoader";
 
@@ -14,7 +15,28 @@ export default connect(state => {
         message="Starting Digital Asset Manager"
         width="100vw"
       >
-        <nav className={styles.Nav}>Nav</nav>
+        <nav className={styles.Nav}>
+          <article className="Parent">
+            <ul>
+              <h1 className={styles.NavTitle}>Company Title</h1>
+              <li className={cx(styles.item, styles.depth1)}>
+                <a href="#">
+                  <span>Group 1</span>
+                </a>
+              </li>
+              <li className={cx(styles.item, styles.depth2)}>
+                <a href="#">
+                  <span>Group 2</span>
+                </a>
+              </li>
+              <li className={cx(styles.item, styles.depth3)}>
+                <a href="#">
+                  <span>Group 3</span>
+                </a>
+              </li>
+            </ul>
+          </article>
+        </nav>
         <section className={styles.Workspace}>Images</section>
       </WithLoader>
     </main>
