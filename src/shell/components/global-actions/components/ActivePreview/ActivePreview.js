16d4703bada95ffbe622e163283e5e737c83ae52
Stuart Runyan <shrunyan@gmail.com>
2020-11-24 18:05:03 -0800

    use component state to track preview window reference

--- src/shell/components/global-actions/components/ActivePreview/ActivePreview.js
+++ src/shell/components/global-actions/components/ActivePreview/ActivePreview.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -10,47 +10,25 @@ export default connect(state => {
     content: state.content
   };
 })(function ActivePreview(props) {
-  let preview;
-
   const origin = window.location.origin;
   const location = useLocation();
 
-  function route(itemZUID) {
-    console.log("route", itemZUID);
-
-    // if not a string or a string that is not an content item zuid
-    // then see if location contains a routable content item
-    if (typeof itemZUID !== "string" || itemZUID.slice(0, 2) !== "7-") {
-      itemZUID = location.pathname
-        .split("/")
-        .find(part => part.slice(0, 2) === "7-");
-    }
-
-    console.log("route:itemZUID", itemZUID);
-
-    if (itemZUID && props.content[itemZUID]) {
-      const item = props.content[itemZUID];
-
-      console.log("route:item", item);
-
-      if (preview && item.web.path) {
-        preview.postMessage(
-          {
-            source: "zesty",
-            route: item.web.path
-          },
-          origin
-        );
-      }
-    }
-  }
+  /**
+   * NOTE: Preview holds a reference to a window object which
+   * we attach event listeners to as such there is some concern with
+   * react state holding on to a prior reference in the event of a render
+   * preventing GC and causing a memory leak.
+   */
+  const [preview, setPreview] = useState(null);
 
   function close() {
-    preview.close();
+    if (preview) {
+      preview.close();
+    }
   }
 
   function open() {
-    preview = window.open(
+    const winRef = window.open(
       origin + "/active-preview/",
       "Active Preview",
       "height=850, width=1400, location=0, menubar=0, status=0, titlebar=0, toolbar=0"
@@ -63,14 +41,50 @@ export default connect(state => {
     window.addEventListener("beforeunload", close);
 
     // Send initial route on open
-    preview.addEventListener("load", route);
+    winRef.addEventListener("load", () => route(location.pathname));
 
-    // if active preview is closed detach event listeners to avoid memory leak
-    preview.addEventListener("beforeunload", function(evt) {
+    // if active preview is closed detach manager event listeners to avoid memory leak
+    winRef.addEventListener("beforeunload", () => {
       zesty.off("UPDATED_CONTENT_ITEM");
       zesty.off("FORCE_PREVIEW_RERENDER");
       window.removeEventListener("beforeunload", close);
     });
+
+    setPreview(winRef);
+  }
+
+  // Sends message to preview window to update route
+  function route(itemZUID) {
+    console.log("route", itemZUID);
+
+    if (preview) {
+      // if not a string or a string that is not a content item zuid
+      // then see if location contains a routable content item
+      // only 7- resources are capable of having a path
+      if (typeof itemZUID !== "string" || itemZUID.slice(0, 2) !== "7-") {
+        itemZUID = location.pathname
+          .split("/")
+          .find(part => part.slice(0, 2) === "7-");
+      }
+
+      console.log("route:itemZUID", itemZUID);
+
+      if (itemZUID && props.content[itemZUID]) {
+        const item = props.content[itemZUID];
+
+        console.log("route:item", item);
+
+        if (item.web.path) {
+          preview.postMessage(
+            {
+              source: "zesty",
+              route: item.web.path
+            },
+            origin
+          );
+        }
+      }
+    }
   }
 
   /**
