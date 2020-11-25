ea8d38bfc2c91df695ee1dc288c2807ab9bdc7ac
David Naimi <d88naimi@gmail.com>
2020-11-25 12:12:59 -0800

    fixed width on search your files and create group breaking at 2000px

--- src/apps/media-2/src/app/components/MediaDetailsModal.js
+++ src/apps/media-2/src/app/components/MediaDetailsModal.js
@@ -78,9 +78,9 @@ export function MediaDetailsModal(props) {
                 }
                 placeholder={"Alt Attribute"}
               />
-              <h3>ZUID: {props.file.id}</h3>
+              <h3 className={styles.ModalLabels}>ZUID: {props.file.id}</h3>
 
-              <h3>
+              <h3 className={styles.ModalLabels}>
                 Updated: <span>{props.file.updated_at}</span>
               </h3>
             </div>
