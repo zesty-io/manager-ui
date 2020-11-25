a997d3888c481710f9aaffb2694ff76031d8ae85
David Naimi <d88naimi@gmail.com>
2020-11-25 10:01:17 -0800

    adding a loader spinner

--- src/apps/media-2/src/app/components/MediaWorkspace.js
+++ src/apps/media-2/src/app/components/MediaWorkspace.js
@@ -31,19 +31,19 @@ export function MediaWorkspace(props) {
                 <div className={styles.Checkered}>
                   <img src={file.url} alt={file.title} />
                 </div>
+                <div className={cx(styles.Load, styles.Loading)}></div>
                 <button className={styles.Check}>
                   <FontAwesomeIcon icon={faCheck} />
                 </button>
               </CardContent>
               <CardFooter className={styles.CardFooter}>
                 <button className={styles.FooterButton}>
-                  <p>
-                    <FontAwesomeIcon
-                      onClick={() => props.setFileDetails(file)}
-                      className={styles.Cog}
-                      icon={faCog}
-                    />
-                  </p>
+                  <FontAwesomeIcon
+                    onClick={() => props.setFileDetails(file)}
+                    className={styles.Cog}
+                    icon={faCog}
+                  />
+
                   <h1 className={styles.Preview}>{file.filename}</h1>
                 </button>
               </CardFooter>
