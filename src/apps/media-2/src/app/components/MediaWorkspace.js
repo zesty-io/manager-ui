6b0d8bfb600e9a7328bea9d29afe26cbdb96816a
Joshua Hanson <joshua.hanson@zesty.io>
2020-12-15 15:23:08 -0600

    drag and drop file upload with react-dnd instead of react-dropzone

--- src/apps/media-2/src/app/components/MediaWorkspace.js
+++ src/apps/media-2/src/app/components/MediaWorkspace.js
@@ -1,8 +1,9 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useRef } from "react";
 import { createSelector } from "reselect";
 import { useDispatch, useSelector } from "react-redux";
 import cx from "classnames";
-import { useDropzone } from "react-dropzone";
+import { useDrop } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "@zesty-io/core/Button";
@@ -17,14 +18,25 @@ const selectGroupFiles = createSelector(
   (files, currentGroup) =>
     files.filter(file => file.group_id === currentGroup.id)
 );
+
 export function MediaWorkspace(props) {
+  const ref = useRef();
+  const hiddenInputRef = useRef();
   const files = useSelector(state =>
     selectGroupFiles(state, props.currentGroup)
   );
   const dispatch = useDispatch();
 
-  const onDrop = useCallback(acceptedFiles => {
-    acceptedFiles.forEach(file => {
+  const chooseFile = useCallback(() => {
+    hiddenInputRef.current.click();
+  }, []);
+
+  function handleFileUpload(event) {
+    uploadFiles(Array.from(event.target.files));
+  }
+
+  function uploadFiles(files) {
+    files.forEach(file => {
       const fileToUpload = {
         file,
         bin_id: props.currentBin.id,
@@ -32,51 +44,22 @@ export function MediaWorkspace(props) {
       };
       dispatch(uploadFile(fileToUpload, props.currentBin));
     });
-  }, []);
+  }
 
-  const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
-    onDrop,
-    noClick: true
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: [NativeTypes.FILE],
+    drop(item, monitor) {
+      uploadFiles(monitor.getItem().files);
+    },
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+    })
   });
-  // const dropzoneRef = useRef();
-  // const dropMessageRef = useRef();
+  const isDragActive = canDrop && isOver;
 
-  // useEffect(() => {
-  //   let counter = 0;
+  drop(ref);
 
-  //   console.log("dropzone: ", dropzoneRef.current);
-  //   if (dropzoneRef.current) {
-  //     const unregisterDndEnter = DnD.enter(dropzoneRef.current, evt => {
-  //       console.log("dragenter");
-  //       counter++;
-  //       dropMessageRef.current.style.display = "flex";
-  //     });
-  //     const unregisterDndLeave = DnD.leave(dropzoneRef.current, evt => {
-  //       console.log("dragleave");
-  //       counter--;
-  //       if (counter === 0) {
-  //         dropMessageRef.current.style.display = "none";
-  //       }
-  //     });
-
-  //     const unregisterDndOver = DnD.over(dropzoneRef.current, () => {});
-  //     const unregisterDndDrop = DnD.drop(dropzoneRef.current, evt => {
-  //       console.log("drop");
-  //       counter = 0;
-  //       dropMessageRef.current.style.display = "none";
-
-  //       Array.from(evt.dataTransfer.files).forEach(file => {
-  //         console.log(file);
-  //       });
-  //     });
-  //     return () => {
-  //       unregisterDndEnter();
-  //       unregisterDndLeave();
-  //       unregisterDndOver();
-  //       unregisterDndDrop();
-  //     };
-  //   }
-  // }, [dropzoneRef.current]);
   return (
     <WithLoader
       // don't show loader if we already have files from before
@@ -84,21 +67,21 @@ export function MediaWorkspace(props) {
       message="Loading Files"
       width="100%"
     >
-      <div
-        {...getRootProps()}
-        // ref={dropzoneRef}
-      >
-        <input {...getInputProps()} />
+      <div ref={ref}>
+        <input
+          type="file"
+          multiple
+          ref={hiddenInputRef}
+          onChange={handleFileUpload}
+          style={{ display: "none" }}
+        />
         <main
           className={cx(styles.Workspace, {
             [styles.hasSelected]: props.selected.length
           })}
         >
           {isDragActive && (
-            <div
-              // ref={dropMessageRef}
-              className={styles.DropMessage}
-            >
+            <div className={styles.DropMessage}>
               <h1>
                 <FontAwesomeIcon icon={faUpload} />
                 Drop and upload!
@@ -125,7 +108,7 @@ export function MediaWorkspace(props) {
             <div className={styles.UploadMessage}>
               <div>Drag and drop files here</div>
               <div>or</div>
-              <Button onClick={open}>
+              <Button onClick={chooseFile}>
                 <FontAwesomeIcon icon={faUpload} />
                 Choose files to upload
               </Button>
