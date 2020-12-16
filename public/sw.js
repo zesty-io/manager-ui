addf7ce922797188fdea94f0ed17a07e338ea3d1
Stuart Runyan <shrunyan@gmail.com>
2020-12-15 16:09:48 -0800

    fixed: pull package version from root file

--- public/sw.js
+++ public/sw.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-20201215-002";
-const RUNTIME = "runtime-20201215-002";
+const PRECACHE = "precache-20201215-003";
+const RUNTIME = "runtime-20201215-003";
 
 const OFFLINE_URL = "/offline";
 
