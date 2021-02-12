fe877a8abdee976dd4cdc831ab61446b8b8d942d
Stuart Runyan <shrunyan@gmail.com>
2021-02-11 17:05:06 -0800

    fixed: init sentry

--- src/utility/sentry.js
+++ src/utility/sentry.js
@@ -1,9 +1,12 @@
 import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
-if (["stage", "production"].includes(window.CONFIG?.ENV)) {
+// window.CONFIG not available so we use the webpack injected variable
+if (["stage", "production"].includes(__CONFIG__?.ENV)) {
   Sentry.init({
-    release: window.CONFIG?.build?.data?.gitCommit,
-    environment: window.CONFIG?.ENV,
+    release: __CONFIG__?.build?.data?.gitCommit,
+    environment: __CONFIG__?.ENV,
+    integrations: [new Integrations.BrowserTracing()],
     dsn:
       "https://2e83c3767c484794a56832affe2d26d9@o162121.ingest.sentry.io/5441698"
   });
