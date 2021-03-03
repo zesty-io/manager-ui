14b6e90c91f8edea27f7401fee991495dfe07985
Stuart Runyan <shrunyan@gmail.com>
2021-03-02 17:56:42 -0800

    bump audit trail limit to allow for a 500 per day action average

--- src/shell/store/user.js
+++ src/shell/store/user.js
@@ -144,7 +144,7 @@ export function getUserLogs() {
     // const start = now.subtract(30, "days").format("YYYY-MM-DD");
     // `start_date=${start}&end_date=${end}`
 
-    return request(`${CONFIG.API_INSTANCE}/env/audits?limit=5000`)
+    return request(`${CONFIG.API_INSTANCE}/env/audits?limit=15000`)
       .then(res => {
         dispatch({
           type: "FETCH_USER_LOGS_SUCCESS",
