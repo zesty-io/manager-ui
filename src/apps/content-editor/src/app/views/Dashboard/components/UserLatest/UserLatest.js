e02209c73a92ef59054fcc0d6705b9d7f971f9cb
David Naimi <d88naimi@gmail.com>
2021-03-10 09:37:55 -0800

    swapped happenAt for createdAt

--- src/apps/content-editor/src/app/views/Dashboard/components/UserLatest/UserLatest.js
+++ src/apps/content-editor/src/app/views/Dashboard/components/UserLatest/UserLatest.js
@@ -26,7 +26,7 @@ export function UserLatest(props) {
       })
       .map(zuid => props.logs[zuid])
       .sort((loga, logb) => {
-        return Date.parse(logb.happenedAt) - Date.parse(loga.happenedAt);
+        return Date.parse(logb.createdAt) - Date.parse(loga.createdAt);
       })
       .slice(0, 5);
 
