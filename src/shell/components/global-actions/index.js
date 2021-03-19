2de67f660365e674c1a52cd05d494393bc2c26e7
Stuart Runyan <stuart@zesty.io>
2021-03-19 13:19:15 -0700

    refactor: notifications into a drawer of actions (#591)
    * refactor: notifications into a drawer of actions
    
    * fixed: styling
    
    * added: styling based on feedback
    
    * notification drawer width

--- src/shell/components/global-actions/index.js
+++ src/shell/components/global-actions/index.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
-import cx from "classnames";
 import useOnclickOutside from "react-cool-onclickoutside";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDungeon, faQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 
 import { Url } from "@zesty-io/core/Url";
-import { Button } from "@zesty-io/core/Button";
 
 import GlobalHelpMenu from "shell/components/GlobalHelpMenu";
-import { GlobalNotifications } from "./components/global-notifications";
 import { ActivePreview } from "./components/ActivePreview";
 
 import styles from "./styles.less";
@@ -27,10 +24,6 @@ export default React.memo(function GlobalActions(props) {
           <ActivePreview className={styles.GlobalActionIcon} />
         </span>
 
-        <span className={styles.GlobalAction}>
-          <GlobalNotifications className={styles.GlobalActionIcon} />
-        </span>
-
         <span
           ref={ref}
           onClick={() => setOpenMenu(!openMenu)}
@@ -43,6 +36,20 @@ export default React.memo(function GlobalActions(props) {
           />
           {openMenu && <GlobalHelpMenu />}
         </span>
+
+        <Url
+          href={`https://zesty.io`}
+          title="View source code commit"
+          target="_blank"
+          className={styles.GlobalAction}
+        >
+          <img
+            src="https://brand.zesty.io/zesty-io-logo.svg"
+            alt={`Zesty.io`}
+            width="18px"
+            height="18px"
+          />
+        </Url>
       </div>
       <div className={styles.AppVersion}>
         <Url
@@ -50,27 +57,9 @@ export default React.memo(function GlobalActions(props) {
           title="View source code commit"
           target="_blank"
         >
-          <img
-            src="https://brand.zesty.io/zesty-io-logo.svg"
-            alt={`Zesty.io version ${CONFIG?.build?.data?.gitCommit}`}
-            width="24px"
-            height="24px"
-          />
           <span className={styles.VersionNumber}>
             {CONFIG?.build?.data?.gitCommit}
           </span>
-        </Url>
-      </div>
-      <div className={styles.Legacy}>
-        <Url
-          href={`https://${props.hash}.manage.zesty.io/`}
-          title="Open Legacy Manager"
-          target="_blank"
-        >
-          <Button>
-            <FontAwesomeIcon icon={faDungeon} />
-            <span className={styles.hide}>Legacy</span>
-          </Button>
         </Url>
       </div>
     </div>
---
+++ src/shell/components/global-notifications/GlobalNotifications.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import moment from "moment-timezone";
+import cx from "classnames";
+import useOnclickOutside from "react-cool-onclickoutside";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faExclamationTriangle,
+  faCheckCircle
+} from "@fortawesome/free-solid-svg-icons";
+
+import { Drawer, DrawerContent } from "@zesty-io/core/Drawer";
+import { Divider } from "@zesty-io/core/Divider";
+import { AppLink } from "@zesty-io/core/AppLink";
+
+import styles from "./GlobalNotifications.less";
+export default connect(state => {
+  return {
+    notifications: state.notifications
+  };
+})(
+  React.memo(function GlobalNotifications(props) {
+    const [initialRender, setInitialRender] = useState(true);
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [showToast, setShowToast] = useState(false);
+
+    const ref = useOnclickOutside(() => {
+      setDrawerOpen(false);
+    });
+
+    useEffect(() => {
+      if (!initialRender && props.notifications.length) {
+        setShowToast(true);
+
+        // On every render set timeout to hide notices
+        const token = setTimeout(() => {
+          setShowToast(false);
+        }, 5000);
+
+        return () => {
+          clearTimeout(token);
+        };
+      }
+
+      // Avoid displaying logout notice on re-login
+      setInitialRender(false);
+    }, [props.notifications.length]);
+
+    const msgIcon = kind => {
+      let icon;
+
+      switch (kind) {
+        case "warn":
+        case "error":
+          icon = faExclamationTriangle;
+          break;
+        case "save":
+        case "success":
+          icon = faCheckCircle;
+          break;
+        default:
+          icon = faBell;
+          break;
+      }
+
+      return icon;
+    };
+
+    return (
+      <aside ref={ref} className={cx(styles.Notifications)}>
+        <span
+          className={cx(styles.Bell, showToast ? styles.Flash : null)}
+          title="See All Notifications"
+          onClick={() => setDrawerOpen(!drawerOpen)}
+        >
+          {props.notifications.length ? (
+            <span className={styles.Count}>{props.notifications.length}</span>
+          ) : (
+            <FontAwesomeIcon icon={faBell} />
+          )}
+        </span>
+
+        {props.notifications[0] && (
+          <div className={cx(styles.Toast, showToast ? null : styles.Hide)}>
+            <FontAwesomeIcon
+              className={cx(
+                props.notifications[0].kind
+                  ? styles[props.notifications[0].kind]
+                  : null,
+                styles.Icon
+              )}
+              icon={msgIcon(props.notifications[0].kind)}
+            />
+            <p className={styles.Message}>{props.notifications[0].message}</p>
+          </div>
+        )}
+
+        <Drawer
+          className={styles.Drawer}
+          position="right"
+          offset="0px"
+          width="450px"
+          height="calc(100vh - 54px)"
+          open={drawerOpen}
+        >
+          <DrawerContent className={styles.DrawerContent}>
+            <header>
+              <h1 className={styles.display}>My Notifications</h1>
+              <AppLink to="/audit-trail">View All Logs</AppLink>
+            </header>
+
+            {!props.notifications.length && (
+              <h2 className={styles.headline}>No actions taken</h2>
+            )}
+
+            <ul>
+              {props.notifications.map((notice, i) => {
+                return (
+                  <li
+                    key={i}
+                    className={cx(styles.Notification, styles.bodyText)}
+                  >
+                    <p className={styles.Message}>
+                      <FontAwesomeIcon
+                        className={cx(
+                          notice.kind ? styles[notice.kind] : null,
+                          styles.Icon
+                        )}
+                        icon={msgIcon(notice.kind)}
+                      />
+                      {notice.message}
+                    </p>
+                    {notice.epoch && (
+                      <small className={cx(styles.Timestamp, styles.caption)}>
+                        {moment(notice.epoch).fromNow()}
+                      </small>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </DrawerContent>
+        </Drawer>
+      </aside>
+    );
+  })
+);
