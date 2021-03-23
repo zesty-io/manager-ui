0366f0ecd5522e357133eabb321da8238bfb4ce5
David Naimi <d88naimi@gmail.com>
2021-03-23 15:19:23 -0700

    added withloader to userlatest

--- src/apps/content-editor/src/app/views/Dashboard/components/UserLatest/UserLatest.js
+++ src/apps/content-editor/src/app/views/Dashboard/components/UserLatest/UserLatest.js
@@ -6,13 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight, faClock } from "@fortawesome/free-solid-svg-icons";
 import { Card, CardHeader, CardContent } from "@zesty-io/core/Card";
 import { AppLink } from "@zesty-io/core/AppLink";
+import { WithLoader } from "@zesty-io/core/WithLoader";
 
 import styles from "./UserLatest.less";
 
 export function UserLatest(props) {
   const [latest, setLatest] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     let userLogs = Object.keys(props.logs)
 
       .filter(logZUID => {
@@ -42,52 +45,55 @@ export function UserLatest(props) {
       })
     ).then(logs => {
       setLatest(logs);
+      setLoading(false);
     });
   }, [props.user, props.logs]);
   return (
     <Card className={styles.UserLatestEdits}>
-      <CardHeader>
-        <FontAwesomeIcon icon={faClock} />
-        {props.cardTitle}
-      </CardHeader>
-      <CardContent className={styles.CardContent}>
-        {latest.length > 0 ? (
-          <ul>
-            {latest.map((log, i) => {
-              let url = "";
+      <WithLoader condition={!loading} message={`Loading ${props.cardTitle}`}>
+        <CardHeader>
+          <FontAwesomeIcon icon={faClock} />
+          {props.cardTitle}
+        </CardHeader>
+        <CardContent className={styles.CardContent}>
+          {latest.length > 0 ? (
+            <ul>
+              {latest.map((log, i) => {
+                let url = "";
 
-              if (log.meta?.url) {
-                url = log.meta.url
-                  .split("/")
-                  .slice(3)
-                  .join("/");
-              }
+                if (log.meta?.url) {
+                  url = log.meta.url
+                    .split("/")
+                    .slice(3)
+                    .join("/");
+                }
 
-              return (
-                <li key={i}>
-                  <hgroup>
-                    <h4>
-                      {log.recentTitle ? log.recentTitle : log.meta.message}
-                    </h4>
-                    <h5>{`Updated: ${moment(log.updatedAt).fromNow()}`}</h5>
-                  </hgroup>
+                return (
+                  <li key={i}>
+                    <hgroup>
+                      <h4>
+                        {log.recentTitle ? log.recentTitle : log.meta.message}
+                      </h4>
+                      <h5>{`Updated: ${moment(log.updatedAt).fromNow()}`}</h5>
+                    </hgroup>
 
-                  {url && (
-                    <AppLink className={styles.AppLink} to={url}>
-                      View
-                      <FontAwesomeIcon icon={faChevronRight} />
-                    </AppLink>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-        ) : props.action == "2" ? (
-          <h3 className={styles.display}>No recent changes</h3>
-        ) : (
-          <h3 className={styles.display}>No recent publishes</h3>
-        )}
-      </CardContent>
+                    {url && (
+                      <AppLink className={styles.AppLink} to={url}>
+                        View
+                        <FontAwesomeIcon icon={faChevronRight} />
+                      </AppLink>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          ) : props.action == "2" ? (
+            <h3 className={styles.display}>No recent changes</h3>
+          ) : (
+            <h3 className={styles.display}>No recent publishes</h3>
+          )}
+        </CardContent>
+      </WithLoader>
     </Card>
   );
 }
