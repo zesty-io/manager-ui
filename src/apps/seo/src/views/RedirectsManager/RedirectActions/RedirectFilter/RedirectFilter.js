a20315df239134a73c0d443e53b6e04875e4da22
David Naimi <d88naimi@gmail.com>
2021-04-13 17:36:57 -0700

    refactored redirectfilter class to functional component

--- src/apps/seo/src/views/RedirectsManager/RedirectActions/RedirectFilter/RedirectFilter.js
+++ src/apps/seo/src/views/RedirectsManager/RedirectActions/RedirectFilter/RedirectFilter.js
@@ -5,61 +5,22 @@ import { Search } from "@zesty-io/core/Search";
 import styles from "./RedirectFilter.less";
 
 export function RedirectFilter(props) {
-  const ref = useRef();
   const [filter, setFilter] = useState("");
 
-  const handleFilter = () => {
+  const handleFilter = val => {
     props.dispatch({
       type: "REDIRECT_FILTER",
-      filter: filterValue
+      filter: val
     });
+    setFilter(val);
   };
 
   return (
     <Search
       className={styles.filter}
-      onKeyUp={handleFilter}
-      onChange={evt => setFilter(evt.target.value)}
+      onChange={handleFilter}
       placeholder="Filter your redirects by url"
-      ref={ref}
       value={filter}
     />
   );
 }
-
-// export default class RedirectFilter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       filterTimeout: ""
-//     };
-
-//     this.redirectFilter = this.redirectFilter.bind(this, "SearchInput");
-//   }
-//   render() {
-//     return (
-//       <Search
-//         className={styles.filter}
-//         onKeyUp={this.redirectFilter}
-//         placeholder="Filter your redirects by url"
-//         ref="SearchInput"
-//       />
-//     );
-//   }
-//   redirectFilter(refName, e) {
-//     const filterValue = e.target.value.toLowerCase().trim();
-
-//     if (this.state.filterTimeout) {
-//       clearTimeout(this.state.filterTimeout);
-//     }
-
-//     this.setState({
-//       filterTimeout: setTimeout(() => {
-//         this.props.dispatch({
-//           type: "REDIRECT_FILTER",
-//           filter: filterValue
-//         });
-//       }, 100)
-//     });
-//   }
-// }
