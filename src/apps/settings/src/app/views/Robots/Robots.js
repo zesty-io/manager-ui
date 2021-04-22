44ac8cf7edd3523d6fdfe05369d22bd4e4b03cfc
David Naimi <d88naimi@gmail.com>
2021-04-22 13:25:29 -0700

    robot.js converted class to functional component (#675)
    * robot.js converted class to functional component
    
    * removing console.err
    
    * adding removeEventListener

--- src/apps/settings/src/app/views/Robots/Robots.js
+++ src/apps/settings/src/app/views/Robots/Robots.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import cx from "classnames";
 import { connect } from "react-redux";
+import { useDomain } from "shell/hooks/use-domain";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faSave, faSpinner } from "@fortawesome/free-solid-svg-icons";
@@ -19,232 +20,196 @@ import styles from "./Robots.less";
 
 export default connect(state => {
   return {
-    domains: state.instance.domains,
     platform: state.platform
   };
-})(
-  class Robots extends Component {
-    constructor(props) {
-      super(props);
+})(function Robots(props) {
+  const domain = useDomain();
 
-      this.state = {
-        loading: false,
-        robots_on: {
-          admin: false,
-          category: "general",
-          dataType: "checkbox",
-          key: "robots_on",
-          keyFriendly: "Search Engine Crawlable?",
-          parsleyAccess: false,
-          tips:
-            "Search engines will have permission to index each page of your site allowing for greater visibility"
-        },
-        robots_text: {
-          admin: false,
-          category: "general",
-          dataType: "textarea",
-          key: "robots_text",
-          keyFriendly: "Custom Robots.txt Content",
-          parsleyAccess: false
-        },
-        robotsUrl: `https://${props.domains[0].domain}/robots.txt`
-      };
+  const [loading, setLoading] = useState(false);
+  const [robotOn, setRobotOn] = useState({
+    admin: false,
+    category: "general",
+    dataType: "checkbox",
+    key: "robots_on",
+    keyFriendly: "Search Engine Crawlable?",
+    parsleyAccess: false,
+    tips:
+      "Search engines will have permission to index each page of your site allowing for greater visibility"
+  });
+  const [robotText, setRobotText] = useState({
+    admin: false,
+    category: "general",
+    dataType: "textarea",
+    key: "robots_text",
+    keyFriendly: "Custom Robots.txt Content",
+    parsleyAccess: false
+  });
+
+  const robotURL = `${domain}/robots.txt`;
+
+  useEffect(() => {
+    request(`${CONFIG.API_INSTANCE}/env/settings`).then(res => {
+      const robots_on = res.data.find(setting => setting.key === "robots_on");
+      const robots_text = res.data.find(
+        setting => setting.key === "robots_text"
+      );
+
+      // Merge current local state with incoming remote state
+      setRobotText(prevRobotText => ({
+        ...prevRobotText,
+        ...robots_text
+      }));
+      setRobotOn(prevRobotOn => ({
+        ...prevRobotOn,
+        ...robots_on,
+        // We require this to be a number to properly convert to a boolean for the FeildTypeBinary compnent
+        value: Number(robots_on.value)
+      }));
+    });
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleKeyDown = evt => {
+    if (
+      ((props.platform.isMac && evt.metaKey) ||
+        (!props.platform.isMac && evt.ctrlKey)) &&
+      evt.key == "s"
+    ) {
+      evt.preventDefault();
+      handleSave();
     }
+  };
 
-    componentDidMount() {
-      window.addEventListener("keydown", this.handleKeyDown);
+  const handleRobotsOn = value => {
+    setRobotOn(prevRobotOn => ({
+      ...prevRobotOn,
+      value
+    }));
+  };
 
-      request(`${CONFIG.API_INSTANCE}/env/settings`).then(res => {
-        const robots_on = res.data.find(setting => setting.key === "robots_on");
-        const robots_text = res.data.find(
-          setting => setting.key === "robots_text"
+  const handleRobotsText = value => {
+    setRobotText(prevRobotText => ({
+      ...prevRobotText,
+      value
+    }));
+  };
+
+  const handleSave = () => {
+    setLoading(true);
+
+    const robotsOn = makeRequest({
+      ...robotOn,
+      value: String(robotOn.value) // The API requires this as a string
+    });
+    const robotsText = makeRequest(robotText);
+
+    Promise.all([robotsOn, robotsText])
+      .then(res => {
+        props.dispatch(
+          notify({
+            kind: "success",
+            message: "robots.txt file settings have been updated"
+          })
         );
-
-        // Merge current local state with incoming remote state
-        this.setState({
-          robots_text: {
-            ...this.state.robots_text,
-            ...robots_text
-          },
-          robots_on: {
-            ...this.state.robots_on,
-            ...robots_on,
-
-            // We require this to be a number to properly convert to a boolean for the FeildTypeBinary compnent
-            value: Number(robots_on.value)
-          }
-        });
+        setLoading(false);
+      })
+      .catch(err => {
+        props.dispatch(
+          notify({
+            kind: "warn",
+            message: `Failed saving robots.txt settings. ${err}`
+          })
+        );
+        setLoading(true);
       });
-    }
+  };
 
-    componentWillUnmount() {
-      window.removeEventListener("keydown", this.handleKeyDown);
-    }
-
-    handleKeyDown = evt => {
-      if (
-        ((this.props.platform.isMac && evt.metaKey) ||
-          (!this.props.platform.isMac && evt.ctrlKey)) &&
-        evt.key == "s"
-      ) {
-        evt.preventDefault();
-        this.handleSave();
+  const makeRequest = data => {
+    return request(
+      `${CONFIG.API_INSTANCE}/env/settings${data.ZUID ? `/${data.ZUID}` : ""}`,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: data.ZUID ? "PUT" : "POST",
+        body: JSON.stringify(data)
       }
-    };
+    );
+  };
 
-    handleRobotsOn = value => {
-      this.setState({
-        robots_on: {
-          ...this.state.robots_on,
-          value
-        }
-      });
-    };
+  return (
+    <WithLoader condition={robotOn.ZUID} message="Finding robots.txt settings">
+      <div className={styles.Robots}>
+        <h2 className={styles.display}>
+          <FontAwesomeIcon icon={faEdit} />
+          Robots.txt Editor
+        </h2>
 
-    handleRobotsText = value => {
-      this.setState({
-        robots_text: {
-          ...this.state.robots_text,
-          value
-        }
-      });
-    };
+        <div className={styles.Row}>
+          <Notice>
+            <p>Changes will not be reflected until a publish event occurs.</p>
+          </Notice>
+          <Notice>
+            <p>
+              Stage/preview urls ALWAYS have robots.txt off to avoid being
+              crawled by search engines.
+            </p>
+          </Notice>
+        </div>
 
-    handleSave = () => {
-      this.setState({
-        loading: true
-      });
+        <div className={styles.Row}>
+          <FieldTypeBinary
+            name="settings[general][robots_on]"
+            label={robotOn.keyFriendly}
+            tooltip={robotOn.tips}
+            value={Boolean(robotOn.value)}
+            offValue="No"
+            onValue="Yes"
+            onChange={handleRobotsOn}
+          />
+        </div>
 
-      const robotsOn = this.makeRequest({
-        ...this.state.robots_on,
-        value: String(this.state.robots_on.value) // The API requires this as a string
-      });
-      const robotsText = this.makeRequest(this.state.robots_text);
+        <div className={cx(styles.IframeWrapper, styles.Row)}>
+          <h2>
+            <Url href={robotURL} target="_blank" title={robotURL}>
+              {robotURL}
+            </Url>
+          </h2>
+          <iframe
+            className={styles.Iframe}
+            src={`${robotURL}?q=${Math.random()
+              .toString(36)
+              .substring(2, 15)}`}
+          ></iframe>
+        </div>
 
-      Promise.all([robotsOn, robotsText])
-        .then(res => {
-          this.props.dispatch(
-            notify({
-              kind: "success",
-              message: "robots.txt file settings have been updated"
-            })
-          );
-          this.setState({
-            loading: false
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          this.props.dispatch(
-            notify({
-              kind: "warn",
-              message: `Failed saving robots.txt settings. ${err}`
-            })
-          );
-          this.setState({
-            loading: false
-          });
-        });
-    };
+        <div className={styles.Row}>
+          <FieldTypeTextarea
+            className={styles.CustomRules}
+            name="settings[general][robots_text]"
+            label={robotText.keyFriendly}
+            tooltip={robotText.tips}
+            onChange={handleRobotsText}
+            defaultValue={robotText.value}
+          />
+        </div>
 
-    makeRequest = data => {
-      return request(
-        `${CONFIG.API_INSTANCE}/env/settings${
-          data.ZUID ? `/${data.ZUID}` : ""
-        }`,
-        {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          method: data.ZUID ? "PUT" : "POST",
-          body: JSON.stringify(data)
-        }
-      );
-    };
-
-    render() {
-      return (
-        <WithLoader
-          condition={this.state.robots_on.ZUID}
-          message="Finding robots.txt settings"
+        <Button
+          kind="save"
+          className={styles.SaveBtn}
+          onClick={handleSave}
+          disabled={loading}
         >
-          <div className={styles.Robots}>
-            <h2 className={styles.display}>
-              <FontAwesomeIcon icon={faEdit} />
-              Robots.txt Editor
-            </h2>
-
-            <div className={styles.Row}>
-              <Notice>
-                <p>
-                  Changes will not be reflected until a publish event occurs.
-                </p>
-              </Notice>
-              <Notice>
-                <p>
-                  Stage/preview urls ALWAYS have robots.txt off to avoid being
-                  crawled by search engines.
-                </p>
-              </Notice>
-            </div>
-
-            <div className={styles.Row}>
-              <FieldTypeBinary
-                name="settings[general][robots_on]"
-                label={this.state.robots_on.keyFriendly}
-                tooltip={this.state.robots_on.tips}
-                value={Boolean(this.state.robots_on.value)}
-                offValue="No"
-                onValue="Yes"
-                onChange={this.handleRobotsOn}
-              />
-            </div>
-
-            <div className={cx(styles.IframeWrapper, styles.Row)}>
-              <h2>
-                <Url
-                  href={this.state.robotsUrl}
-                  target="_blank"
-                  title={this.state.robotsUrl}
-                >
-                  {this.state.robotsUrl}
-                </Url>
-              </h2>
-              <iframe
-                className={styles.Iframe}
-                src={`${this.state.robotsUrl}?q=${Math.random()
-                  .toString(36)
-                  .substring(2, 15)}`}
-              ></iframe>
-            </div>
-
-            <div className={styles.Row}>
-              <FieldTypeTextarea
-                className={styles.CustomRules}
-                name="settings[general][robots_text]"
-                label={this.state.robots_text.keyFriendly}
-                tooltip={this.state.robots_text.tips}
-                onChange={this.handleRobotsText}
-                defaultValue={this.state.robots_text.value}
-              />
-            </div>
-
-            <Button
-              kind="save"
-              className={styles.SaveBtn}
-              onClick={this.handleSave}
-              disabled={this.state.loading}
-            >
-              {this.state.loading ? (
-                <FontAwesomeIcon spin icon={faSpinner} />
-              ) : (
-                <FontAwesomeIcon icon={faSave} />
-              )}
-              Save Settings
-            </Button>
-          </div>
-        </WithLoader>
-      );
-    }
-  }
-);
+          {loading ? (
+            <FontAwesomeIcon spin icon={faSpinner} />
+          ) : (
+            <FontAwesomeIcon icon={faSave} />
+          )}
+          Save Settings
+        </Button>
+      </div>
+    </WithLoader>
+  );
+});
