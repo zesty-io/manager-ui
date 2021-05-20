d59bde3768e1eb1f64273ccf43ba89d2ad11aedc
Stuart Runyan <shrunyan@gmail.com>
2021-05-20 16:57:40 -0700

    improve media group delete modal

--- src/apps/media/src/app/components/MediaDeleteGroupModal.js
+++ src/apps/media/src/app/components/MediaDeleteGroupModal.js
@@ -1,38 +1,43 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Modal, ModalContent, ModalFooter } from "@zesty-io/core/Modal";
-import { Button } from "@zesty-io/core/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faExclamationCircle,
-  faCheck
+  faCheck,
+  faExclamationTriangle,
+  faBan
 } from "@fortawesome/free-solid-svg-icons";
+
+import { Modal, ModalContent, ModalFooter } from "@zesty-io/core/Modal";
+import { Button } from "@zesty-io/core/Button";
 
 import { deleteGroup } from "shell/store/media";
 import { closeTab } from "shell/store/ui";
 
-import shared from "./MediaShared.less";
-// import styles from "./MediaDeleteGroupModal.less";
-
+import styles from "./MediaDeleteGroupModal.less";
 export function MediaDeleteGroupModal(props) {
   const dispatch = useDispatch();
   return (
     <Modal
-      className={shared.Modal}
+      className={styles.Modal}
       type="global"
       open={true}
       onClose={() => props.onClose()}
     >
-      <ModalContent className={shared.subheadline}>
-        Are you sure you want to delete group?
+      <ModalContent className={styles.headline}>
+        <FontAwesomeIcon icon={faExclamationTriangle} />
+        &nbsp;Are you sure?
       </ModalContent>
-      <ModalContent className={shared.subheadline}>
-        <FontAwesomeIcon icon={faExclamationCircle} /> This will also delete all
-        subgroups and files.
+      <ModalContent className={styles.Content}>
+        <p className={styles.subheadline}>
+          Deleting this group will also delete all subgroups and files.
+        </p>
       </ModalContent>
-      <ModalFooter className={shared.ModalFooter}>
+      <ModalFooter className={styles.Footer}>
+        <Button kind="cancel" onClick={() => props.onClose()}>
+          <FontAwesomeIcon icon={faBan} />
+          <span>Cancel</span>
+        </Button>
         <Button
-          kind="save"
           onClick={() => {
             dispatch(deleteGroup(props.currentGroup)).then(() => {
               props.onClose();
@@ -42,10 +47,6 @@ export function MediaDeleteGroupModal(props) {
         >
           <FontAwesomeIcon icon={faCheck} />
           <span>Delete</span>
-        </Button>
-        <Button kind="warn" onClick={() => props.onClose()}>
-          <FontAwesomeIcon icon={faExclamationCircle} />
-          <span>Cancel</span>
         </Button>
       </ModalFooter>
     </Modal>
