306e67f481472669fee986362996d749eb08b3b3
Joshua Hanson <joshua.hanson@zesty.io>
2021-06-01 15:05:59 -0500

    fix: ItemEdit 404 uncaught error (#723)
    * fix: ItemEdit 404 uncaught error
    
    * keep rethrowing error

--- src/apps/content-editor/src/app/views/ItemEdit/ItemEdit.js
+++ src/apps/content-editor/src/app/views/ItemEdit/ItemEdit.js
@@ -176,6 +176,9 @@ export default connect((state, props) => {
         .dispatch(fetchItem(modelZUID, itemZUID))
         // select lang based on content lang
         .then(res => {
+          if (res.status === 404) {
+            throw new Error(res.message);
+          }
           this.props.dispatch(
             selectLang(
               this.props.languages.find(
