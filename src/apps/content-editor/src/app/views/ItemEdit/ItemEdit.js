b7e08f25558f312958caf5583a7bf9b067d4f08e
David Naimi <d88naimi@gmail.com>
2021-06-08 13:51:55 -0700

    swapped message fot 400 error

--- src/apps/content-editor/src/app/views/ItemEdit/ItemEdit.js
+++ src/apps/content-editor/src/app/views/ItemEdit/ItemEdit.js
@@ -179,7 +179,7 @@ export default connect((state, props) => {
           if (res.status === 400) {
             this.props.dispatch(
               notify({
-                message: `Failed saving item: ${res.error}`,
+                message: `Failed fetching item: ${res.error}`,
                 kind: "error"
               })
             );
