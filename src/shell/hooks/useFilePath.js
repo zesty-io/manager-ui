7eca4886100317ee568d16b850bf7b1a1603c3b8
David Naimi <d88naimi@gmail.com>
2021-07-08 13:17:23 -0700

    adding usefilepath look up on a 7- (#804)
    * adding usefilepath look up on a 7-
    
    * clean up
    
    * refactoring  7- helper hook
    
    * more clean up on conditional
    
    * adding comments
    
    * refactor 7- useFileHook
    
    * update return
    
    * moving return outside

--- src/shell/hooks/useFilePath.js
+++ src/shell/hooks/useFilePath.js
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 
 export function useFilePath(ZUID = "") {
-  const getFiles = state => state.files;
-  const selectFiles = createSelector([getFiles], files => files);
-  const files = useSelector(selectFiles);
+  const files = useSelector((state) => state.files);
+  const content = useSelector((state) => state.content);
+
   if (!ZUID) {
     throw new Error("Required ZUID argument was not provided");
   }
@@ -13,16 +13,31 @@ export function useFilePath(ZUID = "") {
   //Currently accepting model zuids, view, stylesheet, javascript zuids
   // There is no place where this method is called by a content item zuid, if there was we would need to reference all items.
   switch (Number(ZUID.split("-")[0])) {
+    // Searching on Model ZUID
     case zuid.prefix.SITE_CONTENT_SET:
-      let findfile = files.find(file => file.contentModelZUID === ZUID);
+      let findfile = files.find((file) => file.contentModelZUID === ZUID);
       if (findfile) {
         return `/code/file/views/${findfile.ZUID}`;
       } else {
         return "/code";
       }
 
+    // Searching on Content Item ZUID
+    case zuid.prefix.SITE_CONTENT_ITEM:
+      const item = content[ZUID];
+
+      if (item.meta.contentModelZUID) {
+        const findFile = files.find(
+          (file) => file.contentModelZUID === item.meta.contentModelZUID
+        );
+        if (findFile) {
+          return `/code/file/views/${findFile.ZUID}`;
+        }
+      }
+      return "/code";
+
     case zuid.prefix.SITE_RESOURCE:
-      const callBack = file => file.ZUID === ZUID;
+      const callBack = (file) => file.ZUID === ZUID;
       let file = files.find(callBack);
       if (file) {
         if (file.type === "text/javascript") {
