ee0a2381be9582c790803be32b90c88724c1d6f5
David Naimi <d88naimi@gmail.com>
2021-07-13 13:10:52 -0700

    Adding error handling for 400 crash (#809)
    * adding error handling for 400
    
    * omitting the else statement

--- src/apps/content-editor/src/app/views/ItemEdit/ItemEdit.js
+++ src/apps/content-editor/src/app/views/ItemEdit/ItemEdit.js
@@ -140,7 +140,7 @@ export default function ItemEdit() {
 
     try {
       const itemResponse = await dispatch(fetchItem(modelZUID, itemZUID));
-      if (itemResponse.status === 404) {
+      if (itemResponse.status === 404 || itemResponse.status === 400) {
         dispatch(
           notify({
             message: itemResponse.message,
