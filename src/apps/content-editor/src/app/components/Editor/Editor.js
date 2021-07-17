c002c40ef07dde8a5efe5a13bbb3627eb581b5cb
Joshua Hanson <joshua.hanson@zesty.io>
2021-07-16 19:05:08 -0500

    fix: Editor -> Field propagate value to TinyMCE if version is changed

--- src/apps/content-editor/src/app/components/Editor/Editor.js
+++ src/apps/content-editor/src/app/components/Editor/Editor.js
@@ -1,82 +1,80 @@
-import { PureComponent } from "react";
-
-import { Breadcrumbs } from "shell/components/global-tabs/components/Breadcrumbs";
-
+import React, { memo, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { AppLink } from "@zesty-io/core/AppLink";
+import { Breadcrumbs } from "shell/components/global-tabs/components/Breadcrumbs";
 import { Field } from "./Field";
-
 import styles from "./Editor.less";
-export default class Editor extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstTextField: this.props.fields.find(
-        (field) => field.datatype === "text"
-      ),
-      firstContentField: this.props.fields.find(
-        (field) =>
-          field.datatype === "textarea" ||
-          field.datatype === "article_writer" ||
-          field.datatype === "markdown" ||
-          field.datatype.includes("wysiwyg")
-      ),
-    };
-  }
 
-  componentDidUpdate() {
-    if (this.props.active) {
-      this.scrollToField(this.props.active);
+export default memo(function Editor({
+  active,
+  fields,
+  scrolled,
+  item,
+  model,
+  onSave,
+  itemZUID,
+}) {
+  const dispatch = useDispatch();
+  const firstTextField = fields.find((field) => field.datatype === "text");
+  const firstContentField = fields.find(
+    (field) =>
+      field.datatype === "textarea" ||
+      field.datatype === "article_writer" ||
+      field.datatype === "markdown" ||
+      field.datatype.includes("wysiwyg")
+  );
+
+  useEffect(() => {
+    if (active) {
+      scrollToField(active);
     }
-  }
+  }, [active]);
 
-  scrollToField = (fieldZUID) => {
+  const scrollToField = (fieldZUID) => {
     const node = document.getElementById(fieldZUID);
     if (node) {
       node.scrollIntoView({ behavior: "auto", block: "start" });
       node.querySelector("input") && node.querySelector("input").focus();
 
       //reset the active prop from the parent
-      this.props.scrolled();
+      scrolled();
     }
   };
 
-  onChange = (value, name) => {
+  const onChange = (value, name) => {
     if (!name) {
       throw new Error("Input is missing name attribute");
     }
 
     // Always dispatch the data update
-    this.props.dispatch({
+    dispatch({
       type: "SET_ITEM_DATA",
-      itemZUID: this.props.itemZUID,
+      itemZUID: itemZUID,
       key: name,
       value: value,
     });
 
     // If we are working with a new item
-    if (this.props.itemZUID.slice(0, 3) === "new") {
-      if (
-        this.state.firstTextField &&
-        this.state.firstTextField.name === name
-      ) {
-        this.props.dispatch({
+    if (itemZUID.slice(0, 3) === "new") {
+      if (firstTextField && firstTextField.name === name) {
+        dispatch({
           type: "SET_ITEM_WEB",
-          itemZUID: this.props.itemZUID,
+          itemZUID,
           key: "metaLinkText",
           value: value,
         });
-        this.props.dispatch({
+        dispatch({
           type: "SET_ITEM_WEB",
-          itemZUID: this.props.itemZUID,
+          itemZUID,
           key: "metaTitle",
           value: value,
         });
 
         // Datasets do not get path parts
-        if (this.props.model.type !== "dataset") {
-          this.props.dispatch({
+        if (model.type !== "dataset") {
+          dispatch({
             type: "SET_ITEM_WEB",
-            itemZUID: this.props.itemZUID,
+            itemZUID,
             key: "pathPart",
             value: value
               .trim()
@@ -87,13 +85,10 @@ export default class Editor extends PureComponent {
         }
       }
 
-      if (
-        this.state.firstContentField &&
-        this.state.firstContentField.name === name
-      ) {
-        this.props.dispatch({
+      if (firstContentField && firstContentField.name === name) {
+        dispatch({
           type: "SET_ITEM_WEB",
-          itemZUID: this.props.itemZUID,
+          itemZUID,
           key: "metaDescription",
           value: value.replace(/<[^>]*>/g, "").slice(0, 160),
         });
@@ -101,56 +96,51 @@ export default class Editor extends PureComponent {
     }
   };
 
-  render() {
-    const { item, active, model, fields, onSave } = this.props;
-    return (
-      <div className={styles.Fields}>
-        {this.props.item.meta && this.props.item.meta.ZUID && (
-          <Breadcrumbs itemZUID={this.props.item.meta.ZUID} />
-        )}
+  return (
+    <div className={styles.Fields}>
+      {item.meta && item.meta.ZUID && <Breadcrumbs itemZUID={item.meta.ZUID} />}
 
-        {fields.length ? (
-          fields
-            .filter((field) => !field.deletedAt)
-            .map((field) => {
-              return (
-                <div
-                  key={`${field.ZUID}`}
-                  id={field.ZUID}
-                  className={styles.Field}
-                >
-                  <Field
-                    ZUID={field.ZUID}
-                    contentModelZUID={field.contentModelZUID}
-                    active={active === field.ZUID}
-                    name={field.name}
-                    label={field.label}
-                    description={field.description}
-                    required={field.required}
-                    relatedFieldZUID={field.relatedFieldZUID}
-                    relatedModelZUID={field.relatedModelZUID}
-                    datatype={field.datatype}
-                    options={field.options}
-                    settings={field.settings}
-                    onChange={this.onChange}
-                    onSave={onSave}
-                    value={item && item.data && item.data[field.name]}
-                    langID={item?.meta?.langID}
-                  />
-                </div>
-              );
-            })
-        ) : (
-          <div className={styles.NoFields}>
-            <h1 className={styles.Display}>No fields have been added</h1>
-            <h2 className={styles.SubHead}>
-              Use the{" "}
-              <AppLink to={`/schema/${model.ZUID}`}>Schema Builder</AppLink> to
-              define your items content
-            </h2>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+      {fields.length ? (
+        fields
+          .filter((field) => !field.deletedAt)
+          .map((field) => {
+            return (
+              <div
+                key={`${field.ZUID}`}
+                id={field.ZUID}
+                className={styles.Field}
+              >
+                <Field
+                  ZUID={field.ZUID}
+                  contentModelZUID={field.contentModelZUID}
+                  active={active === field.ZUID}
+                  name={field.name}
+                  label={field.label}
+                  description={field.description}
+                  required={field.required}
+                  relatedFieldZUID={field.relatedFieldZUID}
+                  relatedModelZUID={field.relatedModelZUID}
+                  datatype={field.datatype}
+                  options={field.options}
+                  settings={field.settings}
+                  onChange={onChange}
+                  onSave={onSave}
+                  item={item}
+                  langID={item?.meta?.langID}
+                />
+              </div>
+            );
+          })
+      ) : (
+        <div className={styles.NoFields}>
+          <h1 className={styles.Display}>No fields have been added</h1>
+          <h2 className={styles.SubHead}>
+            Use the{" "}
+            <AppLink to={`/schema/${model.ZUID}`}>Schema Builder</AppLink> to
+            define your items content
+          </h2>
+        </div>
+      )}
+    </div>
+  );
+});
