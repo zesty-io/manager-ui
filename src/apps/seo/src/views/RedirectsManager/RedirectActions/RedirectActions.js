0664aae846727c945277804ff7a7e4db2cd9a759
David Naimi <d88naimi@gmail.com>
2021-07-21 13:25:41 -0700

    Feature/UI search updates (#835)
    * updating main nav to have a max-width of 350px and keeping all search bar and buttons stacked
    
    * removing unused style
    
    * moving search conpoenent to right side
    
    * moved SEO import next create redirect
    
    * updated audit trail to have search on right side
    
    * removing extra spacing

--- src/apps/seo/src/views/RedirectsManager/RedirectActions/RedirectActions.js
+++ src/apps/seo/src/views/RedirectsManager/RedirectActions/RedirectActions.js
@@ -1,9 +1,6 @@
 import styles from "./RedirectActions.less";
 
 import { RedirectFilter } from "./RedirectFilter";
-import RedirectsImport from "./RedirectsImport";
-
-import { CSVImporter } from "../../../store/imports";
 
 export default function RedirectActions(props) {
   return (
@@ -11,11 +8,6 @@ export default function RedirectActions(props) {
       <h1 className={styles.title}>{props.redirectsTotal} Total Redirects</h1>
       <div className={styles.actions}>
         <RedirectFilter dispatch={props.dispatch} />
-        <RedirectsImport
-          onChange={(evt) => {
-            props.dispatch(CSVImporter(evt));
-          }}
-        />
       </div>
     </header>
   );
