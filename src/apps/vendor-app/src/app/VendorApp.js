c414bf88c08c3f322493e84c2df84634b00cd11a
shrunyan <shrunyan@gmail.com>
2021-08-11 13:11:49 -0700

    added: provide instance zuid to child frames

--- src/apps/vendor-app/src/app/VendorApp.js
+++ src/apps/vendor-app/src/app/VendorApp.js
@@ -1,23 +1,24 @@
 import Cookies from "js-cookie";
 import cx from "classnames";
 import { useEffect, useRef, useState } from "react";
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 
 import { registerFrame } from "shell/store/apps";
 
 import styles from "./VendorApp.less";
-export default connect((state) => {
-  return {};
-})(function VendorApp(props) {
-  console.log("VendorApp", props);
+export default function VendorApp(props) {
+  const dispatch = useDispatch();
+  const instance = useSelector((state) => state.instance);
 
   const [sessionToken] = useState(Cookies.get(CONFIG.COOKIE_NAME));
   const frame = useRef();
   const src = "http://localhost:3000";
 
+  console.log("VendorApp", instance);
+
   useEffect(() => {
     if (frame.current) {
-      props.dispatch(registerFrame(frame.current));
+      dispatch(registerFrame(frame.current));
 
       frame.current.addEventListener("load", () => {
         // Send users session into frame on load
@@ -25,7 +26,9 @@ export default connect((state) => {
           {
             source: "zesty",
             sessionToken,
-            payload: {},
+            payload: {
+              instance,
+            },
           },
           src
         );
@@ -38,4 +41,4 @@ export default connect((state) => {
       <iframe src={src} ref={frame}></iframe>
     </main>
   );
-});
+}
