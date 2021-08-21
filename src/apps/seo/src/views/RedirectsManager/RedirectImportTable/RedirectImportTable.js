4ee8b9c50fe57551dd6367f453a314005408e37b
David Naimi <d88naimi@gmail.com>
2021-08-20 20:07:22 -0700

    converted redirectImportTable to a functional component

--- src/apps/seo/src/views/RedirectsManager/RedirectImportTable/RedirectImportTable.js
+++ src/apps/seo/src/views/RedirectsManager/RedirectImportTable/RedirectImportTable.js
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import cx from "classnames";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,75 +11,16 @@ import ImportTableRowDisabled from "./ImportTableRowDisabled";
 import { createRedirect } from "../../../store/redirects";
 import { cancelImports } from "../../../store/imports";
 
-export default class RedirectImportTable extends Component {
-  constructor(props) {
-    super(props);
+function RedirectImportTable(props) {
+  const handleCancelImport = () => {
+    props.dispatch(cancelImports());
+  };
 
-    this.handleCancelImport = this.handleCancelImport.bind(this);
-    this.handleAddAllRedirects = this.handleAddAllRedirects.bind(this);
-  }
-  render() {
-    return (
-      <section className={styles.RedirectImportTable}>
-        <div className={styles.Actions}>
-          <Button
-            className={styles.addAll}
-            onClick={this.handleAddAllRedirects}
-          >
-            <FontAwesomeIcon icon={faPlus} />
-            Add All Redirects
-          </Button>
-
-          <Button onClick={this.handleCancelImport}>
-            <FontAwesomeIcon icon={faTimes} />
-            Close Import
-          </Button>
-        </div>
-        <div className={styles.Header}>
-          <span className={cx(styles.Cell, styles.subheadline)}>
-            Incoming Path
-          </span>
-          <span className={cx(styles.Cell, styles.subheadline)}>Http Code</span>
-          <span className={cx(styles.Cell, styles.subheadline)}>
-            Redirect Type
-          </span>
-          <span className={cx(styles.Cell, styles.subheadline)}>
-            Redirect Target
-          </span>
-        </div>
-        <main className={styles.TableBody}>
-          {Object.keys(this.props.imports).map((key) => {
-            if (this.props.imports[key].canImport) {
-              return (
-                <RedirectImportTableRow
-                  key={key}
-                  paths={this.props.paths}
-                  dispatch={this.props.dispatch}
-                  siteZuid={this.props.siteZuid}
-                  {...this.props.imports[key]}
-                />
-              );
-            } else {
-              return (
-                <ImportTableRowDisabled
-                  key={key}
-                  {...this.props.imports[key]}
-                />
-              );
-            }
-          })}
-        </main>
-      </section>
-    );
-  }
-  handleCancelImport() {
-    this.props.dispatch(cancelImports());
-  }
-  handleAddAllRedirects() {
-    Object.keys(this.props.imports).forEach((path) => {
-      const redirect = this.props.imports[path];
+  const handleAddAllRedirects = () => {
+    Object.keys(props.imports).forEach((path) => {
+      const redirect = props.imports[path];
       if (redirect.canImport) {
-        this.props.dispatch(
+        props.dispatch(
           createRedirect({
             path: redirect.path,
             query_string: redirect.query_string,
@@ -91,5 +31,51 @@ export default class RedirectImportTable extends Component {
         );
       }
     });
-  }
+  };
+  return (
+    <section className={styles.RedirectImportTable}>
+      <div className={styles.Actions}>
+        <Button className={styles.addAll} onClick={handleAddAllRedirects}>
+          <FontAwesomeIcon icon={faPlus} />
+          Add All Redirects
+        </Button>
+
+        <Button onClick={handleCancelImport}>
+          <FontAwesomeIcon icon={faTimes} />
+          Close Import
+        </Button>
+      </div>
+      <div className={styles.Header}>
+        <span className={cx(styles.Cell, styles.subheadline)}>
+          Incoming Path
+        </span>
+        <span className={cx(styles.Cell, styles.subheadline)}>Http Code</span>
+        <span className={cx(styles.Cell, styles.subheadline)}>
+          Redirect Type
+        </span>
+        <span className={cx(styles.Cell, styles.subheadline)}>
+          Redirect Target
+        </span>
+      </div>
+      <main className={styles.TableBody}>
+        {Object.keys(props.imports).map((key) => {
+          if (props.imports[key].canImport) {
+            return (
+              <RedirectImportTableRow
+                key={key}
+                paths={props.paths}
+                dispatch={props.dispatch}
+                siteZuid={props.siteZuid}
+                {...props.imports[key]}
+              />
+            );
+          } else {
+            return <ImportTableRowDisabled key={key} {...props.imports[key]} />;
+          }
+        })}
+      </main>
+    </section>
+  );
 }
+
+export default RedirectImportTable;
