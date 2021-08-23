fa34c2eb3e20f8d810d594ea562a77c4eaad1862
David Naimi <d88naimi@gmail.com>
2021-08-23 16:15:52 -0700

    adding language to content dropdown one to onem one to many

--- src/apps/content-editor/src/app/components/Editor/Field/Field.js
+++ src/apps/content-editor/src/app/components/Editor/Field/Field.js
@@ -49,7 +49,7 @@ const FieldLabel = memo((props) => {
 // NOTE: Componetized so it can be memoized for input/render perf
 const RelatedOption = memo((props) => {
   return (
-    <span>
+    <span className={styles.RelatedOption}>
       <span onClick={(evt) => evt.stopPropagation()}>
         <AppLink
           className={styles.relatedItemLink}
@@ -129,17 +129,26 @@ function resolveRelatedOptions(
       );
     })
     .map((itemZUID) => {
-      return {
-        filterValue: items[itemZUID].data[field.name],
-        value: itemZUID,
-        component: (
-          <RelatedOption
-            modelZUID={modelZUID}
-            itemZUID={itemZUID}
-            text={items[itemZUID].data[field.name]}
-          />
-        ),
-      };
+      for (const [key, value] of Object.entries(items[itemZUID].siblings)) {
+        if (items[itemZUID].meta.ZUID === value) {
+          return {
+            filterValue: items[itemZUID].data[field.name],
+            value: itemZUID,
+            component: (
+              <RelatedOption
+                modelZUID={modelZUID}
+                itemZUID={itemZUID}
+                text={
+                  <>
+                    <span>{items[itemZUID].data[field.name]}</span>
+                    <em className={styles.Language}>&nbsp;{key}</em>
+                  </>
+                }
+              />
+            ),
+          };
+        }
+      }
     })
     .sort(sortTitle);
 }
