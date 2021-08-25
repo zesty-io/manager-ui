39770a7a0ff58d25b735dec14fe2e1eae39be747
David Naimi <d88naimi@gmail.com>
2021-08-25 16:07:01 -0700

    adding metaKeywords height and width to preview

--- src/shell/components/Head/Preview/Preview.js
+++ src/shell/components/Head/Preview/Preview.js
@@ -2,13 +2,13 @@ import styles from "./Preview.less";
 export const Preview = (props) => (
   <aside className={styles.TagPreviewWrap}>
     {/* {`<!-- Global head tags are inserted before local tags --> \n`} */}
-
+    {console.log("🚀 ~ file: Preview.js ~ PROPS", props)}
     <pre className={styles.TagPreview}>
       <div className={styles.Tag}>{`<head>`}</div>
       <div>
         {props.item &&
           props.item.web &&
-          `  
+          `
   <!-- Auto-generated Head Tags -->
   <title>${props.item.web.metaTitle}</title>
   <link rel="canonical" href="${props.domain}${props.item.web.path}" />
@@ -19,9 +19,12 @@ export const Preview = (props) => (
   <meta name="twitter:card" content="summary">
   <meta property="og:title" content="${props.item.web.metaTitle}" />
   <meta name="twitter:title" content="${props.item.web.metaTitle}">
+  <meta property="name:keywords" content="${props.item.web.metaKeywords}" />
   <meta property="og:description" content="${props.item.web.metaDescription}" />
   <meta property="twitter:description" content="${props.item.web.metaDescription}" />
   <meta property="og:url" content="${props.domain}${props.item.web.path}" />
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
   <meta property="og:site_name" content="${props.instanceName}" />\n
 `}
       </div>
