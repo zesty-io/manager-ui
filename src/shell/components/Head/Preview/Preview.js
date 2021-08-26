e51ca60b0706ebf4f0acc5c71d2e6c9903c3352d
David Naimi <d88naimi@gmail.com>
2021-08-26 11:26:04 -0700

    updated meta formation, updated Notice headtag

--- src/shell/components/Head/Preview/Preview.js
+++ src/shell/components/Head/Preview/Preview.js
@@ -13,12 +13,12 @@ export const Preview = (props) => (
   <link rel="canonical" href="${props.domain}${props.item.web.path}" />
 
   <meta name="description" content="${props.item.web.metaDescription}" />
+  <meta name="keywords" content="${props.item.web.metaKeywords}" />
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
   <meta property="og:type" content="website" />
   <meta name="twitter:card" content="summary">
   <meta property="og:title" content="${props.item.web.metaTitle}" />
   <meta name="twitter:title" content="${props.item.web.metaTitle}">
-  <meta property="name:keywords" content="${props.item.web.metaKeywords}" />
   <meta property="og:description" content="${props.item.web.metaDescription}" />
   <meta property="twitter:description" content="${props.item.web.metaDescription}" />
   <meta property="og:url" content="${props.domain}${props.item.web.path}" />
