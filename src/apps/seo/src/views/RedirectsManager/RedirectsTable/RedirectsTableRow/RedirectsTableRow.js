dbaed1f91b731d6ad7fbbe99898936741befe190
David Naimi <d88naimi@gmail.com>
2021-08-27 14:42:43 -0700

    fixing setModelZuid variable

--- src/apps/seo/src/views/RedirectsManager/RedirectsTable/RedirectsTableRow/RedirectsTableRow.js
+++ src/apps/seo/src/views/RedirectsManager/RedirectsTable/RedirectsTableRow/RedirectsTableRow.js
@@ -38,7 +38,7 @@ export default function RedirectsTableRow(props) {
       } else {
         dispatch(searchItems(props.target)).then((res) => {
           if (res && res.data.length) {
-            setModelZuid(findTarget.meta.contentModelZUID);
+            setModelZuid(res.meta.contentModelZUID);
             setPath(res.data[0].web.path);
           } else {
             setPath("Redirect Target Not Found");
