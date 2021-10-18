e81d8fa4d7749633cbac00e07da35f2b3343232b
faris huskovic <farishuskovic@fariss-MacBook-Pro.local>
2021-10-17 21:32:47 -0500

    update plugins file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,7 +19,6 @@ const { exec } = require("child_process");
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  os.exec();
   if (os.userInfo().username === "runner") {
     const ciEnvConfig = dotenv.config({
       path: path.join(__dirname, "../../", "ci/.env"),
