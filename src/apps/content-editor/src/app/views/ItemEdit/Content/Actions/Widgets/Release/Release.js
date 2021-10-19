597330eb3ba7e2fc14d504b4c2752bee155ee466
shrunyan <shrunyan@gmail.com>
2021-10-19 12:03:20 -0700

    added: widget for adding content item to release

---
+++ src/apps/content-editor/src/app/views/ItemEdit/Content/Actions/Widgets/Release/Release.js
@@ -0,0 +1,107 @@
+import { memo, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faRocket,
+  faEdit,
+  faPowerOff,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { Card, CardHeader, CardContent } from "@zesty-io/core/Card";
+import { FieldTypeDropDown } from "@zesty-io/core/FieldTypeDropDown";
+import { Button } from "@zesty-io/core/Button";
+import { AppLink } from "@zesty-io/core/AppLink";
+import { WithLoader } from "@zesty-io/core/WithLoader";
+
+import { fetchReleases } from "shell/store/releases";
+import { createMember } from "shell/store/releaseMembers";
+
+import styles from "./Release.less";
+export const Release = memo(function Release(props) {
+  const dispatch = useDispatch();
+  const releases = useSelector((state) => state.releases.data);
+
+  const [loading, setLoading] = useState(false);
+  const [addingMember, setAddingMember] = useState(false);
+  const [active, setActive] = useState(true);
+  const [selectedRelease, setSelectedRelease] = useState("0");
+
+  const onAdd = () => {
+    setAddingMember(true);
+    dispatch(
+      createMember(selectedRelease, {
+        resourceZUID: props.item.meta.ZUID,
+        version: props.item.meta.version,
+      })
+    ).finally(() => {
+      setAddingMember(false);
+    });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    dispatch(fetchReleases())
+      .then((res) => {
+        if (!res.data?.length) {
+          if (res.status === 412) {
+            setActive(false);
+          }
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <Card className={styles.Release}>
+      <CardHeader>
+        <FontAwesomeIcon icon={faRocket} /> Releases
+      </CardHeader>
+      <CardContent>
+        <WithLoader condition={!loading}>
+          {active ? (
+            <div className={styles.Row}>
+              <Button
+                onClick={onAdd}
+                disabled={selectedRelease === "0" || addingMember}
+              >
+                {addingMember ? (
+                  <FontAwesomeIcon icon={faSpinner} />
+                ) : (
+                  <FontAwesomeIcon icon={faPlus} />
+                )}
+              </Button>
+              <FieldTypeDropDown
+                name="release"
+                value={selectedRelease}
+                onChange={setSelectedRelease}
+                options={releases.map((release) => {
+                  return {
+                    value: release.ZUID,
+                    component: (
+                      <span>
+                        <span onClick={(evt) => evt.stopPropagation()}>
+                          <AppLink to={`/release/${release.ZUID}`}>
+                            <FontAwesomeIcon icon={faEdit} />
+                          </AppLink>
+                        </span>
+                        &nbsp;{release.name}
+                      </span>
+                    ),
+                  };
+                })}
+              />
+            </div>
+          ) : (
+            <AppLink to={`/release/activate`}>
+              <FontAwesomeIcon icon={faPowerOff} />
+              &nbsp;Activate Releases App
+            </AppLink>
+          )}
+        </WithLoader>
+      </CardContent>
+    </Card>
+  );
+});
