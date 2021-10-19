9ed1a1dd2403d35f4c3790a9680adafbaa86a9aa
David Naimi <d88naimi@gmail.com>
2021-10-19 15:47:25 -0700

    new cypress branch w/o creds

--- cypress/integration/code/actions_spec.js
+++ cypress/integration/code/actions_spec.js
@@ -9,20 +9,19 @@ describe("Code Editor", () => {
     cy.get(".Select").eq(1).click();
     cy.get(".Select li").eq(1).click();
     cy.get('input[type="text"]').type("mySnippet");
-    cy.get('button[type="save"]').eq(1).click();
+    cy.get('button[type="save"]').eq(0).click();
   });
 
   it("Navigate to file", () => {
-    cy.get("#Navigation > article").eq(1).click();
+    cy.get("#Navigation > article").eq(0).click();
   });
   it("Publish file", () => {
-    cy.get('button[kind="secondary"] .fa-cloud-upload-alt').click();
+    cy.get(".fa-cloud-upload-alt").eq(0).click();
     cy.contains("Published", { timeout: 5000 }).should("exist");
   });
 
   it("Sort resources", () => {
-    cy.contains("Order").first().click();
-
+    cy.contains("Order").eq(0).click();
     cy.get('[data-index="0"]').trigger("mousedown", {
       which: 1,
       force: true,
@@ -38,8 +37,10 @@ describe("Code Editor", () => {
     );
   });
 
+  // race conditions sometimes fails
   it("Compare files", () => {
-    cy.get('a[title="Diff Versions"]').click();
+    cy.contains("test.less").click();
+    cy.get('svg[data-icon="history"]').click();
     cy.get(".original").should("exist");
     cy.get(".modified").should("exist");
   });
