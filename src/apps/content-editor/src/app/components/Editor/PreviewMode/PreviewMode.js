8a4a620a4469d7132bb91146883c2926c921a6e8
shrunyan <shrunyan@gmail.com>
2021-12-02 18:10:58 -0800

    fixed: a few layout structures

--- src/apps/content-editor/src/app/components/Editor/PreviewMode/PreviewMode.js
+++ src/apps/content-editor/src/app/components/Editor/PreviewMode/PreviewMode.js
@@ -68,15 +68,14 @@ export default function PreviewMode(props) {
 
   return (
     <div className={styles.DMContainer}>
-      <div className={styles.Notice}>
+      {/* <div className={styles.Notice}>
         {props.dirty && <Notice>Click Save to See Changes</Notice>}
-      </div>
+      </div> */}
 
       <iframe
         ref={preview}
         src={`${CONFIG.URL_MANAGER_PROTOCOL}${instance.ZUID}${CONFIG.URL_MANAGER}/active-preview`}
         frameBorder="0"
-        style={{ width: "100%", height: "100vh" }}
       ></iframe>
     </div>
   );
