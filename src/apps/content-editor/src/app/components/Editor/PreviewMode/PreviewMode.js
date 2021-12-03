cb9f749c1e0d2c63ca4ad50e87492b7e4a73ac97
shrunyan <shrunyan@gmail.com>
2021-12-02 16:56:57 -0800

    added: link active-preview to content updates

--- src/apps/content-editor/src/app/components/Editor/PreviewMode/PreviewMode.js
+++ src/apps/content-editor/src/app/components/Editor/PreviewMode/PreviewMode.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Notice } from "@zesty-io/core/Notice";
 import { useSelector } from "react-redux";
 
 import styles from "./PreviewMode.less";
 export default function PreviewMode(props) {
+  const origin = window.location.origin;
+
   const instance = useSelector((state) => state.instance);
+  const content = useSelector((state) => state.content);
+
+  const preview = useRef(null);
+
+  function refresh() {
+    if (preview.current) {
+      preview.current.contentWindow.postMessage(
+        {
+          source: "zesty",
+          refresh: true,
+        },
+        origin
+      );
+    }
+  }
+
+  // Sends message to preview window to update route
+  function route(itemZUID) {
+    if (preview.current) {
+      // if not a string or a string that is not a content item zuid
+      // then see if location contains a routable content item
+      // only 7- resources are capable of having a path
+      if (typeof itemZUID !== "string" || itemZUID.slice(0, 2) !== "7-") {
+        itemZUID = location.pathname
+          .split("/")
+          .find((part) => part.slice(0, 2) === "7-");
+      }
+
+      if (itemZUID) {
+        const item = content[itemZUID];
+
+        if (item && item.web.path) {
+          preview.current.contentWindow.postMessage(
+            {
+              source: "zesty",
+              route: item.web.path,
+            },
+            origin
+          );
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (preview.current) {
+      // Send initial route on open
+      preview.current.addEventListener("load", () => route());
+    }
+
+    zesty.on("PREVIEW_ROUTE", route);
+    zesty.on("PREVIEW_REFRESH", refresh);
+
+    return () => {
+      zesty.off("PREVIEW_ROUTE");
+      zesty.off("PREVIEW_REFRESH");
+    };
+  }, []);
 
   return (
     <div className={styles.DMContainer}>
@@ -13,7 +73,7 @@ export default function PreviewMode(props) {
       </div>
 
       <iframe
-        key={props.version}
+        ref={preview}
         src={`${CONFIG.URL_MANAGER_PROTOCOL}${instance.ZUID}${CONFIG.URL_MANAGER}/active-preview`}
         frameBorder="0"
         style={{ width: "100%", height: "100vh" }}
