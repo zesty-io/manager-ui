deb33cf3e50cdedb4d36bbca7e9fc9968af75475
David Naimi <d88naimi@gmail.com>
2021-12-20 14:08:17 -0800

    aligned center duo mode toggle

--- src/apps/content-editor/src/app/views/ItemEdit/components/Header/ItemVersioning/VersionSelector/VersionSelector.js
+++ src/apps/content-editor/src/app/views/ItemEdit/components/Header/ItemVersioning/VersionSelector/VersionSelector.js
@@ -77,11 +77,9 @@ export default connect((state, props) => {
               key={`${item.meta?.ZUID}-${item.meta?.version}`}
               className={styles.VersionOption}
               value={item.meta?.version}
-              html={`V<span>ersion</span> ${
-                item.meta?.version
-              }   <small>${moment(item.web?.createdAt).format(
-                "MMM Do YYYY, [at] h:mm a"
-              )}</small>`}
+              html={`Version ${item.meta?.version}   <small>${moment(
+                item.web?.createdAt
+              ).format("MMM Do YYYY, [at] h:mm a")}</small>`}
             />
           ))}
       </Select>
