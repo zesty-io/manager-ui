34144bdc198a7c0c41de0bb2ca29feffc429b1f1
David Naimi <d88naimi@gmail.com>
2021-12-22 14:22:29 -0800

    adding sidebar

--- src/apps/content-editor/src/app/views/ItemEdit/Content/ActionsDrawer/ActionsDrawer.js
+++ src/apps/content-editor/src/app/views/ItemEdit/Content/ActionsDrawer/ActionsDrawer.js
@@ -5,6 +5,11 @@ import cx from "classnames";
 import { actions } from "shell/store/ui";
 
 import { Actions } from "../Actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./ActionsDrawer.less";
 
 export default function ActionsDrawer(props) {
@@ -33,22 +38,51 @@ export default function ActionsDrawer(props) {
 
   return (
     <aside
-      className={cx(
-        styles.Drawer,
-        ui.contentActionsHover && !ui.contentActions ? styles.DrawerHover : "",
-        ui.contentActions ? styles.DrawerAction : ""
-      )}
+      className={cx(styles.Drawer)}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       data-cy="ActionsContent"
     >
-      <Actions
-        {...props}
-        site={{}}
-        set={{
-          type: props.model.type,
-        }}
-      />
+      <div
+        className={cx(
+          ui.contentActionsHover && !ui.contentActions
+            ? styles.Hide
+            : styles.Show,
+          ui.contentActions ? styles.Hide : ""
+        )}
+      >
+        <ul className={styles.Quickbar}>
+          <li>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </li>
+        </ul>
+      </div>
+      <div
+        className={cx(
+          ui.contentActionsHover ? styles.DrawerHover : styles.Hide,
+          ui.contentActions ? styles.DrawerAction : ""
+        )}
+      >
+        <Actions
+          {...props}
+          site={{}}
+          set={{
+            type: props.model.type,
+          }}
+        />
+      </div>
     </aside>
   );
 }
