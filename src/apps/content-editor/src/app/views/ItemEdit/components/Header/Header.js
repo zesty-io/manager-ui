04e87c9bf21f635bdde2e5660ae0acba91670524
David Naimi <d88naimi@gmail.com>
2022-01-19 17:09:34 -0800

    removing unused logic from header clean up

--- src/apps/content-editor/src/app/views/ItemEdit/components/Header/Header.js
+++ src/apps/content-editor/src/app/views/ItemEdit/components/Header/Header.js
@@ -1,17 +1,13 @@
 import React, { Fragment } from "react";
 import cx from "classnames";
-import { useSelector } from "react-redux";
 
 import { DuoModeToggle } from "./DuoModeToggle";
 import { LanguageSelector } from "./LanguageSelector";
-import { PreviewUrl } from "./PreviewUrl";
-import { LiveUrl } from "./LiveUrl";
+
 import ItemNavigation from "./ItemNavigation";
 
 import styles from "./Header.less";
 export function Header(props) {
-  const ui = useSelector((state) => state.ui);
-
   return (
     <header className={styles.Header}>
       <div className={cx(styles.Split)}>
@@ -21,13 +17,6 @@ export function Header(props) {
             itemZUID={props.itemZUID}
             item={props.item}
           />
-
-          {/* {!ui.duoMode && (
-            <div className={styles.ViewLinks}>
-              <LiveUrl item={props.item} />
-              <PreviewUrl item={props.item} instance={props.instance} />
-            </div>
-          )} */}
         </div>
         <div className={styles.Right}>
           <div className={styles.Actions}>
