19d91e3d16f00fe3d9f6817b7d2c4fc6d5574d65
Stuart Runyan <shrunyan@gmail.com>
2022-01-26 11:32:16 -0800

    adaed: content actions drawer widget for displaying related content links

---
+++ src/apps/content-editor/src/app/views/ItemEdit/Content/Actions/Widgets/ContentLinks/ContentLinks.js
@@ -0,0 +1,37 @@
+import { memo, Fragment } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { Card, CardHeader, CardContent } from "@zesty-io/core/Card";
+
+import { PreviewUrl } from "../../../../components/Header/PreviewUrl";
+import { LiveUrl } from "../../../../components/Header/LiveUrl";
+import { InstantUrl } from "./InstantUrl";
+
+import styles from "./ContentLinks.less";
+
+export const ContentLinks = memo(function ContentLinks(props) {
+  return (
+    <Fragment>
+      <Card className={styles.ContentLinks}>
+        <CardHeader>
+          <FontAwesomeIcon icon={faLink} />
+          &nbsp;Links
+        </CardHeader>
+        <CardContent className={styles.Content}>
+          <ul>
+            <li>
+              <LiveUrl item={props.item} />
+            </li>
+            <li>
+              <PreviewUrl item={props.item} />
+            </li>
+            <li>
+              <InstantUrl item={props.item} />
+            </li>
+          </ul>
+        </CardContent>
+      </Card>
+    </Fragment>
+  );
+});
