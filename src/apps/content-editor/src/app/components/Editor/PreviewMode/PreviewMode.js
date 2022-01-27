25b42d2cedce6b84eda004325992360d76b1f200
Stuart Runyan <shrunyan@gmail.com>
2022-01-27 13:23:38 -0800

    reorg: move overlay into active preview to avoid blocking controls

--- src/apps/content-editor/src/app/components/Editor/PreviewMode/PreviewMode.js
+++ src/apps/content-editor/src/app/components/Editor/PreviewMode/PreviewMode.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-
-import { Notice } from "@zesty-io/core/Notice";
 
 import styles from "./PreviewMode.less";
 export default function PreviewMode(props) {
@@ -18,13 +16,8 @@ export default function PreviewMode(props) {
 
   const preview = useRef(null);
 
-  // Track initial version provided. We use this to make a determination
-  // on whether current content has changed or the different version was
-  // picked for previewing
-  const [initialVersion] = useState(props.version);
-
   // Sends message to preview window to update route
-  function route(itemZUID, version) {
+  function route(itemZUID, version, dirty) {
     if (preview.current) {
       // if not a string or a string that is not a content item zuid
       // then see if location contains a routable content item
@@ -54,6 +47,7 @@ export default function PreviewMode(props) {
             route: url,
             settings: instanceSettings,
             version: version,
+            dirty: dirty,
           },
           origin
         );
@@ -63,32 +57,24 @@ export default function PreviewMode(props) {
 
   useEffect(() => {
     if (preview.current) {
-      // Send initial route on open
+      // ActivePreview iframe is loading, send route when ready
       preview.current.addEventListener("load", () =>
-        route(props.itemZUID, props.version)
+        route(props.itemZUID, props.version, props.dirty)
       );
 
-      // If the ActivePreview is loaded and we need to send route updates
+      // ActivePreview iframe is loaded, send route updates
       const doc =
         preview.current.contentDocument ||
         preview.current.contentWindow.document;
       if (doc.readyState === "complete") {
-        route(props.itemZUID, props.version);
+        route(props.itemZUID, props.version, props.dirty);
       }
     }
-  }, [props.itemZUID, props.version]);
+  }, [props.itemZUID, props.version, props.dirty]);
 
   return (
     <div data-cy="DuoModeContainer" className={styles.DMContainer}>
-      {initialVersion === props.version && props.dirty && (
-        <div className={styles.Overlay}>
-          <Notice>Save to update preview</Notice>
-        </div>
-      )}
       <iframe
-        className={
-          initialVersion === props.version && props.dirty ? styles.Blur : null
-        }
         ref={preview}
         src={`${CONFIG.URL_MANAGER_PROTOCOL}${instance.ZUID}${CONFIG.URL_MANAGER}/active-preview`}
         frameBorder="0"
