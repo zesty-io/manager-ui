8bdeca7be4a238f78d2241ac8fe51ddbdc1c3a95
Andres Galindo <andres.galindo@mpulsemobile.com>
2022-03-04 15:09:54 -0800

    Refactor schema structure to match content editor and add loading state to properly handle empty states

--- src/apps/schema/src/index.js
+++ src/apps/schema/src/index.js
@@ -1,41 +1,11 @@
-import { useEffect } from "react";
-import { connect } from "react-redux";
-
-import { WithLoader } from "@zesty-io/core/WithLoader";
-
 import { store, injectReducer } from "shell/store";
-import { fetchModels } from "shell/store/models";
 
 import { navSchema } from "./store/navSchema";
 import { parents } from "./store/parents";
-
-import { fetchSettings } from "shell/store/settings";
 
 import { SchemaBuilder } from "./app";
 
 injectReducer(store, "navSchema", navSchema);
 injectReducer(store, "parents", parents);
 
-export default connect((state) => {
-  return {
-    settings: state.settings,
-    models: state.models,
-  };
-})(function SchemaApp(props) {
-  useEffect(() => {
-    store.dispatch(fetchModels());
-    // used for instant JSON API
-    store.dispatch(fetchSettings());
-  }, []);
-
-  return (
-    <WithLoader
-      condition={
-        props.settings.instance.length && Object.keys(props.models).length
-      }
-      message="Starting Schema"
-    >
-      <SchemaBuilder />
-    </WithLoader>
-  );
-});
+export default SchemaBuilder;
