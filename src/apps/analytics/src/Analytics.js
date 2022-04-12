a6ee5b01dd08b947287d4eb168d3e72eda610333
Andres <agalin920@gmail.com>
2022-04-11 21:27:06 -0700

    Refactor Analytics class to function component and use useDomain hook

--- src/apps/analytics/src/Analytics.js
+++ src/apps/analytics/src/Analytics.js
@@ -1,7 +1,8 @@
-import { PureComponent } from "react";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import cx from "classnames";
 import moment from "moment";
+import { useDomain } from "shell/hooks/use-domain";
 
 // import { ContentVelocity } from "./components/ContentVelocity";
 import { PageviewTraffic } from "./components/PageviewTraffic";
@@ -33,165 +34,147 @@ export default connect(function (state) {
       return acc;
     }, {}),
   };
-})(
-  class Analytics extends PureComponent {
-    state = {
-      recentlyEditedItems: [],
-      favoriteModels: [],
-      loading: true,
-      webEngineOn: true, // in the future this will be a boolean pulled off the account, it will change the dash
-      gaAuthenticated: Boolean(this.props.instance.google_profile_id),
-      gaLegacyAuth: false, // we need response body from cloud function could change this
-    };
+})(function Analytics(props) {
+  const [recentlyEditedItems, setRecentlyEditedItems] = useState([]);
+  const [favoriteModels, setFavoriteModels] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [webEngineOn, setWebEngineOn] = useState(true);
+  const [gaAuthenticated, setGaAuthenticated] = useState(
+    Boolean(props.instance.google_profile_id)
+  );
+  const [gaLegacyAuth, setGaLegacyAuth] = useState(false);
 
-    componentDidMount() {
-      // if (this.props.instance.google_profile_id) {
-      const start = moment().subtract(120, "days").format("YYYY-MM-DD");
+  const domainSet = useDomain();
 
-      this.props
-        .dispatch(fetchRecentItems(this.props.user.ZUID, start))
-        .then((res) => {
-          if (res && res.data) {
-            this.setState({
-              recentlyEditedItems: this.getLastEditedItems(res.data),
-              favoriteModels: this.getFavoriteModels(res.data),
-              loading: false,
-            });
-          } else {
-            this.setState({
-              loading: false,
-            });
-          }
-        });
-      // }
-    }
+  const setGALegacyStatus = (status) => {
+    setGaLegacyAuth(status);
+  };
+  /**
+    Group items by model
+    [
+      [contentModelZUID, [item, item, ...]]
+    ]
+  **/
+  const getFavoriteModels = (items) => {
+    const grouped = items.reduce((acc, item) => {
+      if (acc[item.meta.contentModelZUID]) {
+        acc[item.meta.contentModelZUID].push(item);
+      } else {
+        acc[item.meta.contentModelZUID] = [item];
+      }
+      return acc;
+    }, {});
 
-    setGALegacyStatus = (status) => {
-      this.setState({ gaLegacyAuth: status });
-    };
-    /**
-      Group items by model
-      [
-        [contentModelZUID, [item, item, ...]]
-      ]
-    **/
-    getFavoriteModels(items) {
-      const grouped = items.reduce((acc, item) => {
-        if (acc[item.meta.contentModelZUID]) {
-          acc[item.meta.contentModelZUID].push(item);
-        } else {
-          acc[item.meta.contentModelZUID] = [item];
+    const sorted = Object.keys(grouped)
+      .filter((item) => grouped[item][0].web.metaTitle)
+      .map((contentModelZUID) => {
+        return [contentModelZUID, grouped[contentModelZUID].slice(0, 3)];
+      })
+      .sort((a, b) => {
+        if (a[1].length < b[1].length) {
+          return 1;
         }
-        return acc;
-      }, {});
+        if (a[1].length > b[1].length) {
+          return -1;
+        }
+        return 0;
+      });
 
-      const sorted = Object.keys(grouped)
-        .filter((item) => grouped[item][0].web.metaTitle)
-        .map((contentModelZUID) => {
-          return [contentModelZUID, grouped[contentModelZUID].slice(0, 3)];
-        })
-        .sort((a, b) => {
-          if (a[1].length < b[1].length) {
-            return 1;
-          }
-          if (a[1].length > b[1].length) {
-            return -1;
-          }
-          return 0;
-        });
+    // Top three most edited models
+    return sorted.slice(0, 3);
+  };
 
-      // Top three most edited models
-      return sorted.slice(0, 3);
-    }
+  const getLastEditedItems = (items) => {
+    return [...items]
+      .sort((a, b) => {
+        if (a.meta.updatedAt < b.meta.updatedAt) {
+          return 1;
+        }
+        if (a.meta.updatedAt > b.meta.updatedAt) {
+          return -1;
+        }
+        return 0;
+      })
+      .slice(0, 5);
+  };
 
-    getLastEditedItems(items) {
-      return [...items]
-        .sort((a, b) => {
-          if (a.meta.updatedAt < b.meta.updatedAt) {
-            return 1;
-          }
-          if (a.meta.updatedAt > b.meta.updatedAt) {
-            return -1;
-          }
-          return 0;
-        })
-        .slice(0, 5);
-    }
+  useEffect(() => {
+    const start = moment().subtract(120, "days").format("YYYY-MM-DD");
 
-    render() {
-      const domainSet = Boolean(
-        this.props.instance.domains &&
-          this.props.instance.domains[0] &&
-          this.props.instance.domains[0].domain
-      );
-      return (
-        <section className={styles.Dashboard}>
-          <div className={styles.container}>
-            <div
-              className={cx(
-                styles.columns,
-                styles.graphs,
-                styles.analyticsContainer
-              )}
-            >
-              {(!this.state.gaAuthenticated || this.state.gaLegacyAuth) && (
-                <GoogleAuthOverlay
-                  gaLegacyAuth={this.state.gaLegacyAuth}
-                  domainSet={domainSet}
-                  gaAuthenticated={this.state.gaAuthenticated}
-                  user={this.props.user}
-                  instance={this.props.instance}
-                />
-              )}
-              {/* TODO add Google Auth Modal here */}
-              <div className={cx(styles.column, styles.primary)}>
-                <PageviewTraffic
-                  setGALegacyStatus={this.setGALegacyStatus}
-                  instanceZUID={this.props.instanceZUID}
-                  profileID={this.props.instance.google_profile_id}
-                  data={shelldata.shellBarData()}
-                  domainSet={domainSet}
-                />
-              </div>
+    props.dispatch(fetchRecentItems(props.user.ZUID, start)).then((res) => {
+      if (res && res.data) {
+        setRecentlyEditedItems(getLastEditedItems(res.data));
+        setFavoriteModels(getFavoriteModels(res.data));
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
 
-              <div className={styles.column}>
-                <InboundTraffic
-                  setGALegacyStatus={this.setGALegacyStatus}
-                  instanceZUID={this.props.instanceZUID}
-                  profileID={this.props.instance.google_profile_id}
-                  data={shelldata.shellDoughnutData()}
-                  domainSet={domainSet}
-                />
-                <SocialTraffic
-                  setGALegacyStatus={this.setGALegacyStatus}
-                  instanceZUID={this.props.instanceZUID}
-                  profileID={this.props.instance.google_profile_id}
-                  data={shelldata.shellDoughnutData()}
-                  domainSet={domainSet}
-                />
-              </div>
-            </div>
-
-            <div className={styles.columns}>
-              {/*<div className={styles.column}>
-              <ContentVelocity />
-            </div>*/}
-              <div className={cx(styles.column)}>
-                <RecentlyEdited
-                  items={this.state.recentlyEditedItems}
-                  loading={this.state.loading}
-                />
-              </div>
-              <div className={cx(styles.column, styles.recent)}>
-                <TopPerforming
-                  instanceZUID={this.props.instanceZUID}
-                  profileID={this.props.instance.google_profile_id}
-                />
-              </div>
-            </div>
+  return (
+    <section className={styles.Dashboard}>
+      <div className={styles.container}>
+        <div
+          className={cx(
+            styles.columns,
+            styles.graphs,
+            styles.analyticsContainer
+          )}
+        >
+          {(!gaAuthenticated || gaLegacyAuth) && (
+            <GoogleAuthOverlay
+              gaLegacyAuth={gaLegacyAuth}
+              domainSet={domainSet}
+              gaAuthenticated={gaAuthenticated}
+              user={props.user}
+              instance={props.instance}
+            />
+          )}
+          {/* TODO add Google Auth Modal here */}
+          <div className={cx(styles.column, styles.primary)}>
+            <PageviewTraffic
+              setGALegacyStatus={setGALegacyStatus}
+              instanceZUID={props.instanceZUID}
+              profileID={props.instance.google_profile_id}
+              data={shelldata.shellBarData()}
+              domainSet={domainSet}
+            />
           </div>
-        </section>
-      );
-    }
-  }
-);
+
+          <div className={styles.column}>
+            <InboundTraffic
+              setGALegacyStatus={setGALegacyStatus}
+              instanceZUID={props.instanceZUID}
+              profileID={props.instance.google_profile_id}
+              data={shelldata.shellDoughnutData()}
+              domainSet={domainSet}
+            />
+            <SocialTraffic
+              setGALegacyStatus={setGALegacyStatus}
+              instanceZUID={props.instanceZUID}
+              profileID={props.instance.google_profile_id}
+              data={shelldata.shellDoughnutData()}
+              domainSet={domainSet}
+            />
+          </div>
+        </div>
+
+        <div className={styles.columns}>
+          {/*<div className={styles.column}>
+          <ContentVelocity />
+        </div>*/}
+          <div className={cx(styles.column)}>
+            <RecentlyEdited items={recentlyEditedItems} loading={loading} />
+          </div>
+          <div className={cx(styles.column, styles.recent)}>
+            <TopPerforming
+              instanceZUID={props.instanceZUID}
+              profileID={props.instance.google_profile_id}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+});
