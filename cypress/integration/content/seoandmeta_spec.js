ea4729d710c50620b5e73d1c88c10f738e30378d
David Naimi <d88naimi@gmail.com>
2022-06-02 10:16:49 -0700

    adding force true for modal blocker

--- cypress/integration/content/seoandmeta_spec.js
+++ cypress/integration/content/seoandmeta_spec.js
@@ -10,7 +10,7 @@ describe("Content SEO & Meta", () => {
       "eq",
       "/content/6-a4d6e6f087-1jmlbx/7-b8da98dea8-1nwkc9/meta"
     );
-    cy.get("[data-cy=meta]").click();
+    cy.get("[data-cy=meta]").click({ force: true });
     cy.get("header").find(".Select").first().click();
     cy.get('[data-value="es"]').click();
     cy.get("[data-cy=meta]").click();
