f7855280cd3551735ca45cde853332b369913d84
David Naimi <d88naimi@gmail.com>
2022-06-17 08:58:28 -0700

    swapped lead search and fixed duplicate data being shown

--- src/apps/leads/src/app/components/LeadExporter/LeadExporter.js
+++ src/apps/leads/src/app/components/LeadExporter/LeadExporter.js
@@ -1,7 +1,10 @@
 import { useDispatch } from "react-redux";
 import cx from "classnames";
 
-import { Search } from "@zesty-io/core/Search";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+
 import { DownloadCSVButton } from "./DownloadCSVButton";
 import { FormGroupSelector } from "./FormGroupSelector";
 import { TableDateFilter } from "./TableDateFilter";
@@ -24,15 +27,24 @@ export function LeadExporter() {
         <DownloadCSVButton />
       </div>
       <div className={cx(styles.filter, styles.SearchEnd)}>
-        <Search
-          className={styles.Search}
-          name="text-filter"
-          placeholder="Search across all of your leads"
-          onChange={(value) => {
-            dispatch(setFilterText(value));
+        <TextField
+          id="filled-search"
+          placeholder="Filter file list by name, zuid or code"
+          type="search"
+          variant="outlined"
+          fullWidth
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon fontSize="small" />
+              </InputAdornment>
+            ),
           }}
-          onSubmit={(value) => {
-            dispatch(setFilterText(value));
+          onChange={(evt) => {
+            let term = evt.target.value;
+            if (term === null) return;
+            dispatch(setFilterText(term));
           }}
         />
       </div>
