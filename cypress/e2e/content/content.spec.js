46b76e1f1711d45f007d9c5efe7192e0cc14cdd8
Andres <agalin920@gmail.com>
2022-07-06 20:51:35 -0700

    comment spacing

--- cypress/e2e/content/content.spec.js
+++ cypress/e2e/content/content.spec.js
@@ -222,8 +222,8 @@ describe("Content Specs", () => {
       cy.get("#12-9b96ec-tll2gn input[type=number]")
         .focus()
         /* 
-        input type='number 'cannot be empty so rather than whitespace, it'd have a value of 0
-        to solve for this {selectall} is used to overwrite value as opposed to clear()
+          input type='number 'cannot be empty so rather than whitespace, it'd have a value of 0
+          to solve for this {selectall} is used to overwrite value as opposed to clear()
         */
         .type("{selectall}")
         .type("999")
