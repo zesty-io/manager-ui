6a697fb2d0b9e25e91e7366a715aa8a97e603f99
Andres <agalin920@gmail.com>
2022-07-28 10:19:04 -0700

    Add leading slashes to cy vists

--- cypress/e2e/reports/activity-log/activity-log.spec.js
+++ cypress/e2e/reports/activity-log/activity-log.spec.js
@@ -2,29 +2,29 @@ import moment from "moment";
 describe("Reports > Activity Log > Home", () => {
   describe("Tabs", () => {
     it("Highlights tabs depending on URL", () => {
-      cy.visit("reports/activity-log/resources");
+      cy.visit("/reports/activity-log/resources");
       cy.get(".MuiTabs-root")
         .contains("RESOURCES")
         .should("have.attr", "aria-selected", "true");
 
-      cy.visit("reports/activity-log/users");
+      cy.visit("/reports/activity-log/users");
       cy.get(".MuiTabs-root")
         .contains("USERS")
         .should("have.attr", "aria-selected", "true");
 
-      cy.visit("reports/activity-log/timeline");
+      cy.visit("/reports/activity-log/timeline");
       cy.get(".MuiTabs-root")
         .contains("TIMELINE")
         .should("have.attr", "aria-selected", "true");
 
-      cy.visit("reports/activity-log/insights");
+      cy.visit("/reports/activity-log/insights");
       cy.get(".MuiTabs-root")
         .contains("INSIGHTS")
         .should("have.attr", "aria-selected", "true");
     });
 
     it("Navigates on tab click", () => {
-      cy.visit("reports/activity-log");
+      cy.visit("/reports/activity-log/resources");
 
       cy.get(".MuiTabs-root").contains("RESOURCES").click();
       cy.location("pathname").should("eq", "/reports/activity-log/resources");
@@ -32,11 +32,11 @@ describe("Reports > Activity Log > Home", () => {
       cy.get(".MuiTabs-root").contains("USERS").click();
       cy.location("pathname").should("eq", "/reports/activity-log/users");
 
-      cy.visit("reports/activity-log/timeline");
+      cy.visit("/reports/activity-log/timeline");
       cy.get(".MuiTabs-root").contains("TIMELINE").click();
       cy.location("pathname").should("eq", "/reports/activity-log/timeline");
 
-      cy.visit("reports/activity-log/insights");
+      cy.visit("/reports/activity-log/insights");
       cy.get(".MuiTabs-root").contains("INSIGHTS").click();
       cy.location("pathname").should("eq", "/reports/activity-log/insights");
     });
@@ -44,7 +44,7 @@ describe("Reports > Activity Log > Home", () => {
 
   describe("Filters", () => {
     it("Sets default date url parameters if none are set", () => {
-      cy.visit("reports/activity-log/resources");
+      cy.visit("/reports/activity-log/resources");
       cy.location("search").should(
         "eq",
         `?from=${moment().add(-6, "days").format("YYYY-MM-DD")}&to=${moment()
@@ -54,7 +54,7 @@ describe("Reports > Activity Log > Home", () => {
     });
 
     it("Does not set default date url parameters if they are set", () => {
-      cy.visit("reports/activity-log/resources?from=2020-07-21&to=2020-07-28");
+      cy.visit("/reports/activity-log/resources?from=2020-07-21&to=2020-07-28");
       cy.location("search").should(
         "not.eq",
         `?from=${moment().add(-6, "days").format("YYYY-MM-DD")}&to=${moment()
@@ -66,7 +66,7 @@ describe("Reports > Activity Log > Home", () => {
     it("Displays all url parameters on filters", () => {
       cy.waitOn("/v1/env/audits*", () => {
         cy.visit(
-          "reports/activity-log/resources?from=2022-07-21&to=2022-07-28&ar&resourceType=content&actionByUserZUID=5-aabe9db189-s0n789&sortBy=happenedAt"
+          "/reports/activity-log/resources?from=2022-07-21&to=2022-07-28&ar&resourceType=content&actionByUserZUID=5-aabe9db189-s0n789&sortBy=happenedAt"
         );
       });
       cy.get('[data-cy="filters"]').within(() => {
@@ -79,7 +79,7 @@ describe("Reports > Activity Log > Home", () => {
     it("Adds and modifies url parameters via filters", () => {
       cy.waitOn("/v1/env/audits*", () => {
         cy.visit(
-          "reports/activity-log/resources?from=2022-07-21&to=2022-07-28"
+          "/reports/activity-log/resources?from=2022-07-21&to=2022-07-28"
         );
       });
       cy.get('[data-cy="filters"]').contains("Sort By").next().click();
@@ -110,7 +110,7 @@ describe("Reports > Activity Log > Home", () => {
     it("Navigates to Resource Detail on Resource Item click", () => {
       cy.waitOn("/v1/env/audits*", () => {
         cy.visit(
-          "reports/activity-log/resources?from=2022-07-21&to=2022-07-28"
+          "/reports/activity-log/resources?from=2022-07-21&to=2022-07-28"
         );
       });
       cy.get(".MuiListItem-root").first().click();
@@ -138,7 +138,7 @@ describe("Reports > Activity Log > Home", () => {
     it("Displays partial Skeletons when changing dates and refetching API", () => {
       cy.waitOn("/v1/env/audits*", () => {
         cy.visit(
-          "reports/activity-log/resources?from=2022-07-21&to=2022-07-28"
+          "/reports/activity-log/resources?from=2022-07-21&to=2022-07-28"
         );
       });
 
@@ -167,24 +167,24 @@ describe("Reports > Activity Log > Home", () => {
 
   describe("Empty State", () => {
     it("Displays Resources Tab empty state", () => {
-      cy.visit("reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
       cy.contains("No Resources Found");
     });
     it("Displays Users Tab empty state", () => {
-      cy.visit("reports/activity-log/users?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/users?from=2099-01-01&to=2099-01-02");
       cy.contains("No Users Found");
     });
     it("Displays Timeline Tab empty state", () => {
-      cy.visit("reports/activity-log/timeline?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/timeline?from=2099-01-01&to=2099-01-02");
       cy.contains("No Logs Found");
     });
     it("Displays Insights Tab empty state", () => {
-      cy.visit("reports/activity-log/insights?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/insights?from=2099-01-01&to=2099-01-02");
       cy.contains("No Insights Found");
     });
 
     it("Resets filters", () => {
-      cy.visit("reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
       cy.contains("RESET FILTERS").click();
       cy.location("search").should(
         "eq",
@@ -200,7 +200,7 @@ describe("Reports > Activity Log > Home", () => {
       cy.intercept("GET", "/v1/env/audits*", {
         statusCode: 500,
       }).as("request");
-      cy.visit("reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
 
       cy.wait("@request");
       cy.contains("Whoops!");
@@ -209,7 +209,7 @@ describe("Reports > Activity Log > Home", () => {
       cy.intercept("GET", "/v1/env/audits*", {
         statusCode: 500,
       }).as("request");
-      cy.visit("reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
+      cy.visit("/reports/activity-log/resources?from=2099-01-01&to=2099-01-02");
 
       cy.wait("@request");
       cy.contains("RETRY").click();
@@ -219,7 +219,7 @@ describe("Reports > Activity Log > Home", () => {
 
   it("Fetches initial data from Audit API with url parameters", () => {
     cy.intercept("GET", "/v1/env/audits*").as("request");
-    cy.visit("reports/activity-log/resources?from=2020-01-01&to=2020-01-02");
+    cy.visit("/reports/activity-log/resources?from=2020-01-01&to=2020-01-02");
     cy.wait("@request")
       .its("request.url")
       .should("include", "start_date")
