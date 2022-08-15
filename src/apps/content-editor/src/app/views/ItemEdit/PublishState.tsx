11a064fd622d6656e8da223ddc3b47b18b21b5e7
Andres <agalin920@gmail.com>
2022-08-15 11:39:43 -0700

    Swap Take offline to cancel

--- src/apps/content-editor/src/app/views/ItemEdit/PublishState.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/PublishState.tsx
@@ -120,7 +120,7 @@ export const PublishState = () => {
                   })
                 }
               >
-                Take Offline
+                Cancel
               </Button>,
             ];
           } else {
