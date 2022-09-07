3dce5776d3a245f58b4076137265fee4a756a28b
Andres Galindo <agalin920@gmail.com>
2022-09-07 15:35:18 -0500

    Media manager rtk query implementation (#1333)

--- src/shell/services/types.ts
+++ src/shell/services/types.ts
@@ -11,3 +11,51 @@ export interface Publishing {
   versionZUID: string;
   _active: boolean;
 }
+
+export interface Bin {
+  id: string;
+  name: string;
+  site_id?: string;
+  eco_id?: string;
+  storage_driver: string;
+  storage_name: string;
+  storage_base_url: string;
+  cdn_driver: string;
+  cdn_base_url: string;
+  created_at: Date;
+  deleted_at?: Date;
+  deleted_from_storage_at?: Date;
+  default: boolean;
+}
+
+export interface GroupData {
+  id: string;
+  bin_id: string;
+  group_id: string;
+  name: string;
+  storage_driver: string;
+  storage_name: string;
+  groups: Group[];
+  files: File[];
+}
+
+export interface Group {
+  id: string;
+  bin_id: string;
+  group_id: string;
+  name: string;
+}
+
+export interface File {
+  id: string;
+  bin_id: string;
+  group_id: string;
+  filename: string;
+  title: string;
+  url: string;
+  created_by?: Date;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at?: Date;
+  deleted_from_storage_at?: Date;
+}
