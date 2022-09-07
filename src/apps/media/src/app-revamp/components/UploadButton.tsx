dabd9e6541509bf40bb9cbb0ed4bbb5f27c68fd7
Ryan Pals <ryan.pals@zesty.io>
2022-09-07 16:45:35 -0500

    WIP

--- src/apps/media/src/app-revamp/components/UploadButton.tsx
+++ src/apps/media/src/app-revamp/components/UploadButton.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Button from "@mui/material/Button";
 import FileUpload from "@mui/icons-material/FileUpload";
 import { Typography } from "@mui/material";
+import Pic from "../downloading.svg";
 
 export type UploadButton = {
   onClick: () => void;
