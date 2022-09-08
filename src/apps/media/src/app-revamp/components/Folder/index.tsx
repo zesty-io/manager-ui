6c51e5139ccd4b8c70bac339baecbfa9ab753fbb
JacobDiazCruz <jacob.cruz@pictureworks.com.au>
2022-09-09 07:47:22 +0800

    Added: Folder component

---
+++ src/apps/media/src/app-revamp/components/Folder/index.tsx
@@ -0,0 +1,37 @@
+import { Typography, Button } from "@mui/material";
+import { useHistory } from "react-router-dom";
+import FolderIcon from "@mui/icons-material/Folder";
+
+interface FolderProps {
+  name: string;
+  path: string;
+}
+
+export const Folder: React.FC<FolderProps> = ({ name, path }) => {
+  const history = useHistory();
+
+  return (
+    <Button
+      startIcon={<FolderIcon />}
+      variant="outlined"
+      sx={{
+        color: "grey.500",
+        borderColor: "grey.100",
+      }}
+      onClick={() => {
+        history.push(path);
+      }}
+    >
+      <Typography
+        variant="caption"
+        sx={{
+          overflow: "hidden",
+          whiteSpace: "nowrap",
+          textOverflow: "ellipsis",
+        }}
+      >
+        {name}
+      </Typography>
+    </Button>
+  );
+};
