d8d97bb82abeb075fcd74696c9b37a768ccc9b86
Ryan Pals <ryan.pals@zesty.io>
2022-09-08 10:52:59 -0500

    Dam folder empty state (#1341)
    * Revert "Reset FolderMedia"
    
    This reverts commit 3e1b44df907294e7644df5d655020ab985811482.
    
    * Add empty state for bins & queries to support

--- src/apps/media/src/app-revamp/views/FolderMedia.tsx
+++ src/apps/media/src/app-revamp/views/FolderMedia.tsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router";
+import { EmptyState } from "../components/EmptyState";
+import { mediaManagerApi } from "../../../../../shell/services/mediaManager";
+
+type Params = { binId: string };
+
 export const FolderMedia = () => {
-  return <div>FOLDER VIEW</div>;
+  const params = useParams<Params>();
+  // TODO assumes that it is a bin (i.e. not group)
+  const { binId } = params;
+  // TODO potentially provide user feedback for an invalid binID
+  const { data: files } = mediaManagerApi.useGetBinFilesQuery(binId);
+
+  console.log(`FILES FOR BIN ${binId}`, files);
+
+  if (files && files.length === 0) return <EmptyState />;
+
+  return <div> Bin View </div>;
 };
