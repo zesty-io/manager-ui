143930ff89a21dc4d2649b377a056755accb65ec
Carl Jacob Cruz <40680430+JacobDiazCruz@users.noreply.github.com>
2022-09-20 11:28:39 +0800

    Added: Thumbnail Docfile extensions (#1365)
    Co-authored-by: JacobDiazCruz <jacob.cruz@pictureworks.com.au>

---
+++ src/apps/media/src/app-revamp/components/Thumbnail/ThumbnailContent.tsx
@@ -0,0 +1,66 @@
+import { FC } from "react";
+import { TextField, Typography, CardContent, Box } from "@mui/material";
+
+interface Props {
+  extension: string;
+  backgroundColor: string;
+  color: string;
+  filename: string;
+  onFilenameChange?: (value: string) => void;
+  isEditable?: boolean;
+}
+
+export const ThumbnailContent: FC<Props> = ({
+  extension,
+  filename,
+  backgroundColor,
+  color,
+  onFilenameChange,
+  isEditable,
+}) => {
+  const styledCardContent = {
+    px: 1,
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+  };
+
+  const styledBadge = {
+    px: 1,
+    py: 0.4,
+    borderRadius: "4px",
+    textTransform: "uppercase",
+    position: "absolute",
+    right: 15,
+    transform: "translateY(-120%)",
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          ...styledBadge,
+          backgroundColor,
+          color,
+        }}
+      >
+        {extension}
+      </Box>
+      <CardContent sx={styledCardContent}>
+        {isEditable ? (
+          <TextField
+            value={filename}
+            size="small"
+            variant="outlined"
+            color="primary"
+            InputProps={{ sx: { flex: 1 } }}
+            fullWidth
+            onChange={(e) => onFilenameChange(e.target.value)}
+          />
+        ) : (
+          <Typography variant="caption">{filename}</Typography>
+        )}
+      </CardContent>
+    </>
+  );
+};
