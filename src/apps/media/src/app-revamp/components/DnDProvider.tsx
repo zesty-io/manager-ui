c7551c365e38338c424dc61fee89b88892743002
Carl Cruz <carlcruz@192.168.254.114>
2022-10-04 22:51:30 +0800

    Added: file modal extension previews

--- src/apps/media/src/app-revamp/components/DnDProvider.tsx
+++ src/apps/media/src/app-revamp/components/DnDProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { useDropzone } from "react-dropzone";
 import { useCallback } from "react";
 import { uploadFile } from "../../../../../shell/store/media-revamp";
