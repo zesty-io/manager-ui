a44162c0f60138010275a469cc55a53f817ddfda
Carl Jacob Cruz <40680430+JacobDiazCruz@users.noreply.github.com>
2022-10-05 07:45:52 +0800

    Hotfix: dnd component error (#1399)

--- src/apps/media/src/app-revamp/components/DnDProvider.tsx
+++ src/apps/media/src/app-revamp/components/DnDProvider.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { useDropzone } from "react-dropzone";
 import { useCallback } from "react";
 import { uploadFile } from "../../../../../shell/store/media-revamp";
 import { useDispatch } from "react-redux";
 import Box from "@mui/material/Box";
 
-export const DnDProvider: FC = (children: any) => {
+export const DnDProvider: FC = ({ children }) => {
   const dispatch = useDispatch();
   const onDrop = useCallback((acceptedFiles: File[]) => {
     console.log({ acceptedFiles });
