f83da78b047ef00698491779bc8ad70ced0ce9e9
Andres <agalin920@gmail.com>
2022-10-09 22:07:20 -0700

    Updated empty upload state

--- src/apps/media/src/app-revamp/components/EmptyState.tsx
+++ src/apps/media/src/app-revamp/components/EmptyState.tsx
@@ -1,17 +1,30 @@
-import { Typography } from "@mui/material";
-import { Stack } from "@mui/material";
-import { FC } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import uploadImage from "../../../../../../public/images/uploadImage.png";
 import { UploadButton } from "./UploadButton";
 
-type Props = {};
-export const EmptyState: FC<Props> = ({}) => {
+export const EmptyState = () => {
   return (
-    <Stack sx={{ gap: 2, justifyContent: "center", height: "100%", px: 4 }}>
-      <Typography variant="h4">Start Uploading Now</Typography>
-      <Typography variant="body2">
-        You can drag and drop files here or use the "Upload" button.
-      </Typography>
-      <UploadButton />
-    </Stack>
+    <Box
+      display="flex"
+      alignItems="center"
+      height="100%"
+      sx={{ px: 4 }}
+      justifyContent="center"
+    >
+      <Stack direction="row" spacing={8} alignItems="center">
+        <Box>
+          <Typography variant="h4" fontWeight={600} sx={{ mb: 1 }}>
+            Start Uploading Now
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
+            You can drag and drop files here or use the "Upload" button.
+          </Typography>
+          <UploadButton />
+        </Box>
+        <Box>
+          <img src={uploadImage} width={440} />
+        </Box>
+      </Stack>
+    </Box>
   );
 };
