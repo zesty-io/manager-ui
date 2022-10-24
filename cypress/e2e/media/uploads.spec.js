10cc0755df2ed1baf716743da961ab0e7e165ea5
Ryan Pals <ryan.pals@zesty.io>
2022-10-24 16:48:33 -0500

    Add cypress tests for basic upload flow

---
+++ cypress/e2e/media/uploads.spec.js
@@ -0,0 +1,214 @@
+describe("Media uploads", () => {
+  before(() => {
+    cy.waitOn("*groups*", () => {
+      cy.visit("/media");
+      //cy.visit("/media/2-eaaaca5-p1nggr")
+    });
+  });
+
+  it("uploads a file to a All Media", () => {
+    const fileName = `cypress_upload_test_${Math.floor(
+      Math.random() * 1_000_000
+    )}.txt`;
+    cy.get("input[type=file]").selectFile(
+      {
+        contents: Cypress.Buffer.from(
+          "This is a dummy file for Cypress tests of media uploads"
+        ),
+        fileName,
+        mimeType: "text/plain",
+        lastModified: Date.now(),
+      },
+      {
+        // force:true is valid because we use hidden file inputs to do uploads
+        force: true,
+      }
+    );
+    // Click "Done" button to close upload modal
+    cy.get('button:enabled:contains("Done")').click();
+    // TODO why does this need to be done?
+    // Page needs to be refreshed after a second because
+    // Invalidation happens too early
+    cy.wait(1_000);
+    cy.reload();
+    // Assert file exists
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).should("exist");
+    // CLEANUP
+    // Click filename to open file modal
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).click();
+    // Click delete button
+    cy.get('[data-testid="DeleteIcon"]').click();
+    // Click delete confirmation
+    cy.get('button:enabled:contains("Delete")').click();
+  });
+
+  it("uploads a file to a folder", () => {
+    cy.visit("/media/2-eaaaca5-p1nggr");
+    cy.intercept("*instance*").as("instance");
+    cy.intercept("*groups*").as("groups");
+    cy.intercept("*bins*").as("bins");
+    cy.intercept("*bin/1-6c9618c-r26pt").as("binZuid");
+    cy.intercept("*group/2-eaaaca5-p1nggr").as("groupZuid");
+    cy.wait("@groups")
+      .wait("@bins")
+      .wait("@groupZuid")
+      .wait("@binZuid")
+      .wait("@instance");
+    const fileName = `cypress_upload_test_${Math.floor(
+      Math.random() * 1_000_000
+    )}.txt`;
+    cy.get("input[type=file]")
+      .first()
+      .selectFile(
+        {
+          contents: Cypress.Buffer.from(
+            "This is a dummy file for Cypress tests of media uploads"
+          ),
+          fileName,
+          mimeType: "text/plain",
+          lastModified: Date.now(),
+        },
+        {
+          // force:true is valid because we use hidden file inputs to do uploads
+          force: true,
+        }
+      );
+    // Click "Done" button to close upload modal
+    cy.get('button:enabled:contains("Done")').click();
+    // TODO why does this need to be done?
+    // Page needs to be refreshed after a second because
+    // Invalidation happens too early
+    cy.wait(1_000);
+    cy.reload();
+    // Assert file exists
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).should("exist");
+    // CLEANUP
+    // Click filename to open file modal
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).click();
+    // Click delete button
+    cy.get('[data-testid="DeleteIcon"]').click();
+    // Click delete confirmation
+    cy.get('button:enabled:contains("Delete")').click();
+  });
+
+  it("uploads a file to a bin", () => {
+    cy.visit("/media/1-6c9618c-r26pt");
+    cy.intercept("*instance*").as("instance");
+    cy.intercept("*groups*").as("groups");
+    cy.intercept("*bins*").as("bins");
+    cy.intercept("*bin/1-6c9618c-r26pt").as("binZuid");
+    cy.intercept("*bin/1-6c9618c-r26pt/files").as("binFiles");
+    cy.wait("@groups")
+      .wait("@bins")
+      .wait("@binZuid")
+      .wait("@instance")
+      .wait("@binFiles");
+    const fileName = `cypress_upload_test_${Math.floor(
+      Math.random() * 1_000_000
+    )}.txt`;
+    //cy.wait(10_000)
+    cy.get("input[type=file]")
+      .first()
+      .selectFile(
+        {
+          contents: Cypress.Buffer.from(
+            "This is a dummy file for Cypress tests of media uploads"
+          ),
+          fileName,
+          mimeType: "text/plain",
+          lastModified: Date.now(),
+        },
+        {
+          // force:true is valid because we use hidden file inputs to do uploads
+          force: true,
+        }
+      );
+    // Click "Done" button to close upload modal
+    cy.get('button:enabled:contains("Done")').click();
+    // TODO why does this need to be done?
+    // Page needs to be refreshed after a second because
+    // Invalidation happens too early
+    cy.wait(1_000);
+    cy.reload();
+    // Assert file exists
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).should("exist");
+    // CLEANUP
+    // Click filename to open file modal
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).click();
+    // Click delete button
+    cy.get('[data-testid="DeleteIcon"]').click();
+    // Click delete confirmation
+    cy.get('button:enabled:contains("Delete")').click();
+  });
+
+  it("uploads a file via drag 'n drop", () => {
+    cy.visit("/media/2-eaaaca5-p1nggr");
+    cy.intercept("*instance*").as("instance");
+    cy.intercept("*groups*").as("groups");
+    cy.intercept("*bins*").as("bins");
+    cy.intercept("*bin/1-6c9618c-r26pt").as("binZuid");
+    cy.intercept("*group/2-eaaaca5-p1nggr").as("groupZuid");
+    cy.wait("@groups")
+      .wait("@bins")
+      .wait("@groupZuid")
+      .wait("@binZuid")
+      .wait("@instance");
+    const fileName = `cypress_upload_test_${Math.floor(
+      Math.random() * 1_000_000
+    )}.txt`;
+    cy.get("input[type=file]")
+      .first()
+      .selectFile(
+        {
+          contents: Cypress.Buffer.from(
+            "This is a dummy file for Cypress tests of media uploads"
+          ),
+          fileName,
+          mimeType: "text/plain",
+          lastModified: Date.now(),
+        },
+        {
+          // TODO is this a sufficient test of drag-n-drop?
+          subject: "drag-n-drop",
+          // force:true is valid because we use hidden file inputs to do uploads
+          force: true,
+        }
+      );
+    // Click "Done" button to close upload modal
+    cy.get('button:enabled:contains("Done")').click();
+    // TODO why does this need to be done?
+    // Page needs to be refreshed after a second because
+    // Invalidation happens too early
+    cy.wait(1_000);
+    cy.reload();
+    // Assert file exists
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).should("exist");
+    // CLEANUP
+    // Click filename to open file modal
+    cy.get(`div.MuiCardContent-root:contains("${fileName}")`).click();
+    // Click delete button
+    cy.get('[data-testid="DeleteIcon"]').click();
+    // Click delete confirmation
+    cy.get('button:enabled:contains("Delete")').click();
+  });
+
+  it.skip("displays upload message when dragging a file into the grid", () => {
+    cy.visit("/media/2-eaaaca5-p1nggr");
+    cy.intercept("*instance*").as("instance");
+    cy.intercept("*groups*").as("groups");
+    cy.intercept("*bins*").as("bins");
+    cy.intercept("*bin/1-6c9618c-r26pt").as("binZuid");
+    cy.intercept("*group/2-eaaaca5-p1nggr").as("groupZuid");
+    cy.wait("@groups")
+      .wait("@bins")
+      .wait("@groupZuid")
+      .wait("@binZuid")
+      .wait("@instance");
+    const fileName = `cypress_upload_test_${Math.floor(
+      Math.random() * 1_000_000
+    )}.txt`;
+    cy.get('[data-testid="dnd-provider-box"]')
+      .trigger("dragover")
+      .contains("Upload files to");
+  });
+});
