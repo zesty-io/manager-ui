3b3a89ddf7bed199160d3812038d6247b8923080
Ryan Pals <ryan.pals@zesty.io>
2022-10-31 18:33:45 -0500

    Refactor from <Select> to <Button> with dropdown

--- src/apps/media/src/app-revamp/components/Controls/Controls.tsx
+++ src/apps/media/src/app-revamp/components/Controls/Controls.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { Button, Box, Select, InputLabel, MenuItem } from "@mui/material";
+import { FC, useState } from "react";
+import { Button, Box, Select, InputLabel, MenuItem, Menu } from "@mui/material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { useSelector, useDispatch } from "react-redux";
 import { AppState } from "../../../../../../shell/store/types";
@@ -13,22 +13,40 @@ export const Controls: FC = () => {
   const sortOrder = useSelector(
     (state: AppState) => state.mediaRevamp.sortOrder
   );
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleChange = (sortOrder: MediaSortOrder) => {
+    dispatch(setSortOrder(sortOrder));
+    handleClose();
+  };
 
   return (
-    <Box>
-      <Button endIcon={<ArrowDropDownIcon />}>Sort by</Button>
-      <InputLabel id="media-sort-by">Sort by</InputLabel>
-      <Select
-        labelId="media-sort-by"
-        value={sortOrder}
-        onChange={(evt) =>
-          dispatch(setSortOrder(evt.target.value as MediaSortOrder))
-        }
-      >
-        <MenuItem value="createdDesc">Date Added</MenuItem>
-        <MenuItem value="alphaAsc">Name (A to Z)</MenuItem>
-        <MenuItem value="alphaDesc">Name (Z to A)</MenuItem>
-      </Select>
+    <Box
+      sx={{
+        height: "64px",
+        padding: "12px, 24px, 12px, 24px",
+      }}
+    >
+      <Button endIcon={<ArrowDropDownIcon />} onClick={handleClick}>
+        Sort by
+      </Button>
+      <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
+        <MenuItem onClick={() => handleChange("createdDesc")}>
+          Date Added
+        </MenuItem>
+        <MenuItem onClick={() => handleChange("alphaAsc")}>
+          Name (A to Z)
+        </MenuItem>
+        <MenuItem onClick={() => handleChange("alphaDesc")}>
+          Name (Z to A)
+        </MenuItem>
+      </Menu>
     </Box>
   );
 };
