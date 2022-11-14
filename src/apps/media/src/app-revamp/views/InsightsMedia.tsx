458020c8965130f05957ed3066f6ae5deec4b9c6
Carl Cruz <carlcruz@192.168.254.104>
2022-11-14 22:13:34 +0800

    Added: insights media metrics UI

--- src/apps/media/src/app-revamp/views/InsightsMedia.tsx
+++ src/apps/media/src/app-revamp/views/InsightsMedia.tsx
@@ -1,4 +1,13 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card } from "@mui/material";
+
+const cardStyle = {
+  p: 2,
+  mr: 2,
+  width: "220px",
+  border: "1px solid",
+  borderColor: "grey.100",
+  borderRadius: "8px",
+};
 
 export const InsightsMedia = () => {
   return (
@@ -16,6 +25,32 @@ export const InsightsMedia = () => {
       <Typography variant="h4" fontWeight={600}>
         Insights
       </Typography>
+      <Box sx={{ display: "flex", height: "134px", my: 2 }}>
+        <Box sx={cardStyle}>
+          <Typography
+            variant="h6"
+            sx={{ color: "text.secondary" }}
+            fontWeight={600}
+          >
+            Media Requests
+          </Typography>
+          <Typography variant="h2" sx={{ mt: 1 }} fontWeight={600}>
+            120K
+          </Typography>
+        </Box>
+        <Box sx={cardStyle}>
+          <Typography
+            variant="h6"
+            sx={{ color: "text.secondary" }}
+            fontWeight={600}
+          >
+            Media Bandwidth
+          </Typography>
+          <Typography variant="h2" sx={{ mt: 1 }} fontWeight={600}>
+            120K
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 };
