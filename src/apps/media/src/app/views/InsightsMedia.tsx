d02ff0f43d2a37ca18a1a58cca47f9d6cef16c29
Carl Jacob Cruz <40680430+JacobDiazCruz@users.noreply.github.com>
2022-11-16 07:31:08 +0800

    Feature/dam insights total request and bandwidth (#1530)
    * Updated: insights routes and navigation
    
    * Added: insights media metrics UI
    
    * Added: metrics card in insightsMedia
    
    * Updated: media requests and bandwidth UI
    
    * Added: total requests and bandwidth tooltip and formatter
    
    * Remove: metric loader components
    
    * Remove: format number with commas
    
    * Update: Insights end date format
    
    * Updated: metrics icon styles
    
    * Added: metric card default value
    
    * Remove: redundant variables
    
    * Adjusted: metric card height
    
    * Remove: insights prop ts
    
    * Update: date param type
    
    * Metric card updates
    
    * Design changes
    
    * resolve type error and remove ignore
    
    * Additional error handling
    
    Co-authored-by: Carl Cruz <carlcruz@192.168.254.104>
    Co-authored-by: Andres <agalin920@gmail.com>

--- src/apps/media/src/app/views/InsightsMedia.tsx
+++ src/apps/media/src/app/views/InsightsMedia.tsx
@@ -1,6 +1,57 @@
-import { Box, Typography } from "@mui/material";
+import { FC, useState, useMemo } from "react";
+import { Box, Typography, Card } from "@mui/material";
+import {
+  useGetUsageQuery,
+  useGetRequestsQuery,
+} from "../../../../../shell/services/metrics";
 
-export const InsightsMedia = () => {
+import { MetricCard } from "../../../../../shell/components/MetricsCard";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import CloudRoundedIcon from "@mui/icons-material/CloudRounded";
+import moment from "moment";
+
+const iconStyles = {
+  height: "32px",
+  borderRadius: "16px",
+  padding: 1,
+};
+
+const getDates = (numDays: number) => {
+  const start = new Date();
+  start.setDate(start.getDate() - numDays);
+
+  const end = moment().subtract(30, "days").toISOString();
+
+  return { start, end };
+};
+
+export const InsightsMedia: FC = () => {
+  const [timePeriod, setTimePeriod] = useState(30);
+  const { start, end } = getDates(timePeriod);
+  const dates = useMemo(() => {
+    return [start.toISOString(), end];
+  }, [timePeriod]);
+
+  const {
+    data: usageData,
+    isLoading: usageLoading,
+    error: usageError,
+  } = useGetUsageQuery(dates as [string, string]);
+  const {
+    data: requestData,
+    isLoading: requestsLoading,
+    error: requestError,
+  } = useGetRequestsQuery(dates as [string, string]);
+
+  const totalMediaThroughput = usageData?.MediaConsumption.TotalGBs;
+  const totalMediaRequests = usageData?.MediaConsumption.TotalRequests;
+
+  const totalRequestThroughput = requestData?.TotalThroughputGB;
+  const totalPageRequests = requestData?.TotalRequests;
+
+  const totalRequests = totalPageRequests + totalMediaRequests;
+  const totalThroughput = totalMediaThroughput + totalRequestThroughput;
+
   return (
     <Box
       component="main"
@@ -13,9 +64,53 @@ export const InsightsMedia = () => {
         px: 3,
       }}
     >
-      <Typography variant="h4" fontWeight={600}>
-        Insights
-      </Typography>
+      <Box sx={{ height: "64px" }}>
+        <Typography variant="h4" fontWeight={600}>
+          Insights
+        </Typography>
+      </Box>
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <MetricCard
+          title="Media Requests"
+          value={totalRequests || 0}
+          icon={
+            <Box
+              sx={{
+                ...iconStyles,
+                backgroundColor: "blue.50",
+              }}
+            >
+              <SendRoundedIcon
+                color="info"
+                sx={{
+                  width: "16px",
+                  height: "16px",
+                }}
+              />
+            </Box>
+          }
+          deltaLabel={"VS PRIOR 30 DAYS"}
+        />
+        <MetricCard
+          title="Media Bandwidth"
+          value={totalThroughput || 0}
+          symbol="GB"
+          icon={
+            <Box
+              sx={{
+                ...iconStyles,
+                backgroundColor: "deepOrange.50",
+              }}
+            >
+              <CloudRoundedIcon
+                color="primary"
+                sx={{ width: "16px", height: "16px", color: "primary" }}
+              />
+            </Box>
+          }
+          deltaLabel={"VS PRIOR 30 DAYS"}
+        />
+      </Box>
     </Box>
   );
 };
