e2f091bdf669daee85e4aba6e452042b1d4b7ce5
Andres <agalin920@gmail.com>
2022-11-15 21:11:53 -0800

    ts expect error

--- src/apps/home/app/components/Header.tsx
+++ src/apps/home/app/components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
         px: 3,
         py: 2,
         background: (theme) =>
-          // @ts-ignore
+          // @ts-expect-error - missing module augmentations
           `linear-gradient(91.57deg, ${theme.palette.deepOrange?.[500]} 0%, ${theme.palette.deepOrange?.[600]} 100%)`,
       }}
     >
