bf436c24c58d60300b8f2b18392880cc94783408
Andres <agalin920@gmail.com>
2022-11-17 10:42:54 -0800

    cypress tests

---
+++ cypress/e2e/home/mature.spec.js
@@ -0,0 +1,62 @@
+describe("Mature Home", () => {
+  before(() => {
+    cy.visit("/");
+  });
+  it("Opens Quick Start Guide in new tab", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.contains("Get Started").click();
+    cy.get("@open").should(
+      "have.been.calledOnceWith",
+      "https://zesty.org/quick-start-guide"
+    );
+  });
+  it("Opens Platform Docs in new tab ", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.contains("Platform Docs").click();
+    cy.get("@open").should("have.been.calledOnceWith", "https://zesty.org/");
+  });
+  it("Opens Instance API Docs in new tab ", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.contains("Instance API Docs").click();
+    cy.get("@open").should(
+      "have.been.calledOnceWith",
+      "https://instances-api.zesty.org/"
+    );
+  });
+  it("Opens GraphQL Docs in new tab ", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.contains("GraphQL Docs").click();
+    cy.get("@open").should(
+      "have.been.calledOnceWith",
+      "https://github.com/zesty-io/graphql-zesty"
+    );
+  });
+  it("Opens Parsley Docs in new tab ", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.contains("Parsley Docs").click();
+    cy.get("@open").should(
+      "have.been.calledOnceWith",
+      "https://parsley.zesty.io/"
+    );
+  });
+  it("Opens Release Notes in new tab ", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.contains("Release Notes").click();
+    cy.get("@open").should(
+      "have.been.calledOnceWith",
+      "https://www.zesty.io/mindshare/product-announcements"
+    );
+  });
+});
