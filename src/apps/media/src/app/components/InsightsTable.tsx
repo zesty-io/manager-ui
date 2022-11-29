cc8c12d9b503e074a2e9eb073d144aafc30d7d56
Carl Jacob Cruz <40680430+JacobDiazCruz@users.noreply.github.com>
2022-11-30 04:14:50 +0800

    Feature/dam insights percentage (#1549)
    * Update: insights percentage prerequisites
    
    * Added: metric card delta props
    
    * Update: percentage icons for insights page
    
    * Remove: redundant variable
    
    * Remove: old percentage media icons
    
    * Hotfix: metric card
    
    Co-authored-by: Carl Cruz <carlcruz@192.168.254.104>

---
+++ src/apps/media/src/app/components/InsightsTable.tsx
@@ -0,0 +1,190 @@
+import { FC, useState, useMemo, useRef } from "react";
+import {
+  Box,
+  Typography,
+  Card,
+  Chip,
+  CardMedia,
+  IconButton,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import {
+  useGetUsageQuery,
+  useGetRequestsQuery,
+} from "../../../../../shell/services/metrics";
+import { DataGridPro, GridValueGetterParams } from "@mui/x-data-grid-pro";
+import { File, Bin } from "../../../../../shell/services/types";
+import fileBroken from "../../../../../../public/images/fileBroken.jpg";
+import { useHistory, useLocation } from "react-router-dom";
+import { numberFormatter } from "../../../../../utility/numberFormatter";
+
+import {
+  fileExtension,
+  fileTypeToColor,
+  getExtensions,
+  getDateFilterFn,
+} from "../utils/fileUtils";
+import { AppState } from "../../../../../shell/store/types";
+import {
+  useGetAllBinFilesQuery,
+  useGetBinsQuery,
+} from "../../../../../shell/services/mediaManager";
+import LinkRoundedIcon from "@mui/icons-material/LinkRounded";
+import CheckIcon from "@mui/icons-material/Check";
+
+interface Props {
+  files?: any;
+}
+
+export const InsightsTable: FC<Props> = ({ files }) => {
+  // Thumbnail prerequisites
+  const imageEl = useRef<HTMLImageElement>();
+  const [imageOrientation, setImageOrientation] = useState<string>("");
+  const [lazyLoading, setLazyLoading] = useState(true);
+  const [isImageError, setIsImageError] = useState(false);
+
+  const location = useLocation();
+  const history = useHistory();
+
+  const columns = [
+    {
+      field: "filename",
+      headerName: "Name",
+      sortable: false,
+      flex: 4,
+      renderCell: (params: any) => {
+        const [isImageError, setIsImageError] = useState(false);
+
+        const handleImageError = () => {
+          setIsImageError(true);
+        };
+
+        return (
+          <Box sx={{ display: "flex" }}>
+            <CardMedia
+              component="img"
+              // Note: Remove for now since id is not yet defined in the files
+              // key={params.row.id}
+              onError={handleImageError}
+              data-src={params.row.FullPath}
+              image={isImageError ? fileBroken : params.row.FullPath}
+              loading="lazy"
+              sx={{
+                objectFit: "fill",
+                width: "52px",
+                height: "52px",
+              }}
+            />
+            <Box sx={{ display: "flex", alignItems: "center", ml: 3 }}>
+              <Typography variant="body2">{params.row.FileName}</Typography>
+            </Box>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "Requests",
+      headerName: "Requests",
+      flex: 1,
+      sortable: false,
+    },
+    {
+      field: "ThroughtputGB",
+      headerName: "Bandwidth",
+      flex: 1,
+      sortable: false,
+      renderCell: (params: any) => {
+        return (
+          <Typography>
+            {numberFormatter.format(params.row.ThroughtputGB)} GB
+          </Typography>
+        );
+      },
+    },
+    {
+      field: "type",
+      headerName: "Type",
+      sortable: false,
+      renderCell: (params: any) => {
+        return (
+          <Chip
+            label={fileExtension(params.row.FileName)}
+            sx={{
+              textTransform: "uppercase",
+              backgroundColor: `${fileTypeToColor(
+                fileExtension(params.row.FileName)
+              )}.100`,
+              color: `${fileTypeToColor(
+                fileExtension(params.row.FileName)
+              )}.600`,
+            }}
+            size="small"
+          />
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "",
+      sortable: false,
+      renderCell: (params: any) => {
+        const [isCopied, setIsCopied] = useState(false);
+
+        const handleCopyClick = (data: string) => {
+          navigator?.clipboard
+            ?.writeText(data)
+            .then(() => {
+              setIsCopied(true);
+              setTimeout(() => {
+                setIsCopied(false);
+              }, 1500);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        };
+
+        return (
+          <IconButton
+            onClick={(evt: any) => {
+              evt.stopPropagation();
+              handleCopyClick(params.row.FullPath);
+            }}
+          >
+            {isCopied ? (
+              <CheckIcon sx={{ color: "grey.400" }} />
+            ) : (
+              <LinkRoundedIcon sx={{ color: "grey.400" }} />
+            )}
+          </IconButton>
+        );
+      },
+    },
+  ];
+
+  const handleClick = (params: any) => {
+    const locationParams = new URLSearchParams(location.search);
+    // Note: Remove for now since id is not yet defined in the files data
+    // locationParams.set("fileId", params.row.id);
+    // history.replace({
+    //   pathname: location.pathname,
+    //   search: locationParams.toString(),
+    // });
+  };
+
+  return (
+    <Box sx={{ height: "calc(100vh - 289px)", mt: 2 }}>
+      {files && (
+        <DataGridPro
+          columns={columns}
+          rows={files}
+          rowHeight={52}
+          hideFooter
+          disableColumnFilter
+          disableColumnMenu
+          onRowClick={handleClick}
+        />
+      )}
+    </Box>
+  );
+};
