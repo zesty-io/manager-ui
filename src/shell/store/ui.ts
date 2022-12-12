39fa81f32be84ca94c1432124e7c83d89633c520
Andres <agalin920@gmail.com>
2022-12-12 10:29:19 -0800

    Rename home to launch pad

--- src/shell/store/ui.ts
+++ src/shell/store/ui.ts
@@ -16,7 +16,7 @@ import {
   faImage,
   faAddressCard,
   IconDefinition,
-  faHome,
+  faRocket,
 } from "@fortawesome/free-solid-svg-icons";
 import { isEqual } from "lodash";
 
@@ -173,9 +173,9 @@ export function createTab(
   const tab: Tab = { pathname: path, search };
 
   const appNameMap = {
-    home: {
-      name: "Home",
-      icon: faHome,
+    launchpad: {
+      name: "Launchpad",
+      icon: faRocket,
     },
     seo: {
       name: "SEO",
