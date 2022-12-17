1afc66f3d65e3223b3dd59408b075e8472f82952
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2022-12-17 10:56:52 +0800

    Added: user invite button

--- src/shell/components/global-sidebar/GlobalSidebar.js
+++ src/shell/components/global-sidebar/GlobalSidebar.js
@@ -5,6 +5,7 @@ import { Box, ThemeProvider, IconButton } from "@mui/material";
 import GlobalMenu from "shell/components/global-menu";
 import GlobalCustomApps from "shell/components/global-custom-apps";
 import GlobalActions from "shell/components/global-actions";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import fullZestyLogo from "../../../../public/images/fullZestyLogo.svg";
 import zestyLogo from "../../../../public/images/zestyLogo.svg";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
@@ -87,7 +88,25 @@ export default connect((state) => {
           </IconButton>
           <GlobalMenu openNav={props.ui.openNav} />
           <GlobalCustomApps openNav={props.ui.openNav} />
-          <GlobalActions hash={props.instance.randomHashID} />
+          {/* <GlobalActions hash={props.instance.randomHashID} /> */}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+              minHeight: "200px",
+            }}
+          >
+            <IconButton
+              sx={{
+                backgroundColor: "grey.800",
+                borderRadius: "4px",
+                height: "26px",
+                width: "32px",
+              }}
+            >
+              <GroupAddIcon fontSize="small" sx={{ color: "grey.500" }} />
+            </IconButton>
+          </Box>
         </div>
       </aside>
     </ThemeProvider>
