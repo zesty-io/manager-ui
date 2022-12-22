51ebf6760185fa9dea532a30db3dddff94e06475
Nar Cuenca <narc.ph@gmail.com>
2022-12-22 10:20:58 +0800

    test: updated tabs tests

--- cypress/e2e/settings/tabs.spec.js
+++ cypress/e2e/settings/tabs.spec.js
@@ -102,7 +102,8 @@ describe("Tabs actions", () => {
       .find('[data-testid="PushPinOutlinedIcon"]')
       .should("not.exist");
 
-    cy.wait(250);
+    // Small delay to allow the pinned tabs to be loaded properly
+    cy.wait(500);
 
     // Pin tab #2 and ensure it is pinned
     cy.waitOn("/code/file/views/11-bc386c-gj560c", () => {
@@ -117,7 +118,7 @@ describe("Tabs actions", () => {
       .find('[data-testid="PushPinIcon"]')
       .should("exist");
 
-    cy.wait(250);
+    cy.wait(500);
 
     // Pin tab #3 and ensure it is pinned
     cy.waitOn("/code/file/views/11-d10260-6t3jg6", () => {
@@ -132,7 +133,7 @@ describe("Tabs actions", () => {
       .find('[data-testid="PushPinIcon"]')
       .should("exist");
 
-    cy.wait(250);
+    cy.wait(500);
 
     // Pin tab #4 and ensure it is pinned
     cy.waitOn("/code/file/views/11-77b344-rflphh", () => {
@@ -147,7 +148,7 @@ describe("Tabs actions", () => {
       .find('[data-testid="PushPinIcon"]')
       .should("exist");
 
-    cy.wait(250);
+    cy.wait(500);
 
     // Pin tab #5 and ensure it is pinned
     cy.waitOn("/code/file/views/11-6bb028-b65lkz", () => {
@@ -178,5 +179,34 @@ describe("Tabs actions", () => {
     // Resize the viewport and assert that the dropdown is gone
     cy.viewport(3440, 720);
     cy.get('[data-cy="TabsDropdownButton"').should("not.exist");
+  });
+
+  it("should put an active tab to the left when screen is resized to prevent putting it to the dropdown menu", () => {
+    // Wait for all the tabs to be rendered properly
+    cy.wait(500);
+
+    cy.waitOn("/code/file/views/11-e55790-f19nwx", () => {
+      cy.visit("/code/file/views/11-e55790-f19nwx");
+    });
+    cy.viewport(1024, 720);
+
+    // Ensure that the tab was moved
+    cy.get('[data-cy="ActiveTab"]').should("have.text", "customtype/bevs.csv");
+  });
+
+  it("should put a selected tab from the dropdown menu as the first tab item", () => {
+    cy.viewport(1024, 720);
+
+    // Open dropdown menu
+    cy.get('[data-cy="TabsDropdownButton"').should("exist").click();
+
+    // Search for a pinned tab
+    cy.get('[data-cy="TabsDropdownMenu"').find("input[type=text]").type("card");
+
+    // Click the pinned tab
+    cy.get('[data-cy="TabsDropdownMenu"').find("a").should("exist").click();
+
+    // Ensure that the tab was moved
+    cy.get('[data-cy="ActiveTab"]').should("have.text", "customtype/card.vcf");
   });
 });
