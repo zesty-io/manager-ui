2b070b0a1c500522603432e356adbdf0378e02a4
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2022-12-27 19:29:55 +0800

    Added: view web engine preview event

--- src/shell/components/InstanceFlyoutMenuModal/DomainsMenu.tsx
+++ src/shell/components/InstanceFlyoutMenuModal/DomainsMenu.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import {
   Box,
   Button,
@@ -11,14 +12,17 @@ import {
 } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import LanguageIcon from "@mui/icons-material/Language";
-import { useEffect } from "react";
-
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 interface Props {
   onClose?: () => void;
   instanceZUID?: string;
 }
 
 const DomainsMenu = ({ onClose, instanceZUID }: Props) => {
+  // @ts-ignore
+  // @Note: check if we can migrate this to rtk
+  const instance = useSelector((state) => state.instance);
+
   return (
     <Dialog
       PaperProps={{
@@ -57,6 +61,24 @@ const DomainsMenu = ({ onClose, instanceZUID }: Props) => {
           Manage Domains
         </Button>
       </Box>
+      <DialogContent>
+        <ListItem sx={{ p: 0 }}>
+          <Button
+            variant="outlined"
+            sx={{ height: "32px", mt: 0.5 }}
+            onClick={() =>
+              window.open(
+                // @ts-ignore
+                `${CONFIG.URL_PREVIEW_PROTOCOL}${instance.randomHashID}${CONFIG.URL_PREVIEW}`,
+                "_blank"
+              )
+            }
+            startIcon={<RemoveRedEyeIcon fontSize="small" />}
+          >
+            View Web Engine Preview
+          </Button>
+        </ListItem>
+      </DialogContent>
     </Dialog>
   );
 };
