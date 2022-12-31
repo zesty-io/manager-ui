edf7d348edbb3cc4915dabcb8d6950584f95fa67
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-01-01 01:55:46 +0800

    Fixed: oneguru logo

--- src/shell/components/InstanceFlyoutMenuModal/DomainsMenu.tsx
+++ src/shell/components/InstanceFlyoutMenuModal/DomainsMenu.tsx
@@ -30,7 +30,7 @@ const DomainsMenu = ({ onClose, instanceZUID }: Props) => {
       <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
         <ListItem sx={{ p: 0, width: 0 }}>
           <ListItemIcon sx={{ minWidth: "30px" }}>
-            <IconButton sx={{ p: 0 }} onClick={() => onClose()}>
+            <IconButton onClick={() => onClose()}>
               <ArrowBackIcon fontSize="small" />
             </IconButton>
           </ListItemIcon>
