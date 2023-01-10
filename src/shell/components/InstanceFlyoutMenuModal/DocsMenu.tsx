2c19e7871188ca888e6b5e801582937627389e07
Carl Jacob Cruz <40680430+JacobDiazCruz@users.noreply.github.com>
2023-01-11 04:25:35 +0800

    Added: Instance flyout Menu - Accounts Menu Items (#1684)
    * Added: initial onboarding call component
    
    * Adjusted: nav items paddings and alignments
    
    * Added: schedule a call action
    
    * Fixed inconsistent bottom bar paddings and alignments
    
    * Added: condition to show onboarding section for only 15 days upon instance creation
    
    * Fixed: condition to show onboarding section
    
    * Added: global side nav rtk file
    
    * Update: global side nav rtk mutation
    
    * Added: refresh cache button UI and action
    
    * Added: copy instance zuid UI and action
    
    * Reposition instance flyout menu
    
    * Replace globalSideNav to cloudFunctionsApi
    
    * Updated: cloudFunctionsApi query endpoint
    
    * Move: logic to open onboarding call UI
    
    * Replace: iframe value for onboarding call
    
    * Added: open domains menu
    
    * Added: initial domains menu header
    
    * Added: manage domains click event
    
    * Added: view web engine preview event
    
    * Move: domains list inside domains menu
    
    * Update: domains menu styling
    
    * Update: instance flyout menu size and position
    
    * Update: domains menu rendering
    
    * Move: back icon onclick event
    
    * Fixed: oneguru logo
    
    * Update: domains menu divider
    
    * Update: domain items button size
    
    * Remove: view web engine preview list container
    
    * Replace: domains title font weight
    
    * Update: instance flyout modal sizing
    
    * Added: accounts section within instance flyout menu
    
    * Added: initial docs menu and get help button
    
    * Added: support zesty email link
    
    * Update: instance state type
    
    * Refactor: docs menu heading
    
    * Added: accounts menu items links

---
+++ src/shell/components/InstanceFlyoutMenuModal/DocsMenu.tsx
@@ -0,0 +1,78 @@
+import { useSelector } from "react-redux";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Link,
+  Typography,
+  Divider,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListItem,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import LanguageIcon from "@mui/icons-material/Language";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import EmailIcon from "@mui/icons-material/Email";
+import ChatIcon from "@mui/icons-material/Chat";
+import { AppState } from "../../store/types";
+
+interface Props {
+  onClose?: () => void;
+  instanceZUID?: string;
+}
+
+const DocsMenu = ({ onClose, instanceZUID }: Props) => {
+  const instance = useSelector((state: AppState) => state.instance);
+
+  return (
+    <>
+      <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton onClick={() => onClose()}>
+            <ArrowBackIcon fontSize="small" />
+          </IconButton>
+          <Typography variant="h5" fontWeight={600}>
+            Docs
+          </Typography>
+        </Box>
+        <Box>
+          <Link
+            title="Support"
+            href="mailto:support@zesty.io"
+            color="secondary"
+            underline="none"
+          >
+            <Button
+              variant="contained"
+              color="inherit"
+              size="small"
+              startIcon={<EmailIcon fontSize="small" />}
+            >
+              support@zesty.io
+            </Button>
+          </Link>
+          <Button
+            variant="contained"
+            color="inherit"
+            size="small"
+            sx={{ ml: 1 }}
+            onClick={() => {
+              window.open(`https://www.zesty.io/chat/`, "_blank");
+            }}
+            startIcon={<ChatIcon fontSize="small" />}
+          >
+            Get Help
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default DocsMenu;
--- src/shell/components/InstanceFlyoutMenuModal/index.tsx
+++ src/shell/components/InstanceFlyoutMenuModal/index.tsx
@@ -18,10 +18,13 @@ import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import CheckIcon from "@mui/icons-material/Check";
 import DomainsMenu from "./DomainsMenu";
+import DocsMenu from "./DocsMenu";
 interface Props {
   instanceFaviconUrl?: string;
   instanceName?: string;
   instanceZUID?: string;
+  userFaviconUrl?: string;
+  userFullname?: string;
   onSetShowFaviconModal?: any;
   onClose?: () => void;
 }
@@ -30,6 +33,8 @@ const InstanceFlyoutMenuModal = ({
   instanceFaviconUrl,
   instanceName,
   instanceZUID,
+  userFaviconUrl,
+  userFullname,
   onSetShowFaviconModal,
   onClose,
 }: Props) => {
@@ -43,6 +48,7 @@ const InstanceFlyoutMenuModal = ({
   ] = useRefreshCacheMutation();
   const [isCopiedZuid, setIsCopiedZuid] = useState(false);
   const [showDomainsMenu, setShowDomainsMenu] = useState(false);
+  const [showDocsMenu, setShowDocsMenu] = useState(false);
 
   const handleNavigation = (url: string) => {
     window.open(url, "_blank");
@@ -83,60 +89,125 @@ const InstanceFlyoutMenuModal = ({
           onClose={() => setShowDomainsMenu(false)}
           instanceZUID={instanceZUID}
         />
+      ) : showDocsMenu ? (
+        <DocsMenu
+          onClose={() => setShowDocsMenu(false)}
+          instanceZUID={instanceZUID}
+        />
       ) : (
         <>
-          <Box sx={{ py: 1, height: "340px" }}>
-            <ListItem>
-              <Avatar src={instanceFaviconUrl} />
-              <Typography variant="body2" sx={{ ml: 1.5, fontWeight: 700 }}>
-                {instanceName}
-              </Typography>
-            </ListItem>
-            <MenuItem onClick={onSetShowFaviconModal}>
-              <Typography variant="body2">Update Favicon</Typography>
-            </MenuItem>
-            <MenuItem onClick={() => setShowDomainsMenu(true)}>
-              <Typography variant="body2">Domains</Typography>
-            </MenuItem>
-            <Box sx={{ p: 1 }}>
-              <Button
-                variant="outlined"
-                color="inherit"
-                onClick={() => refreshCache()}
-                startIcon={
-                  <>
-                    {isLoadingRefreshCache ? (
-                      <CircularProgress size="18px" color="inherit" />
-                    ) : !isLoadingRefreshCache && isSuccessRefreshCache ? (
-                      <CheckIcon fontSize="small" />
-                    ) : (
-                      <RefreshIcon fontSize="small" />
-                    )}
-                  </>
-                }
-              >
-                Refresh Cache
-              </Button>
+          <Box
+            sx={{
+              height: "340px",
+              display: "flex",
+            }}
+          >
+            <Box sx={{ width: "200px", py: 1 }}>
+              <ListItem>
+                <Avatar src={instanceFaviconUrl} />
+                <Typography variant="body2" sx={{ ml: 1.5, fontWeight: 700 }}>
+                  {instanceName}
+                </Typography>
+              </ListItem>
+              <MenuItem onClick={onSetShowFaviconModal}>
+                <Typography variant="body2">Update Favicon</Typography>
+              </MenuItem>
+              <MenuItem onClick={() => setShowDomainsMenu(true)}>
+                <Typography variant="body2">Domains</Typography>
+              </MenuItem>
+              <Box sx={{ p: 1 }}>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  onClick={() => refreshCache()}
+                  startIcon={
+                    <>
+                      {isLoadingRefreshCache ? (
+                        <CircularProgress size="18px" color="inherit" />
+                      ) : !isLoadingRefreshCache && isSuccessRefreshCache ? (
+                        <CheckIcon fontSize="small" />
+                      ) : (
+                        <RefreshIcon fontSize="small" />
+                      )}
+                    </>
+                  }
+                >
+                  Refresh Cache
+                </Button>
+              </Box>
+              <Box sx={{ p: 1 }}>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  onClick={() => handleCopyInstanceZUID()}
+                  startIcon={
+                    <>
+                      {isCopiedZuid ? (
+                        <CheckIcon fontSize="small" />
+                      ) : (
+                        <ContentCopyIcon fontSize="small" />
+                      )}
+                    </>
+                  }
+                >
+                  Get Instance ZUID
+                </Button>
+              </Box>
             </Box>
-            <Box sx={{ p: 1 }}>
-              <Button
-                variant="outlined"
-                color="inherit"
-                onClick={() => handleCopyInstanceZUID()}
-                startIcon={
-                  <>
-                    {isCopiedZuid ? (
-                      <CheckIcon fontSize="small" />
-                    ) : (
-                      <ContentCopyIcon fontSize="small" />
-                    )}
-                  </>
+
+            <Box
+              sx={{
+                width: "200px",
+                height: "100%",
+                borderLeftColor: "grey.100",
+                borderLeftStyle: "solid",
+                borderLeftWidth: "1px",
+                py: 1,
+              }}
+            >
+              <ListItem>
+                <Avatar
+                  alt={`${userFullname} Avatar`}
+                  src={`https://www.gravatar.com/avatar/${userFaviconUrl}?d=mm&s=40`}
+                />
+                <Typography variant="body2" sx={{ ml: 1.5, fontWeight: 700 }}>
+                  {userFullname}
+                </Typography>
+              </ListItem>
+              <MenuItem
+                onClick={() =>
+                  handleNavigation("https://www.zesty.io/profile/")
                 }
               >
-                Get Instance ZUID
-              </Button>
+                <Typography variant="body2">User Settings</Typography>
+              </MenuItem>
+              <MenuItem
+                onClick={() =>
+                  handleNavigation("https://www.zesty.io/instances/")
+                }
+              >
+                <Typography variant="body2">See All Instances</Typography>
+              </MenuItem>
+              <MenuItem onClick={() => setShowDocsMenu(true)}>
+                <Typography variant="body2">Docs</Typography>
+              </MenuItem>
+              <MenuItem
+                onClick={() => handleNavigation("https://www.zesty.io/chat/")}
+              >
+                <Typography variant="body2">Get Help</Typography>
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  // @ts-ignore
+                  window.open(`${CONFIG.URL_ACCOUNTS}/logout`, "_self");
+                }}
+              >
+                <Typography variant="body2">Log Out</Typography>
+              </MenuItem>
             </Box>
           </Box>
+
+          {/* Footer */}
           <Box
             display="flex"
             justifyContent="space-between"
