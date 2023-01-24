f5828c281576962398cdee26f650fe3878b4d894
Ryan Pals <ryan.pals@zesty.io>
2023-01-24 11:42:19 -0500

    Global search content list VQA items

--- src/shell/views/SearchPage/ContentList.tsx
+++ src/shell/views/SearchPage/ContentList.tsx
@@ -51,6 +51,7 @@ export const ContentList: FC<ContentList> = ({
             width={width}
             itemCount={results.length}
             height={height}
+            style={{ borderRadius: "8px 8px 0 0" }}
           >
             {Row}
           </FixedSizeList>
--- src/shell/views/SearchPage/SearchPage.tsx
+++ src/shell/views/SearchPage/SearchPage.tsx
@@ -5,19 +5,24 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@zesty-io/material";
+import moment from "moment-timezone";
 
 import { NoSearchResults } from "../../components/NoSearchResults";
 import { useSearchContentQuery } from "../../services/instance";
 import { ContentList } from "./ContentList";
 import { BackButton } from "./BackButton";
-
 export const SearchPage: FC = () => {
   const [params, setParams] = useParams();
   const query = params.get("q") || "";
   const { data: results, isLoading } = useSearchContentQuery(
-    { query },
+    { query, order: "created", dir: "desc" },
     { skip: !query }
   );
+  const sortedResults = results ? [...results] : [];
+  sortedResults?.sort((a, b) => {
+    return moment(b.meta.updatedAt).diff(moment(a.meta.updatedAt));
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <Box
@@ -36,6 +41,7 @@ export const SearchPage: FC = () => {
             padding: "8px 24px",
             gap: "10px",
             height: "52px",
+            boxSizing: "border-box",
             border: `1px solid ${theme.palette.grey[100]}`,
             backgroundColor: "background.paper",
           }}
@@ -59,7 +65,7 @@ export const SearchPage: FC = () => {
                 height: "100%",
               }}
             >
-              <ContentList results={results} loading={isLoading} />
+              <ContentList results={sortedResults} loading={isLoading} />
             </Box>
           ))}
       </Box>
