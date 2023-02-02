e6ca02fc257f5fd7cc2a2f9673f4e8adf38e1a27
Ryan Pals <ryan.pals@zesty.io>
2023-02-02 16:49:19 -0600

    Use z-index values from theme

--- src/shell/components/ContentSearch/index.tsx
+++ src/shell/components/ContentSearch/index.tsx
@@ -15,6 +15,7 @@ import { notify } from "../../store/notifications";
 import Paper from "@mui/material/Paper";
 import Popper from "@mui/material/Popper";
 import Collapse from "@mui/material/Collapse";
+import { useTheme } from "@mui/material/styles";
 
 const ContentSearch: FC = () => {
   const [value, setValue] = useState("");
@@ -36,13 +37,14 @@ const ContentSearch: FC = () => {
   const languages = useSelector((state: any) => state.languages);
   const [open, setOpen] = useState(false);
 
+  const theme = useTheme();
   return (
     <Collapse
       in={open}
       collapsedSize="288px"
       orientation="horizontal"
       sx={{
-        zIndex: 40,
+        zIndex: (theme) => theme.zIndex.drawer - 1,
         height: "40px",
         position: "relative",
         width: "500px",
@@ -97,9 +99,9 @@ const ContentSearch: FC = () => {
                 },
               ]}
               style={{
-                // default z-index is 1300, we want it to be BELOW the side nav close button
-                zIndex: 40,
                 ...props.style,
+                // default z-index is too high, we want it to be BELOW the side nav close button
+                zIndex: theme.zIndex.drawer - 1,
               }}
             />
           );
