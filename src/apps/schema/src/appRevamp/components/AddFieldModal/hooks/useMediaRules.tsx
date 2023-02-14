1e914cb37368c9a442efd251d0bfa5b32428dbf3
Nar Cuenca <narc.ph@gmail.com>
2023-02-14 10:09:29 +0800

    task: removed unused methods and exports

--- src/apps/schema/src/appRevamp/components/AddFieldModal/hooks/useMediaRules.tsx
+++ src/apps/schema/src/appRevamp/components/AddFieldModal/hooks/useMediaRules.tsx
@@ -1,22 +1,8 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { mediaManagerApi } from "../../../../../../../shell/services/mediaManager";
-import { Bin, Group } from "../../../../../../../shell/services/types";
+import { Bin } from "../../../../../../../shell/services/types";
 import { AppState } from "../../../../../../../shell/store/types";
-import { FormValue } from "../views/FieldForm";
-import { DropdownOptions } from "../FieldFormInput";
-
-export interface ItemLimit {
-  title: string;
-  isChecked: boolean;
-  value: string;
-}
-
-export interface LockFolder {
-  title: string;
-  isChecked: boolean;
-  option: any;
-}
 
 export interface CustomGroup {
   value: string;
@@ -24,20 +10,6 @@ export interface CustomGroup {
 }
 
 export const useMediaRules = () => {
-  const [itemLimit, setItemLimit] = useState({
-    title: "Media Item Limit",
-    isChecked: false,
-    value: "1",
-  });
-  const [lockFolder, setLockFolder] = useState({
-    title: "Select Folder",
-    isChecked: false,
-    option: {
-      value: "",
-      label: "",
-    },
-  });
-
   const ecoId = useSelector((state: AppState) => state.instance.ecoID);
   const instanceId = useSelector((state: any) => state.instance.ID);
   const { data: bins, isFetching: isBinsFetching } =
@@ -66,10 +38,6 @@ export const useMediaRules = () => {
   }, [binGroups]);
 
   return {
-    itemLimit,
-    lockFolder,
-    setItemLimit,
-    setLockFolder,
     mediaFoldersOptions,
   };
 };
