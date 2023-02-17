27d6412918ef6aa8edd2fbd85df2e9e10b366c1b
Nar Cuenca <narc.ph@gmail.com>
2023-02-17 08:54:56 +0800

    task: added reusable flterbase component

---
+++ src/apps/schema/src/appRevamp/components/Filters/FilterBase.tsx
@@ -0,0 +1,55 @@
+import { FC } from "react";
+import { Button, ButtonGroup } from "@mui/material";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
+interface FilterBase {
+  isFilterActive: boolean;
+  buttonText: string;
+  onOpenMenu: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onRemoveFilter: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  children?: React.ReactNode;
+}
+export const FilterBase: FC<FilterBase> = ({
+  isFilterActive,
+  buttonText,
+  onOpenMenu,
+  onRemoveFilter,
+  children,
+}) => {
+  if (isFilterActive) {
+    return (
+      <>
+        <ButtonGroup variant="contained">
+          <Button
+            size="small"
+            startIcon={<CheckIcon sx={{ width: "20px", height: "20px" }} />}
+            onClick={onOpenMenu}
+          >
+            {buttonText}
+          </Button>
+          <Button size="small" onClick={onRemoveFilter}>
+            <CloseRoundedIcon fontSize="small" />
+          </Button>
+        </ButtonGroup>
+        {children}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Button
+        variant="outlined"
+        size="small"
+        color="inherit"
+        endIcon={<ArrowDropDownOutlinedIcon />}
+        onClick={onOpenMenu}
+      >
+        {buttonText}
+      </Button>
+      {children}
+    </>
+  );
+};
--- src/apps/schema/src/appRevamp/components/Filters/ModelType.tsx
+++ src/apps/schema/src/appRevamp/components/Filters/ModelType.tsx
@@ -15,6 +15,7 @@ import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { modelIconMap, modelNameMap } from "../../utils";
 import { ModelType as ModelSet } from "../../../../../../shell/services/types";
 import { FiltersProps } from "./index";
+import { FilterBase } from "./FilterBase";
 
 const MODEL_TYPE_FILTERS: ModelSet[] = ["templateset", "pageset", "dataset"];
 
@@ -40,57 +41,31 @@ export const ModelType: FC<FiltersProps> = ({
     });
   };
 
-  const getFilterButton = () => {
-    if (activeModelTypeFilter) {
-      return (
-        <ButtonGroup variant="contained">
-          <Button
-            size="small"
-            startIcon={<CheckIcon sx={{ width: "20px", height: "20px" }} />}
-            onClick={handleOpenMenuClick}
-          >
-            {modelNameMap[activeModelTypeFilter]}
-          </Button>
-          <Button
-            size="small"
-            onClick={() => setActiveFilters({ modelType: "" })}
-          >
-            <CloseRoundedIcon fontSize="small" />
-          </Button>
-        </ButtonGroup>
-      );
-    } else {
-      return (
-        <Button
-          variant="outlined"
-          size="small"
-          color="inherit"
-          endIcon={<ArrowDropDownOutlinedIcon />}
-          onClick={handleOpenMenuClick}
-        >
-          Model Type
-        </Button>
-      );
-    }
-  };
-
   return (
     <>
-      {getFilterButton()}
-      <Menu
-        open={isFilterMenuOpen}
-        anchorEl={menuAnchorEl}
-        onClose={() => setMenuAnchorEl(null)}
+      <FilterBase
+        isFilterActive={Boolean(activeModelTypeFilter)}
+        buttonText={
+          modelNameMap[activeModelTypeFilter as ModelSet] || "Model Type"
+        }
+        onOpenMenu={handleOpenMenuClick}
+        onRemoveFilter={() => setActiveFilters({ modelType: "" })}
       >
-        {MODEL_TYPE_FILTERS.map((filter, index) => (
-          <MenuItem key={index} onClick={() => handleFilterSelect(filter)}>
-            <ListItemIcon>
-              <SvgIcon component={modelIconMap[filter]} />
-            </ListItemIcon>
-            <ListItemText>{modelNameMap[filter]}</ListItemText>
-          </MenuItem>
-        ))}
-      </Menu>
+        <Menu
+          open={isFilterMenuOpen}
+          anchorEl={menuAnchorEl}
+          onClose={() => setMenuAnchorEl(null)}
+        >
+          {MODEL_TYPE_FILTERS.map((filter, index) => (
+            <MenuItem key={index} onClick={() => handleFilterSelect(filter)}>
+              <ListItemIcon>
+                <SvgIcon component={modelIconMap[filter]} />
+              </ListItemIcon>
+              <ListItemText>{modelNameMap[filter]}</ListItemText>
+            </MenuItem>
+          ))}
+        </Menu>
+      </FilterBase>
     </>
   );
 };
