0d10e22666c5829758432ae9952a5c03db4a64b1
Nar Cuenca <narc.ph@gmail.com>
2023-02-17 14:38:08 +0800

    task: added user filter menu item

--- src/apps/schema/src/appRevamp/components/Filters/People.tsx
+++ src/apps/schema/src/appRevamp/components/Filters/People.tsx
@@ -1,11 +1,16 @@
-import { FC, useState } from "react";
+import { FC, useState, useMemo } from "react";
 import {
   Menu,
   MenuItem,
   ListItemAvatar,
   ListItemText,
   Avatar,
+  TextField,
+  InputAdornment,
+  IconButton,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 import { FilterButton } from "./FilterButton";
 import { useGetUsersQuery } from "../../../../../../shell/services/accounts";
@@ -16,13 +21,28 @@ export const People: FC<FiltersProps> = ({
   activeFilters,
   setActiveFilters,
 }) => {
+  const [filter, setFilter] = useState("");
   const [menuAnchorEl, setMenuAnchorEl] = useState<HTMLButtonElement | null>(
     null
   );
   const isFilterMenuOpen = Boolean(menuAnchorEl);
   const { data: users } = useGetUsersQuery();
+
+  const filteredUsers = useMemo(() => {
+    if (!filter.length) {
+      return users;
+    }
+    const _filter = filter.toLowerCase();
+
+    return users?.filter(
+      (user) =>
+        user?.firstName?.toLowerCase().includes(_filter) ||
+        user?.lastName?.toLowerCase().includes(_filter)
+    );
+  }, [filter, users]);
+
   const activeUserFilter = users?.find(
-    (user) => user.ZUID === activeFilters?.people
+    (user) => user?.ZUID === activeFilters?.people
   );
   const buttonText = activeUserFilter
     ? `${activeUserFilter.firstName} ${activeUserFilter.lastName}`
@@ -53,10 +73,42 @@ export const People: FC<FiltersProps> = ({
         PaperProps={{
           style: {
             maxHeight: 52 * 10 + 8, // Item Height * No. of items to show + Padding Top
+            width: 320,
           },
         }}
       >
-        {users?.map((user) => {
+        <MenuItem
+          disableRipple
+          onKeyDown={(e) => e.stopPropagation()}
+          sx={{
+            "&:hover": {
+              backgroundColor: "common.white",
+            },
+            "&.Mui-focusVisible": {
+              backgroundColor: "common.white",
+            },
+          }}
+        >
+          <TextField
+            fullWidth
+            placeholder="Search Users"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon fontSize="small" />
+                </InputAdornment>
+              ),
+              endAdornment: filter.length ? (
+                <IconButton size="small" onClick={() => setFilter("")}>
+                  <CloseRoundedIcon fontSize="small" />
+                </IconButton>
+              ) : null,
+            }}
+          />
+        </MenuItem>
+        {filteredUsers?.map((user) => {
           return (
             <MenuItem
               key={user?.ZUID}
