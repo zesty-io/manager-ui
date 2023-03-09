48bd8d38e495b00e9cc15cb46a7a70ad1d03f609
Nar Cuenca <narc.ph@gmail.com>
2023-03-09 09:45:00 +0800

    task: added content for activity log

--- src/apps/schema/src/appRevamp/components/ModelActivityLog.tsx
+++ src/apps/schema/src/appRevamp/components/ModelActivityLog.tsx
@@ -1,5 +1,92 @@
-import React from "react";
+import { useState, useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { Grid } from "@mui/material";
+import moment from "moment";
+import { isEmpty } from "lodash";
 
+import { ActionsTimeline } from "../../../../reports/src/app/views/ActivityLog/components/ActionsTimeline";
+import { Filters } from "../../../../reports/src/app/views/ActivityLog/components/Filters";
+import { ActionsByUsers } from "../../../../reports/src/app/views/ActivityLog/components/ActionsByUsers";
+import {
+  useGetContentModelQuery,
+  useGetAuditsQuery,
+} from "../../../../../shell/services/instance";
+import { useParams as useSearchParams } from "../../../../../shell/hooks/useParams";
+import { filterByParams } from "../../../../../utility/filterByParams";
+
+type Params = {
+  id: string;
+};
 export const ModelActivityLog = () => {
-  return <div>ModelActivityLog</div>;
+  const { id } = useParams<Params>();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [initialized, setInitialized] = useState(false);
+
+  const {
+    data: modelData,
+    isLoading: isLoadingModelData,
+    isFetching: isFetchingModelData,
+  } = useGetContentModelQuery(id, { skip: !id });
+  const {
+    data: audits,
+    isLoading: isLoadingAudits,
+    isFetching: isFetchingAudits,
+  } = useGetAuditsQuery(
+    {
+      start_date: moment(searchParams.get("from")).format("L"),
+      end_date: moment(searchParams.get("to")).format("L"),
+    },
+    { skip: !initialized }
+  );
+
+  useEffect(() => {
+    if (
+      !searchParams.get("from") &&
+      !searchParams.get("to") &&
+      !isEmpty(modelData)
+    ) {
+      setSearchParams(
+        moment(modelData?.createdAt).format("YYYY-MM-DD"),
+        "from"
+      );
+      setSearchParams(moment().format("YYYY-MM-DD"), "to");
+    }
+
+    setInitialized(true);
+  }, [modelData, location.pathname]);
+
+  const relatedAudits = useMemo(
+    () => audits?.filter((audit) => audit.affectedZUID === id) || [],
+    [id, audits]
+  );
+
+  const filteredAudits = useMemo(
+    () =>
+      relatedAudits?.length ? filterByParams(relatedAudits, searchParams) : [],
+    [relatedAudits, searchParams]
+  );
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Filters
+          filters={["action", "actionByUserZUID"]}
+          actions={relatedAudits}
+          showSkeletons={isLoadingAudits || isLoadingModelData}
+        />
+      </Grid>
+      <Grid item xs={9}>
+        <ActionsTimeline
+          actions={filteredAudits}
+          showSkeletons={isLoadingAudits || isLoadingModelData}
+        />
+      </Grid>
+      <Grid item xs={3}>
+        <ActionsByUsers
+          actions={filteredAudits}
+          showSkeletons={isFetchingAudits || isFetchingModelData}
+        />
+      </Grid>
+    </Grid>
+  );
 };
