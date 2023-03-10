98bee33bbdcb66f69191ccfacf2c726380e192ee
Nar Cuenca <narc.ph@gmail.com>
2023-03-10 14:47:27 +0800

    task: removed unused type

--- src/shell/components/Filters/DateFilter/types.ts
+++ src/shell/components/Filters/DateFilter/types.ts
@@ -5,7 +5,7 @@ export type PresetType =
   | "last_30_days"
   | "last_3_months"
   | "last_12_months";
-export type DateFilterModalType = "on" | "before" | "after" | "daterange" | "";
+export type DateFilterModalType = "on" | "before" | "after" | "";
 
 export interface DateFilterValue {
   type: "preset" | DateFilterModalType;
