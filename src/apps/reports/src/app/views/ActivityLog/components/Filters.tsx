830fa338cb9a8b1366bbf179c5fc8b7656f5504b
Nar Cuenca <28705606+theofficialnar@users.noreply.github.com>
2023-03-15 00:32:37 +0800

    Schema Activity Log: Updated remaining filters, updated daterange design (#1921)
    * task: convert old filters
    
    * task: close model on date select
    
    * task: daterange filter reset
    
    * fix: incorrect user zuid
    
    * task: layout update
    
    * chore: removed comment

--- src/apps/reports/src/app/views/ActivityLog/components/Filters.tsx
+++ src/apps/reports/src/app/views/ActivityLog/components/Filters.tsx
@@ -1,14 +1,7 @@
-import { useMemo, FC } from "react";
-import {
-  Box,
-  Select,
-  MenuItem,
-  FormControl,
-  FormLabel,
-  Skeleton,
-} from "@mui/material";
+import { useMemo, FC, useEffect, useState } from "react";
+import { Box, FormControl, Skeleton } from "@mui/material";
 import moment from "moment";
-import { uniqBy } from "lodash";
+import { uniqBy, isEqual } from "lodash";
 import { useParams } from "../../../../../../../shell/hooks/useParams";
 import { accountsApi } from "../../../../../../../shell/services/accounts";
 import { Audit } from "../../../../../../../shell/services/types";
@@ -19,7 +12,74 @@ import {
   GenericFilter,
 } from "../../../../../../../shell/components/Filters";
 
-const RESOURCE_TYPES = ["Code", "Content", "Schema", "Settings"];
+const RESOURCE_TYPES = [
+  {
+    text: "Code",
+    value: "code",
+  },
+  {
+    text: "Content",
+    value: "content",
+  },
+  {
+    text: "Schema",
+    value: "schema",
+  },
+  {
+    text: "Settings",
+    value: "settings",
+  },
+];
+const HAPPENED_AT = [
+  {
+    text: "Most Recent",
+    value: "",
+  },
+  {
+    text: "Oldest First",
+    value: "happenedAt",
+  },
+];
+const USER_ACTIVITY = [
+  {
+    text: "Most Active",
+    value: "",
+  },
+  {
+    text: "Most Recently Active",
+    value: "happenedAt",
+  },
+  {
+    text: "Least Active",
+    value: "leastActive",
+  },
+];
+const ACTION = [
+  {
+    text: "Created",
+    value: "1",
+  },
+  {
+    text: "Modified",
+    value: "2",
+  },
+  {
+    text: "Deleted",
+    value: "3",
+  },
+  {
+    text: "Published",
+    value: "4",
+  },
+  {
+    text: "Unpublished",
+    value: "5",
+  },
+  {
+    text: "Scheduled",
+    value: "6",
+  },
+];
 
 type Filter =
   | "happenedAt"
@@ -39,8 +99,9 @@ export const Filters: FC<FiltersProps> = ({
   showSkeletons,
 }) => {
   const [params, setParams] = useParams();
-
   const { data: usersRoles } = accountsApi.useGetUsersRolesQuery();
+  const [defaultDateRange, setDefaultDateRange] =
+    useState<DateRangeFilterValue>({ from: null, to: null });
 
   const uniqueUserActions = useMemo(() => {
     const uniqueUsers = uniqBy(actions, "actionByUserZUID");
@@ -48,63 +109,85 @@ export const Filters: FC<FiltersProps> = ({
     return uniqueUsers?.map((user) => ({
       firstName: user.firstName,
       lastName: user.lastName,
-      ZUID: user.ZUID,
+      ZUID: user.actionByUserZUID,
       email: user.email,
     }));
   }, [actions]);
 
-  const uniqueUsersRoles = useMemo(
-    () => uniqBy(usersRoles, "role.ZUID"),
-    [usersRoles]
-  );
+  const uniqueUsersRoles = useMemo(() => {
+    if (usersRoles?.length) {
+      const uniqueRoles = uniqBy(usersRoles, "role.ZUID");
+
+      return uniqueRoles?.map((role) => ({
+        text: role.role.name,
+        value: role.role.name,
+      }));
+    }
+  }, [usersRoles]);
+
+  const dateRange: DateRangeFilterValue = useMemo(() => {
+    if (Boolean(params.get("from")) && Boolean(params.get("to"))) {
+      const currentDateRange = {
+        from: moment(params.get("from")).format("YYYY-MM-DD"),
+        to: moment(params.get("to")).format("YYYY-MM-DD"),
+      };
+
+      if (isEqual(currentDateRange, defaultDateRange)) {
+        // Don't show the date on the date range filter if it's the same with the default date
+        return { from: null, to: null };
+      }
+
+      return currentDateRange;
+    }
+
+    return { from: null, to: null };
+  }, [params, defaultDateRange]);
+
+  useEffect(() => {
+    // Store a copy of the default date range which will be used to reset the date range
+    if (
+      Boolean(params.get("from")) &&
+      Boolean(params.get("to")) &&
+      !defaultDateRange.from &&
+      !defaultDateRange.to
+    ) {
+      setDefaultDateRange({
+        from: moment(params.get("from")).format("YYYY-MM-DD"),
+        to: moment(params.get("to")).format("YYYY-MM-DD"),
+      });
+    }
+  }, [params]);
 
   const getFilter = (filter: Filter) => {
-    /* 
-      TODO: 
-        - Make a generic filter component that will accept an array of options for the common filters
-        - For other more specific filters, convert them into reusable filters
-    */
     switch (filter) {
       case "happenedAt":
         return (
-          <>
-            <FormLabel>Sort By</FormLabel>
-            <Select
-              value={params.get("sortBy") || ""}
-              onChange={(evt) => setParams(evt.target.value, "sortBy")}
-              size="small"
-              displayEmpty
-            >
-              <MenuItem value="">Most Recent</MenuItem>
-              <MenuItem value="happenedAt">Oldest First</MenuItem>
-            </Select>
-          </>
+          <GenericFilter
+            options={HAPPENED_AT}
+            value={params.get("sortBy") || ""}
+            onChange={(happenedAt) =>
+              setParams(happenedAt?.toString(), "sortBy")
+            }
+            defaultButtonText="Most Recent"
+          />
         );
       case "sortByUsers":
         return (
-          <>
-            <FormLabel>Sort By</FormLabel>
-            <Select
-              value={params.get("sortByUsers") || ""}
-              onChange={(evt) => setParams(evt.target.value, "sortByUsers")}
-              size="small"
-              displayEmpty
-            >
-              <MenuItem value="">Most Active</MenuItem>
-              <MenuItem value="happenedAt">Most Recently Active</MenuItem>
-              <MenuItem value="leastActive">Least Active</MenuItem>
-            </Select>
-          </>
+          <GenericFilter
+            defaultButtonText="Most Active"
+            value={params.get("sortByUsers") || ""}
+            onChange={(userActivity) =>
+              setParams(userActivity.toString(), "sortByUsers")
+            }
+            options={USER_ACTIVITY}
+          />
         );
       case "resourceType":
         return (
           <GenericFilter
             defaultButtonText="Resource Type"
             value={params.get("resourceType") || ""}
-            options={RESOURCE_TYPES.map((type) => ({
-              text: type,
-              value: type.toLowerCase(),
-            }))}
+            options={RESOURCE_TYPES}
             onChange={(resourceType) =>
               setParams(resourceType?.toString(), "resourceType")
             }
@@ -121,87 +204,67 @@ export const Filters: FC<FiltersProps> = ({
         );
       case "action":
         return (
-          <>
-            <FormLabel>Action Type</FormLabel>
-            <Select
-              value={params.get("action") || ""}
-              onChange={(evt) => setParams(evt.target.value, "action")}
-              size="small"
-              displayEmpty
-            >
-              <MenuItem value="">All</MenuItem>
-              <MenuItem value="1">Created</MenuItem>
-              <MenuItem value="2">Modified</MenuItem>
-              <MenuItem value="3">Deleted</MenuItem>
-              <MenuItem value="4">Published</MenuItem>
-              <MenuItem value="5">Unpublished</MenuItem>
-              <MenuItem value="6">Scheduled</MenuItem>
-            </Select>
-          </>
+          <GenericFilter
+            defaultButtonText="Action Type"
+            value={params.get("action") || ""}
+            options={ACTION}
+            onChange={(action) => setParams(action.toString(), "action")}
+          />
         );
       case "userRole":
         return (
-          <>
-            <FormLabel>User Role</FormLabel>
-            <Select
-              value={params.get("userRole") || ""}
-              onChange={(evt) => setParams(evt.target.value, "userRole")}
-              size="small"
-              displayEmpty
-            >
-              <MenuItem value="">All</MenuItem>
-              {uniqueUsersRoles.map((userRole) => (
-                <MenuItem key={userRole.role.ZUID} value={userRole.role.name}>
-                  {userRole.role.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </>
+          <GenericFilter
+            defaultButtonText="User Role"
+            value={params.get("userRole") || ""}
+            onChange={(role) => setParams(role.toString(), "userRole")}
+            options={uniqueUsersRoles}
+          />
         );
       default:
         break;
     }
   };
 
+  const handleDateRangeFilterChanged = (range: DateRangeFilterValue) => {
+    if (!range.to && !range.from) {
+      // Reset the date range filter to the default value when user input date range is cleared
+      setParams(
+        moment(defaultDateRange.from).isValid() ? defaultDateRange.from : "",
+        "from"
+      );
+      setParams(
+        moment(defaultDateRange.to).isValid() ? defaultDateRange.to : "",
+        "to"
+      );
+    } else {
+      setParams(range.from, "from");
+      setParams(range.to, "to");
+    }
+  };
+
   return (
-    // TODO: Fix spacing layout
     <Box
       data-cy="filters"
       sx={{
         display: "flex",
-        gap: 1,
-        justifyContent: "space-between",
-        mt: 2,
-        mb: 1.5,
+        gap: 1.5,
+        my: 1.5,
       }}
     >
-      <Box sx={{ display: "flex", gap: 1.5 }}>
-        {filters.map((filter, idx) =>
-          showSkeletons ? (
-            <Skeleton key={idx} variant="rectangular" width={172} height={56} />
-          ) : (
-            <FormControl key={idx} sx={{ width: 172 }}>
-              {getFilter(filter)}
-            </FormControl>
-          )
-        )}
-      </Box>
+      {filters.map((filter, idx) =>
+        showSkeletons ? (
+          <Skeleton key={idx} variant="rectangular" width={172} height={56} />
+        ) : (
+          <Box key={idx}>{getFilter(filter)}</Box>
+        )
+      )}
+
       {showSkeletons ? (
         <Skeleton variant="rectangular" width={250} height={56} />
       ) : (
         <DateRangeFilter
-          value={{
-            from: params.get("from")
-              ? moment(params.get("from")).format("YYYY-MM-DD")
-              : null,
-            to: params.get("to")
-              ? moment(params.get("to")).format("YYYY-MM-DD")
-              : null,
-          }}
-          onChange={(filter: DateRangeFilterValue) => {
-            setParams(moment(filter.from).isValid() ? filter.from : "", "from");
-            setParams(moment(filter.to).isValid() ? filter.to : "", "to");
-          }}
+          value={dateRange}
+          onChange={handleDateRangeFilterChanged}
           inactiveButtonText="Date"
           headerTitle="Date"
         />
