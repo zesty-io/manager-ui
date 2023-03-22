0c6bd1690ccdcb1e7aed3f2a3825602cc58dc01c
Nar Cuenca <narc.ph@gmail.com>
2023-03-22 13:38:09 +0800

    task: moved onboarding call component to its own component

---
+++ src/shell/components/global-sidebar/components/OnboardingCall.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Avatar, Box, Button, Dialog, Typography, Stack } from "@mui/material";
+
+import salesAvatar from "../../../../../public/images/salesAvatar.png";
+import fullZestyLogo from "../../../../../public/images/fullZestyLogo.svg";
+
+export const OnboardingCall = () => {
+  const [showMeetModal, setShowMeetModal] = useState(false);
+
+  return (
+    <>
+      <Box px={2} py={2.5}>
+        <Stack direction="row" justifyContent="space-between">
+          <Avatar
+            src={salesAvatar}
+            alt="Sales Avatar"
+            sx={{
+              width: "32px",
+              height: "32px",
+            }}
+          />
+          <img
+            src={fullZestyLogo}
+            alt="Full Zesty Logo"
+            width={70}
+            height={20}
+          />
+        </Stack>
+        <Typography variant="h6" color="common.white" my={1.5}>
+          Schedule an onboarding call with our support team
+        </Typography>
+
+        <Button
+          variant="outlined"
+          onClick={() => setShowMeetModal(true)}
+          size="small"
+        >
+          Schedule a call
+        </Button>
+      </Box>
+      <Dialog open={showMeetModal} onClose={() => setShowMeetModal(false)}>
+        <iframe
+          width="364"
+          height="800"
+          src="https://zesty.zohobookings.com/portal-embed#/customer/3973976000000039370"
+        ></iframe>
+      </Dialog>
+    </>
+  );
+};
