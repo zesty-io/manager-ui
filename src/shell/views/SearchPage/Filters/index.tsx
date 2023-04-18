5f359811f776c11a6c7fb72ae4edac307e6a1fc9
Nar Cuenca <narc.ph@gmail.com>
2023-04-18 09:20:37 +0800

    task: add user filter

--- src/shell/views/SearchPage/Filters/index.tsx
+++ src/shell/views/SearchPage/Filters/index.tsx
@@ -1,19 +1,34 @@
+import { useMemo } from "react";
 import { Stack } from "@mui/material";
 
 import { useParams } from "../../../hooks/useParams";
 import { SortByFilter, FilterValues } from "./SortByFilter";
 import { UserFilter } from "../../../components/Filters/";
+import { useGetUsersQuery } from "../../../services/accounts";
 
 export const Filters = () => {
   const [params, setParams] = useParams();
+  const { data: users } = useGetUsersQuery();
+  const userOptions = useMemo(() => {
+    return users?.map((user) => ({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      ZUID: user.ZUID,
+      email: user.email,
+    }));
+  }, [users]);
 
   return (
     <Stack direction="row" gap={1.5}>
       <SortByFilter
         value={(params.get("sort") as FilterValues) || "modified"}
-        onChange={(value) => {
-          setParams(value, "sort");
-        }}
+        onChange={(value) => setParams(value, "sort")}
+      />
+      <UserFilter
+        value={params.get("user") || ""}
+        onChange={(value) => setParams(value, "user")}
+        defaultButtonText="People"
+        options={userOptions}
       />
     </Stack>
   );
