0da10b8c18bf3603c8bac0c866c0c1bae8eed7e1
Nar Cuenca <narc.ph@gmail.com>
2023-04-26 07:57:59 +0800

    task: added cypress test scenarios

---
+++ cypress/e2e/search/advanced-search.spec.js
@@ -0,0 +1,6 @@
+describe("Global Search: Advanced Search", () => {
+  it.skip("Opens the advanced search modal", () => {});
+  it.skip("Passes text input from the global search input to the advanced search modal", () => {});
+  it.skip("Is able to clear all user input when clicking the clear all button", () => {});
+  it.skip("Takes the user to the search page with all user input set in the URL parameters accordingly", () => {});
+});
---
+++ cypress/e2e/search/search-page.spec.js
@@ -0,0 +1,11 @@
+describe("Global Search: Search Page", () => {
+  it.skip("Displays search results", () => {});
+
+  it.skip("ets filters as url search parameters", () => {});
+
+  it.skip("Sets URL search parameters are applied to the filter components", () => {});
+
+  it.skip("Shows the no results screen", () => {});
+
+  it.skip("Shows the no results matching the filters applied screen", () => {});
+});
