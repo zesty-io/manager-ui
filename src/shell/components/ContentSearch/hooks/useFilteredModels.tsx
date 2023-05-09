37b4d8779fb21e75880e90fc8e8c056c893e358b
Nar Cuenca <narc.ph@gmail.com>
2023-05-09 12:28:07 +0800

    task: added model type keyword matching

--- src/shell/components/ContentSearch/hooks/useFilteredModels.tsx
+++ src/shell/components/ContentSearch/hooks/useFilteredModels.tsx
@@ -4,15 +4,60 @@ import { isEmpty } from "lodash";
 import { Suggestion } from "../index";
 import { useGetContentModelsQuery } from "../../../services/instance";
 
+const templatesetKeywords = [
+  "single page model",
+  "single page models",
+  "singlepage model",
+  "singlepage models",
+];
+const pagesetKeywords = [
+  "multi page model",
+  "multi page models",
+  "multipage model",
+  "multipage models",
+];
+const datasetKeywords = [
+  "headless data",
+  "headless data models",
+  "headless data model",
+];
+
 type UseFilteredModels = [Suggestion[], (searchTerm: string) => void];
 export const useFilteredModels: () => UseFilteredModels = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const { data: models } = useGetContentModelsQuery();
 
   const filteredModels: Suggestion[] = useMemo(() => {
+    let modelDatatype = "";
+    const term = searchTerm?.toLowerCase();
+
+    // Check if the user's keyword is a datatype
+    if (templatesetKeywords.includes(term)) {
+      modelDatatype = "templateset";
+    }
+
+    if (pagesetKeywords.includes(term)) {
+      modelDatatype = "pageset";
+    }
+
+    if (datasetKeywords.includes(term)) {
+      modelDatatype = "dataset";
+    }
+
+    // Filter models
     if (!isEmpty(models)) {
-      const _models = models.filter((model) =>
-        model.label?.toLowerCase().includes(searchTerm?.toLowerCase())
+      /**
+       * Allow users to match models via the ff:
+       * - Model title
+       * - Model ZUID
+       * - Model type
+       */
+      // TODO: Verify if we still need to use a fuzzy search lib for the model type keywords and model label
+      const _models = models.filter(
+        (model) =>
+          model.label?.toLowerCase().includes(term) ||
+          model.ZUID === term ||
+          model.type === modelDatatype
       );
 
       return _models.map((model) => {
