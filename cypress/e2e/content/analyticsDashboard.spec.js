ead0b4de59f8d66b184e7f79f8e722ee48bd8d7b
Andres <agalin920@gmail.com>
2023-07-10 09:23:49 -0700

    cypress tests

---
+++ cypress/e2e/content/analyticsDashboard.spec.js
@@ -0,0 +1,36 @@
+describe("Analytics dashboard", () => {
+  before(() => {
+    cy.waitOn("*getPropertyDataByQuery*", () => {
+      cy.visit("/content");
+    });
+  });
+  it("Renders the page with the zesty.pw property", () => {
+    cy.contains("http://zesty.pw");
+  });
+  it("Filters properties on search input", () => {
+    cy.getBySelector("analytics-settings").click();
+    cy.get('input[placeholder="Search Google Analytics Properties"]').type(
+      "zesty.pw"
+    );
+    cy.get(".MuiList-root .MuiListItemButton-root").should("have.length", 1);
+    cy.get("body").type("{esc}");
+  });
+  it("Allows property switching by updating instance setting", () => {
+    cy.getBySelector("analytics-settings").click();
+    cy.waitOn("*/env/settings/*", () => {
+      cy.contains("zesty.pw - GA4").click();
+    });
+  });
+  it("Displays linked google account information", () => {
+    cy.getBySelector("analytics-settings").click();
+    cy.contains("GA Settings").click();
+    cy.contains("Stuart Runyan");
+    cy.get("body").type("{esc}");
+  });
+  it("Applies selected date filter to url params", () => {
+    cy.url().should("include", "datePreset=last_14_days");
+    cy.getBySelector("date_default").click();
+    cy.contains("Last 7 days").click();
+    cy.url().should("include", "datePreset=last_7_days");
+  });
+});
