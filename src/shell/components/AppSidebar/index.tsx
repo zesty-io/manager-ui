2ecfa51391eacfc5be573fff8b22e7224ec53ccf
Nar Cuenca <narc.ph@gmail.com>
2023-07-19 11:37:55 +0800

    task: created reusable appsidebar component

---
+++ src/shell/components/AppSidebar/index.tsx
@@ -0,0 +1,51 @@
+import { FC, useMemo } from "react";
+import { Box, Stack, Typography, PaletteMode } from "@mui/material";
+import { alpha, createTheme, ThemeProvider } from "@mui/material/styles";
+import { getTheme } from "@zesty-io/material";
+
+interface Props {
+  mode?: PaletteMode;
+  HeaderSubComponent?: React.ReactNode;
+  headerTitle?: string;
+}
+export const AppSideBar: FC<Readonly<Props>> = ({
+  mode = "light",
+  HeaderSubComponent,
+  headerTitle,
+  children,
+  ...props
+}) => {
+  const theme = useMemo(() => {
+    return getTheme(mode);
+  }, [mode]);
+
+  const isLightMode = mode === "light";
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Stack
+        gap={1.5}
+        pt={1.5}
+        sx={{
+          backgroundColor: isLightMode ? "common.white" : "grey.900",
+        }}
+        {...props}
+      >
+        {!!headerTitle && (
+          <Typography
+            variant="h6"
+            color="text.primary"
+            fontWeight={700}
+            lineHeight="24px"
+            fontSize={18}
+            px={1.5}
+          >
+            {headerTitle}
+          </Typography>
+        )}
+        {!!HeaderSubComponent && <Box>{HeaderSubComponent}</Box>}
+        {children}
+      </Stack>
+    </ThemeProvider>
+  );
+};
