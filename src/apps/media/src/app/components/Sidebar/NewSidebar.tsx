6f7601e48053df8a83c8c7cf8f1e0995724c68c1
Nar Cuenca <narc.ph@gmail.com>
2023-08-09 14:49:11 +0800

    task: add upload button to sidebar

--- src/apps/media/src/app/components/Sidebar/NewSidebar.tsx
+++ src/apps/media/src/app/components/Sidebar/NewSidebar.tsx
@@ -1,16 +1,16 @@
-import React, { FC, useEffect, useState, useRef } from "react";
-import {
-  ScheduleRounded,
-  InsightsRounded,
-  FileUploadRounded,
-} from "@mui/icons-material";
+import { FC, useEffect, useState, useRef } from "react";
+import { ScheduleRounded, InsightsRounded } from "@mui/icons-material";
 import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
 
 import {
   AppSideBar,
   SubMenu,
 } from "../../../../../../shell/components/AppSidebar";
+import { UploadButton } from "../UploadButton";
 import { useParams } from "../../../../../../shell/hooks/useParams";
+import { useGetBinsQuery } from "../../../../../../shell/services/mediaManager";
+import { AppState } from "../../../../../../shell/store/types";
 
 interface Props {
   lockedToGroupId?: string;
@@ -23,6 +23,13 @@ export const Sidebar: FC<Props> = ({ lockedToGroupId, isSelectDialog }) => {
   const term = (params as URLSearchParams).get("term");
   const clearAndFocus = (params as URLSearchParams).get("cf");
   const [searchTerm, setSearchTerm] = useState("");
+  const instanceId = useSelector((state: AppState) => state.instance.ID);
+  const ecoId = useSelector((state: AppState) => state.instance.ecoID);
+
+  const { data: bins } = useGetBinsQuery({
+    instanceId,
+    ecoId,
+  });
 
   useEffect(() => {
     setSearchTerm(term || "");
@@ -34,6 +41,7 @@ export const Sidebar: FC<Props> = ({ lockedToGroupId, isSelectDialog }) => {
     }
   }, [clearAndFocus]);
 
+  const defaultBin = bins?.find((bin) => bin.default) || bins?.[0];
   const subMenu: SubMenu[] = [
     {
       name: "Recents",
@@ -48,18 +56,27 @@ export const Sidebar: FC<Props> = ({ lockedToGroupId, isSelectDialog }) => {
   ];
 
   return (
-    <AppSideBar
-      data-cy="media-nav"
-      mode="dark"
-      headerTitle={isSelectDialog ? "Insert from Media" : "Media"}
-      subMenus={lockedToGroupId ? null : subMenu}
-      ref={appSideBarRef}
-      searchPlaceholder="Search Media"
-      titleButtonTooltip="Upload"
-      hideSubMenuOnSearch={false}
-      titleButtonIcon={FileUploadRounded}
-      filterKeyword={searchTerm}
-      onFilterEnter={(keyword) => history.push(`/media/search?term=${keyword}`)}
-    />
+    <>
+      <AppSideBar
+        data-cy="media-nav"
+        mode="dark"
+        headerTitle={isSelectDialog ? "Insert from Media" : "Media"}
+        subMenus={lockedToGroupId ? null : subMenu}
+        ref={appSideBarRef}
+        searchPlaceholder="Search Media"
+        hideSubMenuOnSearch={false}
+        filterKeyword={searchTerm}
+        onFilterEnter={(keyword) =>
+          history.push(`/media/search?term=${keyword}`)
+        }
+        TitleButtonComponent={
+          <UploadButton
+            currentBinId={defaultBin?.id}
+            isIconButton
+            caller="sidebar"
+          />
+        }
+      />
+    </>
   );
 };
