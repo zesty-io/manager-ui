edd7c3d0a93b08d74aa8af68baaee00dfc7e519d
Nar Cuenca <narc.ph@gmail.com>
2023-09-15 10:49:37 +0800

    task: make common content breadcrumb component

--- src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/index.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/index.tsx
@@ -21,7 +21,7 @@ import { AppState } from "../../../../../../../../shell/store/types";
 import { ItemEditHeaderActions } from "./ItemEditHeaderActions";
 import { VersionSelector } from "./VersionSelector";
 import { LanguageSelector } from "./LanguageSelector";
-import { ItemEditBreadcrumbs } from "./ItemEditBreadcrumbs";
+import { ContentBreadcrumbs } from "../../../../components/ContentBreadcrumbs";
 import { DuoModeSwitch } from "./DuoModeToggle";
 import { MoreMenu } from "./MoreMenu";
 import { DuplicateItemDialog } from "./DuplicateItemDialog";
@@ -115,7 +115,7 @@ export const ItemEditHeader = ({ saving, onSave }: HeaderProps) => {
             }}
           >
             <Box>
-              <ItemEditBreadcrumbs />
+              <ContentBreadcrumbs />
               <Typography
                 variant="h3"
                 fontWeight="700"
