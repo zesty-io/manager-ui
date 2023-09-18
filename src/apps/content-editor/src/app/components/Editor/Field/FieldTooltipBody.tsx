4684f065090ff886e64de46175cc45e9f18fcc41
Nar Cuenca <narc.ph@gmail.com>
2023-09-18 14:06:26 +0800

    task: tooltip body

--- src/apps/content-editor/src/app/components/Editor/Field/FieldTooltipBody.tsx
+++ src/apps/content-editor/src/app/components/Editor/Field/FieldTooltipBody.tsx
@@ -1,5 +1,19 @@
-import { Stack, Box, Typography, TextField, Button } from "@mui/material";
+import {
+  Stack,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  IconButton,
+  InputAdornment,
+  Tooltip,
+  Link,
+} from "@mui/material";
 import { Database } from "@zesty-io/material";
+import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
+import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
+import { Link as RouterLink } from "react-router-dom";
+import moment from "moment-timezone";
 
 import { ContentModelField } from "../../../../../../../shell/services/types";
 import { FieldIcon } from "../../../../../../schema/src/app/components/Field/FieldIcon";
@@ -39,6 +53,70 @@ export const FieldTooltipBody = ({ data }: FieldTooltipBodyProps) => {
           Edit Field
         </Button>
       </Stack>
+      <Stack gap={2.5} p={1.5}>
+        <CopyField value={data?.name} title="API / Parsley Code ID" />
+        <CopyField value={data?.ZUID} title="Field ZUID" />
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="body3" fontWeight={600} color="text.secondary">
+            Field added on {moment(data?.createdAt)?.format("MMM D, YYYY")}
+          </Typography>
+          <Link
+            component={RouterLink}
+            to="#"
+            color="text.secondary"
+            underline="always"
+            variant="body3"
+            fontWeight={600}
+            sx={{
+              textDecorationColor: "#475467",
+
+              "&:hover": {
+                textDecorationColor: "#475467",
+              },
+            }}
+          >
+            View Model Activity Log
+          </Link>
+        </Stack>
+      </Stack>
     </>
+  );
+};
+
+type CopyFieldProps = {
+  value: string;
+  title: string;
+  tooltip?: string;
+};
+const CopyField = ({ value, title, tooltip }: CopyFieldProps) => {
+  return (
+    <Stack gap={0.5}>
+      <Stack direction="row" gap={1} alignItems="center">
+        <Typography variant="body2">{title}</Typography>
+        <Tooltip title={tooltip} placement="top">
+          <InfoRoundedIcon color="action" sx={{ fontSize: 12 }} />
+        </Tooltip>
+      </Stack>
+      <TextField
+        disabled
+        value={value}
+        inputProps={{
+          sx: {
+            ":read-only": {
+              textFillColor: "#101828",
+            },
+          },
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton size="small">
+                <ContentCopyRoundedIcon color="action" />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Stack>
   );
 };
