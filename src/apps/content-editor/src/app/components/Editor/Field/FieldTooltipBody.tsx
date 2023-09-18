8fa68ad3ede81b8e0e49f635425c7138021897de
Nar Cuenca <narc.ph@gmail.com>
2023-09-18 11:14:22 +0800

    task: tooltip header

--- src/apps/content-editor/src/app/components/Editor/Field/FieldTooltipBody.tsx
+++ src/apps/content-editor/src/app/components/Editor/Field/FieldTooltipBody.tsx
@@ -1,9 +1,44 @@
-import { Paper } from "@mui/material";
+import { Stack, Box, Typography, TextField, Button } from "@mui/material";
+import { Database } from "@zesty-io/material";
+
 import { ContentModelField } from "../../../../../../../shell/services/types";
+import { FieldIcon } from "../../../../../../schema/src/app/components/Field/FieldIcon";
 
 type FieldTooltipBodyProps = {
   data: ContentModelField;
 };
 export const FieldTooltipBody = ({ data }: FieldTooltipBodyProps) => {
-  return <>Hello world</>;
+  return (
+    <>
+      <Stack
+        p={1.25}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{
+          backgroundColor: "grey.50",
+        }}
+      >
+        <Stack direction="row" alignItems="center" gap={1.5}>
+          <FieldIcon type={data?.datatype} />
+          <Stack>
+            <Typography variant="body2" fontWeight={600}>
+              {data?.label} {data?.required && "*"}
+            </Typography>
+            <Typography variant="body3" fontWeight={600} color="text.secondary">
+              {data?.datatype}
+            </Typography>
+          </Stack>
+        </Stack>
+        <Button
+          variant="contained"
+          color="inherit"
+          size="small"
+          startIcon={<Database color="action" />}
+        >
+          Edit Field
+        </Button>
+      </Stack>
+    </>
+  );
 };
--- src/shell/components/InteractiveTooltip/index.tsx
+++ src/shell/components/InteractiveTooltip/index.tsx
@@ -1,4 +1,13 @@
-import { TooltipProps, PaperProps, Tooltip, Box, Paper } from "@mui/material";
+import {
+  TooltipProps,
+  PaperProps,
+  Tooltip,
+  Box,
+  Paper,
+  ThemeProvider,
+} from "@mui/material";
+import { theme } from "@zesty-io/material";
+
 type Slots = {
   title: JSX.Element;
   body: JSX.Element;
@@ -15,26 +24,28 @@ export const InteractiveTooltip = ({
   PaperProps,
 }: InteractiveTooltipProps) => {
   return (
-    <Tooltip
-      enterDelay={800}
-      enterNextDelay={800}
-      title={
-        <Paper
-          elevation={2}
-          sx={{
-            p: 1.5,
-            mb: 1.25,
-            borderRadius: 1,
-          }}
-          {...PaperProps}
-        >
-          {slots.body}
-        </Paper>
-      }
-      components={{ Tooltip: Box }}
-      {...TooltipProps}
-    >
-      {slots.title}
-    </Tooltip>
+    <ThemeProvider theme={theme}>
+      <Tooltip
+        enterDelay={800}
+        enterNextDelay={800}
+        title={
+          <Paper
+            elevation={2}
+            sx={{
+              p: 1.5,
+              mb: 1.25,
+              borderRadius: 1,
+            }}
+            {...PaperProps}
+          >
+            {slots.body}
+          </Paper>
+        }
+        components={{ Tooltip: Box }}
+        {...TooltipProps}
+      >
+        {slots.title}
+      </Tooltip>
+    </ThemeProvider>
   );
 };
