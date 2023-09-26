6a6ce4493e598b57130a48b7608cbe4395a031a3
Nar Cuenca <narc.ph@gmail.com>
2023-09-26 13:39:14 +0800

    fix: overflowing text

--- src/apps/content-editor/src/app/components/Editor/Field/FieldTooltipBody.tsx
+++ src/apps/content-editor/src/app/components/Editor/Field/FieldTooltipBody.tsx
@@ -40,7 +40,7 @@ export const FieldTooltipBody = ({ data }: FieldTooltipBodyProps) => {
         <Stack direction="row" alignItems="center" gap={1.5}>
           <FieldIcon type={data?.datatype} />
           <Stack>
-            <Typography variant="body2" fontWeight={600}>
+            <Typography variant="body2" fontWeight={600} noWrap maxWidth={200}>
               {data?.label} {data?.required && "*"}
             </Typography>
             <Typography variant="body3" fontWeight={600} color="text.secondary">
@@ -58,6 +58,9 @@ export const FieldTooltipBody = ({ data }: FieldTooltipBodyProps) => {
               `/schema/${data?.contentModelZUID}/fields/${data?.ZUID}`
             )
           }
+          sx={{
+            flexShrink: 0,
+          }}
         >
           Edit Field
         </Button>
