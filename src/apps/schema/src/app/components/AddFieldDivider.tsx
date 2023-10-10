f4f85a2813cf852c96b8d8331aabc86df52eb3e5
Nar Cuenca <narc.ph@gmail.com>
2023-10-10 10:20:27 +0800

    fix: inconsistent line height

--- src/apps/schema/src/app/components/AddFieldDivider.tsx
+++ src/apps/schema/src/app/components/AddFieldDivider.tsx
@@ -27,11 +27,11 @@ export const AddFieldDivider = ({
   return (
     <Box
       data-cy="InBetweenFieldAddFieldBtn"
-      py="5px"
       display="flex"
       alignItems={"center"}
       sx={styles}
       onClick={onDividerClick}
+      height={12}
     >
       <IconButton
         sx={{
@@ -49,7 +49,7 @@ export const AddFieldDivider = ({
       </IconButton>
       <Box
         visibility="hidden"
-        flex={1}
+        width="100%"
         sx={{
           height: "2px",
           backgroundColor: "primary.main",
