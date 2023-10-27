c8a4026c37f3e8e5548145fa33a2ffc719bfe7b7
Nar Cuenca <narc.ph@gmail.com>
2023-10-27 15:10:00 +0800

    task: moved to one single file

--- src/shell/components/InAppAnnouncement/index.tsx
+++ src/shell/components/InAppAnnouncement/index.tsx
@@ -3,8 +3,19 @@ import { ThemeProvider } from "@mui/material";
 import { theme } from "@zesty-io/material";
 import moment from "moment";
 import { useLocalStorage } from "react-use";
+import {
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Stack,
+  Button,
+  Box,
+} from "@mui/material";
+import OpenInNewRoundedIcon from "@mui/icons-material/OpenInNewRounded";
+import ScheduledRoundedIcon from "@mui/icons-material/ScheduleRounded";
+import PlayArrowRoundedIcon from "@mui/icons-material/PlayArrowRounded";
 
-import { AnnouncementPopup } from "./AnnouncementPopup";
 import { useGetAnnouncementsQuery } from "../../services/announcements";
 
 export const InAppAnnouncement = () => {
@@ -38,6 +49,17 @@ export const InAppAnnouncement = () => {
     }
   }, [announcements]);
 
+  const onIgnoreAnnouncement = (zuid: string) => {
+    if (!readAnnouncements.includes(zuid)) {
+      setReadAnnouncements([...readAnnouncements, zuid]);
+      setActiveAnnouncementIndex(activeAnnouncementIndex + 1);
+    }
+  };
+
+  const onOpenTab = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   const announcementData = unreadAnnouncements
     ? unreadAnnouncements[activeAnnouncementIndex]
     : null;
@@ -47,15 +69,81 @@ export const InAppAnnouncement = () => {
   return (
     <ThemeProvider theme={theme}>
       {announcementData && (
-        <AnnouncementPopup
-          announcementData={announcementData}
-          onIgnoreAnnouncement={(zuid) => {
-            if (!readAnnouncements.includes(zuid)) {
-              setReadAnnouncements([...readAnnouncements, zuid]);
-              setActiveAnnouncementIndex(activeAnnouncementIndex + 1);
-            }
-          }}
-        />
+        <Dialog
+          open
+          onClose={() => onIgnoreAnnouncement(announcementData?.zuid)}
+          maxWidth="md"
+          PaperProps={{ sx: { width: 640 } }}
+        >
+          <DialogContent sx={{ p: 0 }}>
+            <Stack
+              m={2.5}
+              p={3}
+              width={600}
+              height={338}
+              sx={{ background: "linear-gradient(90deg, #ed4c0b, #fc8238)" }}
+              alignItems="center"
+              justifyContent="center"
+              boxSizing="border-box"
+            >
+              <Box
+                component="img"
+                alt="announcement-banner-image"
+                src={announcementData?.feature_image?.data[0]?.url ?? ""}
+                maxWidth="100%"
+                maxHeight="100%"
+                sx={{
+                  cursor: "pointer",
+                }}
+                onClick={() => onOpenTab(announcementData?.announcement_link)}
+              />
+            </Stack>
+            <Stack gap={1} px={2.5} pb={2.5}>
+              <Typography variant="h4" fontWeight={700}>
+                {announcementData?.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {announcementData?.description}
+              </Typography>
+            </Stack>
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: "space-between" }}>
+            <Button
+              variant="text"
+              color="inherit"
+              onClick={() => onIgnoreAnnouncement(announcementData?.zuid)}
+            >
+              Ignore
+            </Button>
+            <Stack direction="row" gap={1}>
+              <Button
+                variant="outlined"
+                startIcon={<OpenInNewRoundedIcon />}
+                onClick={() => onOpenTab(announcementData?.announcement_link)}
+              >
+                Read Announcement
+              </Button>
+              {announcementData?.cta_type === "play_video" && (
+                <Button
+                  variant="contained"
+                  startIcon={<PlayArrowRoundedIcon />}
+                  onClick={() => onOpenTab(announcementData?.video_link)}
+                >
+                  Show Video
+                </Button>
+              )}
+              {announcementData?.cta_type === "schedule_training" && (
+                <Button
+                  variant="contained"
+                  startIcon={<ScheduledRoundedIcon />}
+                  onClick={() => onOpenTab(announcementData?.training_link)}
+                >
+                  Schedule Training
+                </Button>
+              )}
+            </Stack>
+          </DialogActions>
+        </Dialog>
       )}
     </ThemeProvider>
   );
