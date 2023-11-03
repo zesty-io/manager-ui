0b3f40b3a83d49f3e366bd7fe9685b41b5db7c12
Nar Cuenca <narc.ph@gmail.com>
2023-11-03 09:45:33 +0800

    task: only show latest announcement

--- src/shell/components/InAppAnnouncement/index.tsx
+++ src/shell/components/InAppAnnouncement/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useMemo } from "react";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@zesty-io/material";
 import moment from "moment";
@@ -19,132 +19,122 @@ import PlayArrowRoundedIcon from "@mui/icons-material/PlayArrowRounded";
 import { useGetAnnouncementsQuery } from "../../services/marketing";
 
 export const InAppAnnouncement = () => {
-  const [activeAnnouncementIndex, setActiveAnnouncementIndex] = useState(0);
   const [readAnnouncements, setReadAnnouncements] = useLocalStorage(
     "zesty:readAnnouncements",
     []
   );
   const { data: announcements } = useGetAnnouncementsQuery();
 
-  const unreadAnnouncements = useMemo(() => {
+  const latestAnnouncement = useMemo(() => {
     if (announcements?.length) {
-      return announcements
-        .filter((announcement) => {
-          const isInPublishRange = moment().isBetween(
-            moment(announcement?.start_date_and_time),
-            moment(announcement?.end_date_and_time)
-          );
-
-          return (
-            isInPublishRange && !readAnnouncements.includes(announcement?.zuid)
-          );
-        })
-        ?.sort((a, b) => moment(b.created_at).diff(a.created_at));
+      return [...announcements].sort((a, b) =>
+        moment(b.created_at).diff(moment(a.created_at))
+      )?.[0];
     }
   }, [announcements]);
 
   const onIgnoreAnnouncement = (zuid: string) => {
     if (!readAnnouncements.includes(zuid)) {
       setReadAnnouncements([...readAnnouncements, zuid]);
-      setActiveAnnouncementIndex(activeAnnouncementIndex + 1);
     }
   };
 
-  const announcementData = unreadAnnouncements
-    ? unreadAnnouncements[activeAnnouncementIndex]
-    : null;
+  if (
+    !latestAnnouncement ||
+    readAnnouncements.includes(latestAnnouncement.zuid)
+  ) {
+    return <></>;
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      {announcementData && (
-        <Dialog
-          open
-          onClose={() => onIgnoreAnnouncement(announcementData?.zuid)}
-          maxWidth="md"
-          PaperProps={{ sx: { width: 640 } }}
-          data-cy="AnnouncementPopup"
-        >
-          <DialogContent sx={{ p: 0 }}>
-            <Stack
-              component="a"
-              href={announcementData?.announcement_link}
-              target="_blank"
-              m={2.5}
-              p={2.5}
-              width={600}
-              height={340}
-              sx={{
-                background: "linear-gradient(90deg, #EC4A0A 0%, #FD853A 100%)",
-              }}
-              alignItems="center"
-              justifyContent="center"
-              boxSizing="border-box"
-            >
-              <Box
-                component="img"
-                alt="announcement-banner-image"
-                src={
-                  `${announcementData?.feature_image?.data[0]?.url}?fit=cover&width=560&height=300` ??
-                  ""
-                }
-                maxWidth="100%"
-                maxHeight="100%"
-              />
-            </Stack>
-            <Stack gap={1} px={2.5} pb={2.5}>
-              <Typography variant="h4" fontWeight={700}>
-                {announcementData?.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {announcementData?.description}
-              </Typography>
-            </Stack>
-          </DialogContent>
-          <DialogActions sx={{ justifyContent: "space-between" }}>
+      <Dialog
+        open
+        onClose={() => onIgnoreAnnouncement(latestAnnouncement?.zuid)}
+        maxWidth="md"
+        PaperProps={{ sx: { width: 640 } }}
+        data-cy="AnnouncementPopup"
+      >
+        <DialogContent sx={{ p: 0 }}>
+          <Stack
+            component="a"
+            href={latestAnnouncement?.announcement_link}
+            target="_blank"
+            m={2.5}
+            p={2.5}
+            width={600}
+            height={340}
+            sx={{
+              background: "linear-gradient(90deg, #EC4A0A 0%, #FD853A 100%)",
+            }}
+            alignItems="center"
+            justifyContent="center"
+            boxSizing="border-box"
+          >
+            <Box
+              component="img"
+              alt="announcement-banner-image"
+              src={
+                `${latestAnnouncement?.feature_image?.data[0]?.url}?fit=cover&width=560&height=300` ??
+                ""
+              }
+              maxWidth="100%"
+              maxHeight="100%"
+            />
+          </Stack>
+          <Stack gap={1} px={2.5} pb={2.5}>
+            <Typography variant="h4" fontWeight={700}>
+              {latestAnnouncement?.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {latestAnnouncement?.description}
+            </Typography>
+          </Stack>
+        </DialogContent>
+        <DialogActions sx={{ justifyContent: "space-between" }}>
+          <Button
+            variant="text"
+            color="inherit"
+            onClick={() => onIgnoreAnnouncement(latestAnnouncement?.zuid)}
+            data-cy="IgnoreAnnouncementButton"
+          >
+            Ignore
+          </Button>
+          <Stack direction="row" gap={1}>
             <Button
-              variant="text"
-              color="inherit"
-              onClick={() => onIgnoreAnnouncement(announcementData?.zuid)}
-              data-cy="IgnoreAnnouncementButton"
+              variant="outlined"
+              startIcon={<OpenInNewRoundedIcon />}
+              disabled={!latestAnnouncement?.announcement_link}
+              href={latestAnnouncement?.announcement_link}
+              target="_blank"
             >
-              Ignore
+              Read Announcement
             </Button>
-            <Stack direction="row" gap={1}>
-              <Button
-                variant="outlined"
-                startIcon={<OpenInNewRoundedIcon />}
-                disabled={!announcementData?.announcement_link}
-                href={announcementData?.announcement_link}
-                target="_blank"
-              >
-                Read Announcement
-              </Button>
-              {announcementData?.cta_type === "play_video" &&
-                announcementData?.video_link && (
-                  <Button
-                    variant="contained"
-                    startIcon={<PlayArrowRoundedIcon />}
-                    href={announcementData?.video_link}
-                    target="_blank"
-                  >
-                    Show Video
-                  </Button>
-                )}
-              {announcementData?.cta_type === "schedule_training" &&
-                announcementData?.training_link && (
-                  <Button
-                    variant="contained"
-                    startIcon={<ScheduledRoundedIcon />}
-                    href={announcementData?.training_link}
-                    target="_blank"
-                  >
-                    Schedule Training
-                  </Button>
-                )}
-            </Stack>
-          </DialogActions>
-        </Dialog>
-      )}
+            {latestAnnouncement?.cta_type === "play_video" &&
+              latestAnnouncement?.video_link && (
+                <Button
+                  variant="contained"
+                  startIcon={<PlayArrowRoundedIcon />}
+                  href={latestAnnouncement?.video_link}
+                  target="_blank"
+                >
+                  Show Video
+                </Button>
+              )}
+            {latestAnnouncement?.cta_type === "schedule_training" &&
+              latestAnnouncement?.training_link && (
+                <Button
+                  variant="contained"
+                  startIcon={<ScheduledRoundedIcon />}
+                  href={latestAnnouncement?.training_link}
+                  target="_blank"
+                >
+                  Schedule Training
+                </Button>
+              )}
+          </Stack>
+        </DialogActions>
+      </Dialog>
     </ThemeProvider>
   );
 };
