6f7a95403f6057cd87f9908e9d32b5dc7e1167c6
Nar Cuenca <narc.ph@gmail.com>
2023-11-13 10:18:32 +0800

    fix: update error message

--- src/shell/store/content.js
+++ src/shell/store/content.js
@@ -704,7 +704,7 @@ export function fetchItemPublishings() {
             notify({
               kind: "warn",
               message: `${res.status}:Failed to fetch item publishings${
-                res.error && ": " + res.error
+                res.error ? ": " + res.error : ""
               }`,
             })
           );
