57fb2bc641f1798f673e9e45f978531b834efa1a
Nar Cuenca <narc.ph@gmail.com>
2023-11-21 15:20:29 +0800

    task: create custom slash commands plugin

---
+++ src/shell/components/FieldTypeTinyMCE/plugins/slashcommands.ts
@@ -0,0 +1,174 @@
+import tinymce from "tinymce/tinymce";
+
+tinymce.PluginManager.add("slashcommands", (editor) => {
+  const insertActions = [
+    {
+      text: "Paragraph",
+      icon: "p",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<p>Paragraph</p>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Heading 1",
+      icon: "h1",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<h1>Heading 1</h1>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Heading 2",
+      icon: "h2",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<h2>Heading 2</h2>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Heading 3",
+      icon: "h3",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<h3>Heading 3</h3>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Heading 4",
+      icon: "h4",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<h4>Heading 4</h4>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Heading 5",
+      icon: "h5",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<h5>Heading 5</h5>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Heading 6",
+      icon: "h6",
+      action: () => {
+        editor.execCommand("mceInsertContent", false, "<h6>Heading 6</h6>");
+        editor.selection.select(editor.selection.getNode());
+      },
+    },
+    {
+      text: "Link",
+      icon: "link",
+      action: () => {
+        editor.execCommand("mceLink");
+      },
+    },
+    {
+      text: "Quote",
+      icon: "quote",
+      action: () => {
+        editor.execCommand("mceBlockQuote");
+      },
+    },
+    {
+      text: "Code Block",
+      icon: "code-sample",
+      action: () => {
+        editor.execCommand("CodeSample");
+      },
+    },
+    {
+      text: "Bulleted list",
+      icon: "unordered-list",
+      action: () => {
+        editor.execCommand("InsertUnorderedList");
+      },
+    },
+    {
+      text: "Time",
+      icon: "ordered-list",
+      action: () => {
+        editor.execCommand("mceInsertTime");
+      },
+    },
+    {
+      text: "Special Character",
+      icon: "insert-character",
+      action: () => {
+        editor.execCommand("mceShowCharmap");
+      },
+    },
+    {
+      text: "Emoji",
+      icon: "emoji",
+      action: () => {
+        editor.execCommand("mceEmoticons");
+      },
+    },
+    {
+      text: "Image",
+      icon: "image",
+      action: () => {
+        editor.execCommand("mceZestyMediaApp", false, "Image");
+      },
+    },
+    {
+      text: "Video",
+      icon: "emoji",
+      action: () => {
+        editor.execCommand("mceZestyMediaApp", false, "Video");
+      },
+    },
+    {
+      text: "Table",
+      icon: "table",
+      action: () => {
+        editor.execCommand("mceInsertTableDialog");
+      },
+    },
+    {
+      text: "Embed",
+      icon: "zesty-embed",
+      action: () => {
+        editor.execCommand("mceSocialMediaEmbed");
+      },
+    },
+  ];
+
+  // Register the slash commands autocompleter
+  editor.ui.registry.addAutocompleter("slashcommands", {
+    ch: "/",
+    minChars: 0,
+    columns: 1,
+    fetch: (pattern) => {
+      const matchedActions = insertActions.filter((action) => {
+        return action.text.toLowerCase().indexOf(pattern.toLowerCase()) !== -1;
+      });
+
+      return new Promise((resolve) => {
+        const results = matchedActions.map((action) => {
+          return {
+            meta: action,
+            text: action.text,
+            icon: action.icon,
+            value: action.text,
+          };
+        });
+
+        resolve(results);
+      });
+    },
+    onAction: (autocompleteApi, range, value, meta) => {
+      editor.selection.setRng(range);
+      // Some actions don't delete the "slash", so we delete all the slash
+      // command content before performing the action
+      editor.execCommand("Delete");
+      meta.action();
+      autocompleteApi.hide();
+    },
+  });
+
+  return {};
+});
