5c533efcef67ffc8a735b562a1222d27199e9173
Nar Cuenca <28705606+theofficialnar@users.noreply.github.com>
2023-11-28 06:37:18 +0800

    Chore: Remove unused components (#2327)
    * chore: remove unused components
    
    * task: removed unused componens

--- src/shell/components/NotFound/NotFound.js
+++
@@ -1,9 +0,0 @@
-import styles from "./NotFound.less";
-
-export default function NotFound(props) {
-  return (
-    <section className={styles.NotFound}>
-      <h1 className={styles.display}>{props.message}</h1>
-    </section>
-  );
-}
