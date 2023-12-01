c9ce4bf0af8c018d74f2bae9009edc3261900884
Andres Galindo <agalin920@gmail.com>
2023-11-30 16:05:21 -0800

    Minor media field enhancments (#2362)

--- src/apps/content-editor/src/app/components/FieldTypeMedia.tsx
+++ src/apps/content-editor/src/app/components/FieldTypeMedia.tsx
@@ -498,6 +498,7 @@ const MediaItem = ({
               ref={imageEl}
               onLoad={handleImageLoad}
               onError={handleImageError}
+              draggable={false}
             />
           ) : (
             <Box width="80px" height="80px"></Box>
@@ -524,7 +525,12 @@ const MediaItem = ({
             </Typography>
           ) : (
             <Box>
-              <Typography variant="body2" color="text.primary" noWrap>
+              <Typography
+                variant="body2"
+                color="text.primary"
+                fontWeight={600}
+                noWrap
+              >
                 {data?.filename}
               </Typography>
               <Typography variant="body2" color="text.secondary" noWrap>
@@ -532,7 +538,7 @@ const MediaItem = ({
               </Typography>
             </Box>
           )}
-          <Box display="flex" gap={1}>
+          <Box display="flex" gap={1} justifyContent="flex-end">
             <Tooltip title="Replace File" placement="bottom" enterDelay={800}>
               <IconButton
                 size="small"
