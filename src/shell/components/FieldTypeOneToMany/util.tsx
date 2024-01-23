b85e6f37792b84592ff4326d7133c7467f1f886d
Nar Cuenca <narc.ph@gmail.com>
2024-01-24 07:53:06 +0800

    task: revert map change

--- src/shell/components/FieldTypeOneToMany/util.tsx
+++ src/shell/components/FieldTypeOneToMany/util.tsx
@@ -19,8 +19,8 @@ export const resolveRelatedOptions = (
   }
 
   return Object.keys(items)
-    .map((itemZUID) => {
-      if (
+    .filter((itemZUID) => {
+      return (
         items[itemZUID] &&
         items[itemZUID].meta &&
         items[itemZUID].meta.contentModelZUID === modelZUID &&
@@ -28,44 +28,43 @@ export const resolveRelatedOptions = (
         (langID === items[itemZUID].meta.langID ||
           // ...unless option already selected
           value?.split(",").includes(itemZUID))
-      ) {
-        // Matching ItemZUID to languageZUID to get language code {key} to display in dropdown
-        let langCode = "";
-        if (items[itemZUID].siblings) {
-          const match = Object.entries(items[itemZUID].siblings).find(
-            (arr) => items[itemZUID].meta.ZUID === arr[1]
-          );
-          if (match) {
-            langCode = match[0];
-          }
+      );
+    })
+    .map((itemZUID) => {
+      // Matching ItemZUID to languageZUID to get language code {key} to display in dropdown
+      let langCode = "";
+      if (items[itemZUID].siblings) {
+        const match = Object.entries(items[itemZUID].siblings).find(
+          (arr) => items[itemZUID].meta.ZUID === arr[1]
+        );
+        if (match) {
+          langCode = match[0];
         }
-
-        return {
-          value: itemZUID,
-          inputLabel: items[itemZUID].data[field.name] || "",
-          component: (
-            <ResolvedOption
-              modelZUID={modelZUID}
-              itemZUID={itemZUID}
-              html={
-                <div
-                  style={{
-                    display: "inline-grid",
-                    gridTemplateColumns: "minmax(50px, 100%) 41px",
-                  }}
-                >
-                  <span
-                    style={{ textOverflow: "ellipsis", overflow: "hidden" }}
-                  >
-                    {items[itemZUID].data[field.name]}
-                  </span>
-                  <em className={styles.Language}>&nbsp;{langCode}</em>
-                </div>
-              }
-            />
-          ),
-        };
       }
+
+      return {
+        value: itemZUID,
+        inputLabel: items[itemZUID].data[field.name] || "",
+        component: (
+          <ResolvedOption
+            modelZUID={modelZUID}
+            itemZUID={itemZUID}
+            html={
+              <div
+                style={{
+                  display: "inline-grid",
+                  gridTemplateColumns: "minmax(50px, 100%) 41px",
+                }}
+              >
+                <span style={{ textOverflow: "ellipsis", overflow: "hidden" }}>
+                  {items[itemZUID].data[field.name]}
+                </span>
+                <em className={styles.Language}>&nbsp;{langCode}</em>
+              </div>
+            }
+          />
+        ),
+      };
     })
     .sort((a, b) => (a.inputLabel > b.inputLabel ? 1 : -1));
 };
