06f938000a101fb16d96ac45f83a0491283872c0
Nar Cuenca <narc.ph@gmail.com>
2024-02-01 10:36:53 +0800

    task: only allow owners to invite new owners

--- src/shell/components/InviteMembersModal/RoleSelectModal.tsx
+++ src/shell/components/InviteMembersModal/RoleSelectModal.tsx
@@ -7,13 +7,15 @@ import {
   Typography,
 } from "@mui/material";
 import { alpha } from "@mui/material/styles";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import CheckIcon from "@mui/icons-material/Check";
 import AdminPanelSettingsRoundedIcon from "@mui/icons-material/AdminPanelSettingsRounded";
 import { RoleAccessInfo } from "./RoleAccessInfo";
 import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import CodeRoundedIcon from "@mui/icons-material/CodeRounded";
 import RecommendRoundedIcon from "@mui/icons-material/RecommendRounded";
+
+import { useGetCurrentUserRolesQuery } from "../../services/accounts";
 
 const roles = [
   {
@@ -94,44 +96,59 @@ interface Props {
 }
 
 export const RoleSelectModal = ({ role, onSelect, onClose }: Props) => {
+  const { data: currentUserRoles } = useGetCurrentUserRolesQuery();
   const [hoveredRoleIndex, setHoveredRoleIndex] = useState(role);
+
+  const isOwner = useMemo(() => {
+    if (currentUserRoles?.length) {
+      return currentUserRoles.some((role) =>
+        ["owner"].includes(role.name?.toLowerCase())
+      );
+    }
+  }, [currentUserRoles]);
 
   return (
     <Dialog open={true} onClose={onClose} fullWidth maxWidth={"xs"}>
       <Box display="flex" sx={{ height: "400px" }}>
         <Box sx={{ mt: 1 }}>
-          {roles.map((roleItem, index) => (
-            <ListItemButton
-              onMouseLeave={() => setHoveredRoleIndex(role)}
-              onMouseEnter={() => setHoveredRoleIndex(index)}
-              onClick={() => onSelect(index)}
-              sx={{
-                width: "171px",
-                ...(role === index && {
-                  backgroundColor: (theme) =>
-                    alpha(
-                      theme.palette.primary.main,
-                      theme.palette.action.hoverOpacity
-                    ),
-                }),
-              }}
-            >
-              <ListItemText
-                primary={roleItem.name}
-                primaryTypographyProps={{
-                  variant: "body1",
-                }}
-              />
-              <ListItemIcon
+          {roles.map((roleItem, index) => {
+            if (roleItem.name === "Owner" && !isOwner) {
+              return <></>;
+            }
+
+            return (
+              <ListItemButton
+                onMouseLeave={() => setHoveredRoleIndex(role)}
+                onMouseEnter={() => setHoveredRoleIndex(index)}
+                onClick={() => onSelect(index)}
                 sx={{
-                  visibility: role !== index && "hidden",
-                  justifyContent: "flex-end",
+                  width: "171px",
+                  ...(role === index && {
+                    backgroundColor: (theme) =>
+                      alpha(
+                        theme.palette.primary.main,
+                        theme.palette.action.hoverOpacity
+                      ),
+                  }),
                 }}
               >
-                <CheckIcon color="primary" />
-              </ListItemIcon>
-            </ListItemButton>
-          ))}
+                <ListItemText
+                  primary={roleItem.name}
+                  primaryTypographyProps={{
+                    variant: "body1",
+                  }}
+                />
+                <ListItemIcon
+                  sx={{
+                    visibility: role !== index && "hidden",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  <CheckIcon color="primary" />
+                </ListItemIcon>
+              </ListItemButton>
+            );
+          })}
         </Box>
         <Box sx={{ mt: 2, px: 2 }}>
           <Box display="flex" gap={1.5} alignItems="center">
