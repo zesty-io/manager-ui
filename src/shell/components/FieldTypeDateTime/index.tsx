eaccd8b8e794ba943cf79d02984b20a3594d5bb0
Nar Cuenca <narc.ph@gmail.com>
2024-04-03 11:24:51 +0800

    task: create new field date time component

--- src/shell/components/FieldTypeDateTime/index.tsx
+++ src/shell/components/FieldTypeDateTime/index.tsx
@@ -4,30 +4,95 @@ import {
   DesktopDateTimePicker,
   DesktopDateTimePickerProps,
 } from "@mui/x-date-pickers";
-import { TextField } from "@mui/material";
+import { TextField, Autocomplete } from "@mui/material";
+import moment from "moment";
 
-export interface FieldTypeDateTimeProps
-  extends Omit<DesktopDateTimePickerProps<Date, Date>, "renderInput"> {
+import { FieldTypeDate } from "../FieldTypeDate";
+import { getTimeOptions } from "./util";
+
+// export interface FieldTypeDateTimeProps
+//   extends Omit<DesktopDateTimePickerProps<Date, Date>, "renderInput"> {
+//   required?: boolean;
+//   name: string;
+//   error?: boolean;
+// }
+type FieldTypeDateTimeProps = {
   required?: boolean;
   name: string;
   error?: boolean;
-}
+  value: Date;
+  onChange: (date: Date) => void;
+};
+
+const TIME_OPTIONS = getTimeOptions();
 
 export const FieldTypeDateTime = ({
   required,
   error,
-  ...props
-}: FieldTypeDateTimeProps) => {
+  name,
+  value,
+  onChange,
+}: // ...props
+FieldTypeDateTimeProps) => {
+  const handleChange = (date: Date) => {
+    console.log(date);
+  };
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DesktopDateTimePicker
-        data-testid="zds-date-time-picker"
-        renderInput={(params) => (
-          <TextField {...params} fullWidth size="small" error={error} />
-        )}
-        // Spread props at the end to allow prop overrides
-        {...props}
-      />
-    </LocalizationProvider>
+    <FieldTypeDate
+      name={name}
+      required={required}
+      value={value ? new Date(value) : null}
+      // format="MMM dd, yyyy"
+      // onChange={(date) => onDateChange(date, name, datatype)}
+      onChange={handleChange}
+      error={error}
+      slots={{
+        timePicker: (
+          <Autocomplete
+            disableClearable
+            // open
+            // freeSolo
+            forcePopupIcon={false}
+            renderInput={(params) => (
+              <TextField placeholder="HH:MM" {...params} />
+            )}
+            options={TIME_OPTIONS}
+            getOptionLabel={(option) => option.inputValue}
+            sx={{
+              width: 96,
+              "& .MuiAutocomplete-inputRoot": {
+                py: 0.75,
+                px: 1,
+
+                "& input.MuiOutlinedInput-input.MuiAutocomplete-input": {
+                  p: 0,
+                  height: 28,
+                },
+              },
+            }}
+            slotProps={{
+              paper: {
+                sx: {
+                  width: 184,
+                },
+              },
+            }}
+          />
+        ),
+      }}
+    />
   );
+  // return (
+  //   <LocalizationProvider dateAdapter={AdapterDateFns}>
+  //     <DesktopDateTimePicker
+  //       data-testid="zds-date-time-picker"
+  //       renderInput={(params) => (
+  //         <TextField {...params} fullWidth size="small" error={error} />
+  //       )}
+  //       // Spread props at the end to allow prop overrides
+  //       {...props}
+  //     />
+  //   </LocalizationProvider>
+  // );
 };
