02e1ccb4f2eccaa4216885d155d6ca193e8b803f
Nar Cuenca <narc.ph@gmail.com>
2024-04-04 17:41:51 +0800

    task: show error tooltip and border

--- src/shell/components/FieldTypeDateTime/index.tsx
+++ src/shell/components/FieldTypeDateTime/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState, useMemo, useRef } from "react";
-import { TextField, Autocomplete, Typography } from "@mui/material";
+import { TextField, Autocomplete, Typography, Tooltip } from "@mui/material";
 import moment from "moment";
 
 import { FieldTypeDate } from "../FieldTypeDate";
 import { TIME_OPTIONS, getFilteredTimeOptions } from "./util";
+
+const TIME_FORMAT_REGEX = /^((1[0-2]|0?[1-9]):([0-5][0-9]) ?([ap][m]))$/gi;
 
 type FieldTypeDateTimeProps = {
   required?: boolean;
@@ -24,6 +26,7 @@ export const FieldTypeDateTime = ({
   const [timeKeyCount, setTimeKeyCount] = useState(0);
   const [isTimeFieldActive, setIsTimeFieldActive] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [invalidInput, setInvalidInput] = useState(false);
 
   const [dateString, timeString] = value?.split(" ") ?? [null, null];
 
@@ -38,7 +41,15 @@ export const FieldTypeDateTime = ({
   }, [value]);
 
   const filteredTimeOptions = useMemo(() => {
-    return getFilteredTimeOptions(inputValue);
+    const timeOptions = getFilteredTimeOptions(inputValue.trim());
+
+    if (!timeOptions?.length) {
+      setInvalidInput(!TIME_FORMAT_REGEX.test(inputValue));
+    } else {
+      setInvalidInput(false);
+    }
+
+    return timeOptions;
   }, [inputValue]);
 
   return (
@@ -64,72 +75,83 @@ export const FieldTypeDateTime = ({
         error={error}
         slots={{
           timePicker: (
-            <Autocomplete
-              disableClearable
-              key={timeKeyCount}
-              open={isTimeFieldActive}
-              value={TIME_OPTIONS?.find((time) => time.value === timeString)}
-              forcePopupIcon={false}
-              inputValue={inputValue}
-              renderInput={(params) => (
-                <TextField
-                  ref={timeFieldRef}
-                  placeholder="HH:MM"
-                  onClick={() => {
-                    setIsTimeFieldActive(true);
-                    if (!dateString && !timeString) {
-                      onChange(
-                        `${moment().format("yyyy-MM-DD")} 00:00:00.000000`
-                      );
-                    }
-                  }}
-                  onBlur={() => {
-                    setIsTimeFieldActive(false);
-                    // Check what's the closest match to the user input then select that on blur
-                    const matchedTimeOption =
-                      getFilteredTimeOptions(inputValue);
+            <Tooltip
+              open={invalidInput}
+              title="Invalid Time"
+              placement="top-start"
+            >
+              <Autocomplete
+                disableClearable
+                key={timeKeyCount}
+                open={isTimeFieldActive}
+                value={TIME_OPTIONS?.find((time) => time.value === timeString)}
+                forcePopupIcon={false}
+                inputValue={inputValue}
+                renderInput={(params) => (
+                  <TextField
+                    ref={timeFieldRef}
+                    placeholder="HH:MM"
+                    error={invalidInput}
+                    onClick={() => {
+                      setIsTimeFieldActive(true);
+                      if (!dateString && !timeString) {
+                        onChange(
+                          `${moment().format("yyyy-MM-DD")} 00:00:00.000000`
+                        );
+                      }
+                    }}
+                    onBlur={() => {
+                      setIsTimeFieldActive(false);
+                      // Check what's the closest match to the user input then select that on blur
+                      const matchedTimeOption =
+                        getFilteredTimeOptions(inputValue);
 
-                    console.log("possible value on blur", matchedTimeOption);
+                      if (matchedTimeOption?.length) {
+                        onChange(`${dateString} ${matchedTimeOption[0].value}`);
+                      }
+                    }}
+                    sx={{
+                      "& .Mui-focused.MuiAutocomplete-inputRoot fieldset.MuiOutlinedInput-notchedOutline":
+                        {
+                          borderColor: invalidInput
+                            ? (theme) => theme.palette.error.main
+                            : (theme) => theme.palette.primary.main,
+                        },
+                    }}
+                    {...params}
+                  />
+                )}
+                options={filteredTimeOptions}
+                getOptionLabel={(option) => option.inputValue}
+                filterOptions={(e) => e}
+                onChange={(_, time) => {
+                  onChange(`${dateString} ${time.value}`);
+                  setIsTimeFieldActive(false);
+                }}
+                onInputChange={(_, value) => {
+                  setInputValue(value);
+                }}
+                sx={{
+                  width: 96,
+                  "& .MuiAutocomplete-inputRoot": {
+                    py: 0.75,
+                    px: 1,
 
-                    if (matchedTimeOption?.length) {
-                      onChange(`${dateString} ${matchedTimeOption[0].value}`);
-                    } else {
-                      // Show error
-                    }
-                  }}
-                  {...params}
-                />
-              )}
-              options={filteredTimeOptions}
-              getOptionLabel={(option) => option.inputValue}
-              filterOptions={(e) => e}
-              onChange={(_, time) => {
-                onChange(`${dateString} ${time.value}`);
-                setIsTimeFieldActive(false);
-              }}
-              onInputChange={(_, value) => {
-                setInputValue(value);
-              }}
-              sx={{
-                width: 96,
-                "& .MuiAutocomplete-inputRoot": {
-                  py: 0.75,
-                  px: 1,
-
-                  "& input.MuiOutlinedInput-input.MuiAutocomplete-input": {
-                    p: 0,
-                    height: 28,
+                    "& input.MuiOutlinedInput-input.MuiAutocomplete-input": {
+                      p: 0,
+                      height: 28,
+                    },
                   },
-                },
-              }}
-              slotProps={{
-                paper: {
-                  sx: {
-                    width: 184,
+                }}
+                slotProps={{
+                  paper: {
+                    sx: {
+                      width: 184,
+                    },
                   },
-                },
-              }}
-            />
+                }}
+              />
+            </Tooltip>
           ),
         }}
       />
