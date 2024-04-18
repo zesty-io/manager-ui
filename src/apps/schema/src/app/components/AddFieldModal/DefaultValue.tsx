09f51d89622676c7bfd4b434c05771b0cd37cd07
Andres <agalin920@gmail.com>
2024-04-18 10:10:31 -0700

    fix spacing of body3

--- src/apps/schema/src/app/components/AddFieldModal/DefaultValue.tsx
+++ src/apps/schema/src/app/components/AddFieldModal/DefaultValue.tsx
@@ -62,7 +62,7 @@ export const DefaultValue = ({
           variant="body3"
           color="text.secondary"
           fontWeight="600"
-          sx={{ mb: 1 }}
+          sx={{ mb: 1, display: "block" }}
         >
           Set a predefined value for this field
         </Typography>
