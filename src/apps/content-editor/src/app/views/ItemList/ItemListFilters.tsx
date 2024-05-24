c036bf84f499152a63ad766445c88251a51ef090
Nar Cuenca <narc.ph@gmail.com>
2024-05-24 11:07:13 +0800

    task: create date filter hook

--- src/apps/content-editor/src/app/views/ItemList/ItemListFilters.tsx
+++ src/apps/content-editor/src/app/views/ItemList/ItemListFilters.tsx
@@ -1,14 +1,13 @@
 import { Box, Menu, MenuItem, Button } from "@mui/material";
 import {
   DateFilter,
-  DateRangeFilterValue,
   FilterButton,
 } from "../../../../../../shell/components/Filters";
 import { useEffect, useMemo, useState } from "react";
 import { useParams } from "../../../../../../shell/hooks/useParams";
-import { DateFilterValue } from "../../../../../../shell/components/Filters/DateFilter";
 import { ArrowDropDownOutlined } from "@mui/icons-material";
 import { useGetLangsQuery } from "../../../../../../shell/services/instance";
+import { useDateFilterParams } from "../../../../../../shell/hooks/useDateFilterParams";
 
 const SORT_ORDER = {
   dateSaved: "Date Saved",
@@ -52,119 +51,8 @@ export const ItemListFilters = () => {
     id: "",
   });
   const [params, setParams] = useParams();
-
-  const handleDateFilterChanged = (dateFilter: DateFilterValue) => {
-    switch (dateFilter.type) {
-      case "daterange": {
-        const value = dateFilter.value as DateRangeFilterValue;
-
-        setParams(value.to, "to");
-        setParams(value.from, "from");
-        setParams(null, "datePreset");
-        return;
-      }
-
-      case "on": {
-        const value = dateFilter.value as string;
-
-        setParams(value, "to");
-        setParams(value, "from");
-        setParams(null, "datePreset");
-        return;
-      }
-      case "before": {
-        const value = dateFilter.value as string;
-
-        setParams(value, "to");
-        setParams(null, "from");
-        setParams(null, "datePreset");
-        return;
-      }
-      case "after": {
-        const value = dateFilter.value as string;
-
-        setParams(value, "from");
-        setParams(null, "to");
-        setParams(null, "datePreset");
-        return;
-      }
-      case "preset": {
-        const value = dateFilter.value as string;
-
-        setParams(value, "datePreset");
-        setParams(null, "to");
-        setParams(null, "from");
-        return;
-      }
-
-      default: {
-        setParams(null, "to");
-        setParams(null, "from");
-        setParams(null, "datePreset");
-        return;
-      }
-    }
-  };
-
-  const activeDateFilter: DateFilterValue = useMemo(() => {
-    const isPreset = !!params.get("datePreset");
-    const isBefore = !!params.get("to") && !!!params.get("from");
-    const isAfter = !!params.get("from") && !!!params.get("to");
-    const isOn =
-      !!params.get("to") &&
-      !!params.get("from") &&
-      params.get("to") === params.get("from");
-    const isDateRange =
-      !!params.get("to") &&
-      !!params.get("from") &&
-      params.get("to") !== params.get("from");
-
-    if (isPreset) {
-      return {
-        type: "preset",
-        value: params.get("datePreset"),
-      };
-    }
-
-    if (isBefore) {
-      return {
-        type: "before",
-        value: params.get("to"),
-      };
-    }
-
-    if (isAfter) {
-      return {
-        type: "after",
-        value: params.get("from"),
-      };
-    }
-
-    if (isOn) {
-      return {
-        type: "on",
-        value: params.get("from"),
-      };
-    }
-
-    if (isDateRange) {
-      return {
-        type: "daterange",
-        value: {
-          from: params.get("from"),
-          to: params.get("to"),
-        },
-      };
-    }
-
-    return {
-      type: "",
-      value: "",
-    };
-  }, [params]);
-
+  const [activeDateFilter, setActiveDateFilter] = useDateFilterParams();
   const { data: languages } = useGetLangsQuery({});
-
   const activeLanguageCode = params.get("language");
 
   useEffect(() => {
@@ -256,7 +144,7 @@ export const ItemListFilters = () => {
       <DateFilter
         withDateRange
         defaultButtonText="Date Saved"
-        onChange={(value) => handleDateFilterChanged(value)}
+        onChange={(value) => setActiveDateFilter(value)}
         value={activeDateFilter}
       />
       <Button
