2e7c2fa5f38ae072142acc8506ef649bb987759e
Nar Cuenca <narc.ph@gmail.com>
2024-05-29 15:37:14 +0800

    task: add api endpoints menu items

--- src/apps/content-editor/src/app/views/ItemList/ItemListActions.tsx
+++ src/apps/content-editor/src/app/views/ItemList/ItemListActions.tsx
@@ -6,6 +6,8 @@ import {
   Menu,
   MenuItem,
   ListItemIcon,
+  Typography,
+  Chip,
 } from "@mui/material";
 import { Database, IconButton } from "@zesty-io/material";
 import {
@@ -16,21 +18,47 @@ import {
   CheckRounded,
   CodeRounded,
   ContentCopyRounded,
+  BoltRounded,
+  KeyboardArrowRightRounded,
+  DataObjectRounded,
+  VisibilityRounded,
+  DesignServicesRounded,
 } from "@mui/icons-material";
 import { forwardRef, useState, useCallback } from "react";
 import { useHistory, useParams as useRouterParams } from "react-router";
 import { useFilePath } from "../../../../../../shell/hooks/useFilePath";
 import { useParams } from "../../../../../../shell/hooks/useParams";
 import { debounce } from "lodash";
+import { useGetContentModelsQuery } from "../../../../../../shell/services/instance";
+import { useGetDomainsQuery } from "../../../../../../shell/services/accounts";
+import { ApiType } from "../../../../../schema/src/app/components/ModelApi";
+import { AppState } from "../../../../../../shell/store/types";
+import { useSelector } from "react-redux";
 
 export const ItemListActions = forwardRef((props, ref) => {
   const { modelZUID } = useRouterParams<{ modelZUID: string }>();
+  const { data: contentModels } = useGetContentModelsQuery();
+  const { data: domains } = useGetDomainsQuery();
   const history = useHistory();
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement>(null);
   const codePath = useFilePath(modelZUID);
   const [isCopied, setIsCopied] = useState(false);
   const [params, setParams] = useParams();
   const [searchTerm, setSearchTerm] = useState(params.get("search") || "");
+  const instance = useSelector((state: AppState) => state.instance);
+  const [showApiEndpoints, setShowApiEndpoints] = useState<null | HTMLElement>(
+    null
+  );
+  const [apiEndpointType, setApiEndpointType] = useState("quick-access");
+  const isDataset =
+    contentModels?.find((model) => model.ZUID === modelZUID)?.type ===
+    "dataset";
+  const apiTypeEndpointMap: Partial<Record<ApiType, string>> = {
+    "quick-access": `/-/instant/${modelZUID}.json`,
+    // "site-generators": item ? `/${item?.web?.path}/?toJSON` : "/?toJSON",
+    "site-generators": "/?toJSON", //TODO: Doesn't seem to be possible when in the table view
+  };
+  const liveDomain = domains?.find((domain) => domain.branch == "live");
 
   const handleCopyClick = (data: string) => {
     navigator?.clipboard
@@ -104,6 +132,32 @@ export const ItemListActions = forwardRef((props, ref) => {
           Copy ZUID
         </MenuItem>
         <MenuItem
+          onClick={(event) => {
+            setShowApiEndpoints(event.currentTarget);
+            setApiEndpointType("quick-access");
+          }}
+        >
+          <ListItemIcon>
+            <BoltRounded />
+          </ListItemIcon>
+          View Quick Access API
+          <KeyboardArrowRightRounded color="action" sx={{ ml: "auto" }} />
+        </MenuItem>
+        {!isDataset && (
+          <MenuItem
+            onClick={(event) => {
+              setShowApiEndpoints(event.currentTarget);
+              setApiEndpointType("site-generators");
+            }}
+          >
+            <ListItemIcon>
+              <DataObjectRounded />
+            </ListItemIcon>
+            View Site Generators API
+            <KeyboardArrowRightRounded color="action" sx={{ ml: "auto" }} />
+          </MenuItem>
+        )}
+        <MenuItem
           onClick={() => {
             history.push(`/schema/${modelZUID}`);
           }}
@@ -123,6 +177,76 @@ export const ItemListActions = forwardRef((props, ref) => {
           </ListItemIcon>
           Edit Template
         </MenuItem>
+      </Menu>
+      <Menu
+        anchorEl={showApiEndpoints}
+        open={!!showApiEndpoints}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        onClose={() => {
+          setShowApiEndpoints(null);
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            setShowApiEndpoints(null);
+            window.open(
+              // @ts-expect-error config not typed
+              `${CONFIG.URL_PREVIEW_PROTOCOL}${instance.randomHashID}${CONFIG.URL_PREVIEW}${apiTypeEndpointMap[apiEndpointType]}`,
+              "_blank"
+            );
+          }}
+        >
+          <ListItemIcon>
+            <DesignServicesRounded />
+          </ListItemIcon>
+          <Typography
+            variant="inherit"
+            noWrap
+            sx={{
+              width: 172,
+            }}
+          >
+            {/* @ts-expect-error config not typed */}
+            {`${instance.randomHashID}${CONFIG.URL_PREVIEW}${apiTypeEndpointMap[apiEndpointType]}`}
+          </Typography>
+          <Chip size="small" label="Dev" />
+        </MenuItem>
+        {liveDomain && (
+          <MenuItem
+            onClick={() => {
+              setShowApiEndpoints(null);
+              window.open(
+                `https://${liveDomain.domain}${
+                  apiTypeEndpointMap[
+                    apiEndpointType as keyof typeof apiTypeEndpointMap
+                  ]
+                }`,
+                "_blank"
+              );
+            }}
+          >
+            <ListItemIcon>
+              <VisibilityRounded />
+            </ListItemIcon>
+            <Typography
+              variant="inherit"
+              noWrap
+              sx={{
+                width: 172,
+              }}
+            >
+              {`${liveDomain.domain}${
+                apiTypeEndpointMap[
+                  apiEndpointType as keyof typeof apiTypeEndpointMap
+                ]
+              }`}
+            </Typography>
+            <Chip size="small" label="Prod" />
+          </MenuItem>
+        )}
       </Menu>
       <TextField
         onChange={handleSearchChange}
