57845da99b53c3683d9ef35a3b201da20d36ef00
Andres <agalin920@gmail.com>
2024-07-12 15:03:03 -0700

    Added T00:00:00 to the date string to avoid time zone discrepancies

--- src/apps/content-editor/src/app/views/ItemList/index.tsx
+++ src/apps/content-editor/src/app/views/ItemList/index.tsx
@@ -194,7 +194,7 @@ export const ItemList = () => {
           if (!clonedItem.data[key]) return;
 
           clonedItem.data[key] = new Date(
-            clonedItem.data[key]
+            clonedItem.data[key] + "T00:00:00"
           )?.toLocaleDateString("en-US", {
             year: "numeric",
             month: "short",
