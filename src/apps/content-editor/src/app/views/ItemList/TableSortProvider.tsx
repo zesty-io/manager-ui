77b771bff49ef6826b636d55a218c0764947b6c2
Nar Cuenca <narc.ph@gmail.com>
2024-07-24 16:14:11 +0800

    task: sync sort model with localstorage

--- src/apps/content-editor/src/app/views/ItemList/TableSortProvider.tsx
+++ src/apps/content-editor/src/app/views/ItemList/TableSortProvider.tsx
@@ -1,5 +1,6 @@
-import { useState, createContext, Dispatch, SetStateAction } from "react";
+import { useState, createContext, useLayoutEffect } from "react";
 import { GridSortModel, GridSortItem } from "@mui/x-data-grid-pro";
+import { useParams as useRouterParams } from "react-router";
 
 type TableSortContextType = [
   GridSortModel,
@@ -14,7 +15,29 @@ type TableSortProviderType = {
   children?: React.ReactNode;
 };
 export const TableSortProvider = ({ children }: TableSortProviderType) => {
-  const [sortModel, setSortModel] = useState<GridSortModel>();
+  const [sortModel, setSortModel] = useState<GridSortModel>([]);
+  const { modelZUID } = useRouterParams<{ modelZUID: string }>();
+
+  useLayoutEffect(() => {
+    if (!modelZUID) return;
+
+    const stateFromLocalStorage = localStorage?.getItem(
+      `${modelZUID}-dataGridState`
+    );
+
+    if (stateFromLocalStorage) {
+      const { sortModel: sortModelFromLocalStorage } = JSON.parse(
+        stateFromLocalStorage
+      )?.sorting;
+
+      if (
+        Array.isArray(sortModelFromLocalStorage) &&
+        sortModelFromLocalStorage?.length
+      ) {
+        setSortModel(sortModelFromLocalStorage);
+      }
+    }
+  }, [modelZUID]);
 
   return (
     <TableSortContext.Provider value={[sortModel, setSortModel]}>
