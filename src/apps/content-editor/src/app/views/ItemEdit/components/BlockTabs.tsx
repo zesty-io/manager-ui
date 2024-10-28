299cf7bb7997729e0143eb7f074922a96118b861
Andres <agalin920@gmail.com>
2024-10-28 14:42:09 -0700

    add border and remove max width

--- src/apps/content-editor/src/app/views/ItemEdit/components/BlockTabs.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/components/BlockTabs.tsx
@@ -11,23 +11,36 @@ export const BlockTabs = (props: any) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Tabs value={value} onChange={(event, newValue) => setValue(newValue)}>
-        <Tab
-          label="Variants"
-          icon={<VerticalSplitRounded fontSize="small" />}
-          iconPosition="start"
-        />
-        <Tab
-          label="Info"
-          icon={<InfoRounded fontSize="small" />}
-          iconPosition="start"
-        />
-      </Tabs>
+      <Box
+        sx={{
+          borderBottom: (theme) => `2px solid ${theme.palette.border}`,
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={(event, newValue) => setValue(newValue)}
+          sx={{
+            position: "relative",
+            top: "2px",
+          }}
+        >
+          <Tab
+            label="Variants"
+            icon={<VerticalSplitRounded fontSize="small" />}
+            iconPosition="start"
+          />
+          <Tab
+            label="Info"
+            icon={<InfoRounded fontSize="small" />}
+            iconPosition="start"
+          />
+        </Tabs>
+      </Box>
       {value === 0 && <div>Tab 1</div>}
       {value === 1 && (
         <ThemeProvider theme={customTheme}>
           <Box
-            maxWidth={320}
+            // maxWidth={320}
             pb={3}
             height="100%"
             sx={{
