629d6acb01e91218039368a18cab76266cda6482
Andres <agalin920@gmail.com>
2024-11-11 14:16:54 -0800

    variant search support

--- src/apps/content-editor/src/app/views/ItemEdit/components/BlockTabs.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/components/BlockTabs.tsx
@@ -1,13 +1,15 @@
 import {
+  Button,
   Tabs,
   Tab,
   Box,
   Typography,
   List,
   ListItemButton,
-  Button,
+  TextField,
+  InputAdornment,
 } from "@mui/material";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@zesty-io/material";
 import { VerticalSplitRounded, InfoRounded } from "@mui/icons-material";
@@ -18,7 +20,8 @@ import { useGetContentModelItemsQuery } from "../../../../../../../shell/service
 import { ContentItem } from "../../../../../../../shell/services/types";
 import moment from "moment-timezone";
 import { useGetUsersQuery } from "../../../../../../../shell/services/accounts";
-import { AddRounded } from "@mui/icons-material";
+import { AddRounded, SearchRounded } from "@mui/icons-material";
+import noSearchResults from "../../../../../../../../public/images/noSearchResults.svg";
 
 export const BlockTabs = (props: any) => {
   const [value, setValue] = useState(0);
@@ -27,6 +30,8 @@ export const BlockTabs = (props: any) => {
     modelZUID: modelZUID,
   });
   const history = useHistory();
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
 
   return (
     <ThemeProvider theme={theme}>
@@ -84,33 +89,95 @@ export const BlockTabs = (props: any) => {
         </Button>
       </Box>
       {value === 0 && (
-        <List
-          disablePadding
-          sx={{
-            mt: 2,
-            borderRadius: "8px",
-            borderWidth: 1,
-            borderStyle: "solid",
-            borderColor: "border",
-            backgroundColor: "common.white",
-            height: "calc(100% - 64px)",
-            overflowY: "auto",
-          }}
-        >
-          {data
-            ?.slice()
-            ?.sort((a, b) => {
-              return a.web?.metaTitle.localeCompare(b.web?.metaTitle);
-            })
-            ?.map((block) => (
-              <BlockVariantCard block={block} />
-            ))}
-        </List>
+        <>
+          <TextField
+            placeholder="Search variants"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            variant="outlined"
+            sx={{
+              my: 2,
+              width: "100%",
+            }}
+            inputRef={searchRef}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchRounded color="action" />
+                </InputAdornment>
+              ),
+            }}
+          />
+          {!!data?.filter((block) =>
+            block.web?.metaTitle.toLowerCase().includes(search.toLowerCase())
+          )?.length && (
+            <List
+              disablePadding
+              sx={{
+                borderRadius: "8px",
+                borderWidth: 1,
+                borderStyle: "solid",
+                borderColor: "border",
+                backgroundColor: "common.white",
+                height: "calc(100% - 120px)",
+                overflowY: "auto",
+              }}
+            >
+              {data
+                ?.filter((block) =>
+                  block.web?.metaTitle
+                    .toLowerCase()
+                    .includes(search.toLowerCase())
+                )
+                ?.slice()
+                ?.sort((a, b) => {
+                  return a.web?.metaTitle.localeCompare(b.web?.metaTitle);
+                })
+                ?.map((block) => (
+                  <BlockVariantCard block={block} />
+                ))}
+            </List>
+          )}
+          {search &&
+            !data?.filter((block) =>
+              block.web?.metaTitle.toLowerCase().includes(search.toLowerCase())
+            )?.length && (
+              <Box display="flex" gap={2}>
+                <Box
+                  component="img"
+                  src={noSearchResults}
+                  width={120}
+                  height={110}
+                ></Box>
+                <Box>
+                  <Typography variant="h4">
+                    Your search <strong>"{search}"</strong> could not find any
+                    results
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    mt={1}
+                    mb={3}
+                  >
+                    Try adjusting your search. We suggest check all words are
+                    spelled correctly or try using different keywords.
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    startIcon={<SearchRounded />}
+                    onClick={() => searchRef?.current?.focus()}
+                  >
+                    Search Again
+                  </Button>
+                </Box>
+              </Box>
+            )}
+        </>
       )}
       {value === 1 && (
         <ThemeProvider theme={customTheme}>
           <Box
-            // maxWidth={320}
             height="calc(100% - 64px)"
             sx={{
               overflowY: "auto",
