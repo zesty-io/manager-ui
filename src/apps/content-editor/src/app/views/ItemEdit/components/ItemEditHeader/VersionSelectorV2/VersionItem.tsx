f5ea2b4772cce01b5fab853788efa41282038f37
Nar Cuenca <narc.ph@gmail.com>
2024-11-26 15:13:01 +0800

    task: create version item component

--- src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/VersionSelectorV2/VersionItem.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/VersionSelectorV2/VersionItem.tsx
@@ -1,2 +1,67 @@
-type VersionItemProps = {};
-export const VersionItem = ({}: VersionItemProps) => {};
+import { Box, MenuItem, Stack, Typography } from "@mui/material";
+import { ScheduleRounded, LanguageRounded } from "@mui/icons-material";
+
+import { ContentItem } from "../../../../../../../../../shell/services/types";
+
+export type Version = {
+  itemZUID: string;
+  modelZUID: string;
+  itemVersionZUID: string;
+  itemVersion: number;
+  labels: any[];
+  createdAt: string;
+  isPublished: boolean;
+  isScheduled: boolean;
+};
+type VersionItemProps = {
+  data: Version;
+  isActive: boolean;
+  withBottomBorder: boolean;
+};
+export const VersionItem = ({
+  data,
+  isActive,
+  withBottomBorder,
+}: VersionItemProps) => {
+  return (
+    <MenuItem
+      sx={{
+        borderColor: "border",
+        bgcolor: isActive ? "background.paper" : "transparent",
+        p: 2,
+      }}
+      divider={withBottomBorder}
+    >
+      <Stack direction="row" justifyContent="space-between" width="100%">
+        <Stack direction="row" gap={1}>
+          <Typography variant="body1" color="text.primary" fontWeight={700}>
+            v{data?.itemVersion}
+          </Typography>
+          {data?.isPublished && (
+            <Stack direction="row" gap={0.25} alignItems="center">
+              <LanguageRounded color="success" fontSize="small" />
+              <Typography variant="body2" color="success.dark" fontWeight={600}>
+                Published
+              </Typography>
+            </Stack>
+          )}
+          {data?.isScheduled && (
+            <Stack direction="row" gap={0.25} alignItems="center">
+              <ScheduleRounded
+                color="warning"
+                fontSize="small"
+                sx={{ mr: 0.25 }}
+              />
+              <Typography variant="body2" color="warning.main" fontWeight={600}>
+                Scheduled
+              </Typography>
+            </Stack>
+          )}
+        </Stack>
+        <Typography variant="body1" color="text.secondary">
+          {data?.createdAt}
+        </Typography>
+      </Stack>
+    </MenuItem>
+  );
+};
--- src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/VersionSelectorV2/index.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/VersionSelectorV2/index.tsx
@@ -1,53 +1,156 @@
-import { useState, memo } from "react";
-import { Button, Menu, MenuItem, Box, Tooltip, Chip } from "@mui/material";
+import { useState, memo, useMemo } from "react";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  Box,
+  Tooltip,
+  Chip,
+  Divider,
+} from "@mui/material";
 import { KeyboardArrowDownRounded } from "@mui/icons-material";
 import { useLocation, useParams } from "react-router";
+import moment from "moment";
+
+const formatDateTime = (dateTimeString: string) => {
+  if (!dateTimeString) return "";
+
+  const momentDate = moment(dateTimeString);
+  const now = moment();
+
+  if (momentDate.isSame(now, "day")) {
+    return `Today ${momentDate.format("h:mm A")}`;
+  } else if (momentDate.isSame(now.clone().subtract(1, "day"), "day")) {
+    return `Yesterday ${momentDate.format("h:mm A")}`;
+  } else if (momentDate.isSame(now.clone().add(1, "day"), "day")) {
+    return `Tomorrow ${momentDate.format("h:mm A")}`;
+  } else {
+    return momentDate.format("MMM D h:mm A");
+  }
+};
 
 import {
   useGetContentItemVersionsQuery,
   useGetItemPublishingsQuery,
 } from "../../../../../../../../../shell/services/instance";
+import { VersionItem, Version } from "./VersionItem";
 
 type VersionSelectorProps = {
-  version: number;
+  activeVersion: number;
 };
-export const VersionSelector = memo(({ version }: VersionSelectorProps) => {
-  const { modelZUID, itemZUID } = useParams<{
-    modelZUID: string;
-    itemZUID: string;
-  }>();
-  const [anchorEl, setAnchorEl] = useState<HTMLElement>(null);
-  const { data: versions } = useGetContentItemVersionsQuery({
-    modelZUID,
-    itemZUID,
-  });
+export const VersionSelector = memo(
+  ({ activeVersion }: VersionSelectorProps) => {
+    const [anchorEl, setAnchorEl] = useState<HTMLElement>(null);
+    const { modelZUID, itemZUID } = useParams<{
+      modelZUID: string;
+      itemZUID: string;
+    }>();
+    const { data: itemPublishings } = useGetItemPublishingsQuery(
+      {
+        modelZUID,
+        itemZUID,
+      },
+      { skip: !modelZUID || !itemZUID }
+    );
+    const { data: versions } = useGetContentItemVersionsQuery(
+      {
+        modelZUID,
+        itemZUID,
+      },
+      { skip: !modelZUID || !itemZUID }
+    );
 
-  return (
-    <Tooltip
-      title="View Versions"
-      enterDelay={1000}
-      enterNextDelay={1000}
-      placement="top-start"
-    >
-      <Button
-        sx={{
-          color: "text.disabled",
-          fontWeight: 600,
-          height: 28,
-          minWidth: "unset",
-          padding: 0.25,
-          " .MuiButton-endIcon": {
-            marginLeft: 0.5,
-          },
-        }}
-        color="inherit"
-        endIcon={<KeyboardArrowDownRounded color="action" />}
-        onClick={(e) => setAnchorEl(e.currentTarget)}
-      >
-        v{version}
-        <Chip label="Draft" color="info" size="small" sx={{ ml: 0.5 }} />
-      </Button>
-    </Tooltip>
-  );
-});
+    const mappedVersions: Version[] = useMemo(() => {
+      if (!versions?.length) return [];
+
+      const activeVersion = itemPublishings?.find(
+        (itemPublishing) => itemPublishing._active
+      );
+      const scheduledVersion = itemPublishings?.find(
+        (item) =>
+          !item._active &&
+          moment.utc(item.publishAt).isAfter(moment.utc()) &&
+          !item.unpublishAt
+      );
+
+      return versions.map((v) => ({
+        itemZUID: v?.meta?.ZUID,
+        modelZUID: v?.meta?.contentModelZUID,
+        itemVersionZUID: v?.web?.versionZUID,
+        itemVersion: v?.meta?.version,
+        labels: [],
+        createdAt: formatDateTime(v?.web?.createdAt),
+        isPublished: activeVersion?.version === v?.meta?.version,
+        isScheduled: scheduledVersion?.version === v?.meta?.version,
+      }));
+    }, [versions, itemPublishings]);
+
+    return (
+      <>
+        <Tooltip
+          title="View Versions"
+          enterDelay={1000}
+          enterNextDelay={1000}
+          placement="top-start"
+        >
+          <Button
+            sx={{
+              color: "text.disabled",
+              fontWeight: 600,
+              height: 28,
+              minWidth: "unset",
+              padding: 0.25,
+              " .MuiButton-endIcon": {
+                marginLeft: 0.5,
+              },
+            }}
+            color="inherit"
+            endIcon={<KeyboardArrowDownRounded color="action" />}
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+          >
+            v{activeVersion}
+            <Chip label="Draft" color="info" size="small" sx={{ ml: 0.5 }} />
+          </Button>
+        </Tooltip>
+        <Menu
+          onClose={() => setAnchorEl(null)}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: -26,
+            horizontal: "right",
+          }}
+          anchorEl={anchorEl}
+          open={!!anchorEl}
+          slotProps={{
+            paper: {
+              sx: {
+                maxHeight: 540,
+                overflow: "auto",
+                width: 379,
+                bgcolor: "grey.50",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiMenu-list": {
+              py: 0,
+            },
+          }}
+        >
+          {mappedVersions?.map((version, index) => (
+            <VersionItem
+              key={version?.itemVersionZUID}
+              data={version}
+              isActive={activeVersion === version?.itemVersion}
+              withBottomBorder={index + 1 < versions?.length}
+            />
+          ))}
+        </Menu>
+      </>
+    );
+  }
+);
 VersionSelector.displayName = "VersionSelector";
