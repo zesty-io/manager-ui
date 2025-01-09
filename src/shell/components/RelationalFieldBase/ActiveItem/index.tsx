a00974b3775a7d17c17e97781ebec267af245c41
Nar Cuenca <narc.ph@gmail.com>
2025-01-09 14:13:55 +0800

    task: create more menu

--- src/shell/components/RelationalFieldBase/ActiveItem/index.tsx
+++ src/shell/components/RelationalFieldBase/ActiveItem/index.tsx
@@ -1,10 +1,25 @@
 import { memo, useState, useMemo, useEffect } from "react";
-import { Typography, Stack, Box, Skeleton, IconButton } from "@mui/material";
+import {
+  Typography,
+  Stack,
+  Box,
+  Menu,
+  MenuItem,
+  ListItemText,
+  ListItemIcon,
+  IconButton,
+} from "@mui/material";
 import {
   DragIndicatorRounded,
   Edit,
   MoreHoriz,
   ImageRounded,
+  CloudUploadRounded,
+  ScheduleRounded,
+  DesignServicesRounded,
+  LanguageRounded,
+  WidgetsRounded,
+  CloseRounded,
 } from "@mui/icons-material";
 import moment from "moment-timezone";
 import { useHistory } from "react-router";
@@ -35,6 +50,7 @@ export const ActiveItem = memo(
     draggable,
   }: ActiveItemProps) => {
     const [imageURL, setImageURL] = useState(null);
+    const [anchorEl, setAnchorEl] = useState(null);
     const history = useHistory();
     const { data: contentItem, isLoading: isLoadingContentItem } =
       useGetContentItemQuery(itemZUID, {
@@ -158,91 +174,75 @@ export const ActiveItem = memo(
     }
 
     return (
-      <Stack
-        direction="row"
-        sx={{
-          bgcolor: "background.paper",
-          height: 64,
-          width: "100%",
-          border: 1,
-          borderColor: "border",
-          borderRadius: 2,
-          alignItems: "center",
-          overflow: "hidden",
-        }}
-      >
-        <Stack direction="row" alignItems="center" flexGrow={1}>
-          {draggable && (
-            <IconButton
-              disableRipple
-              disableFocusRipple
-              disableTouchRipple
-              size="xsmall"
-              sx={{ cursor: "grab", mx: 0.5 }}
-            >
-              <DragIndicatorRounded fontSize="small" />
-            </IconButton>
-          )}
-          {!!imageFields?.length &&
-            (!!imageURL ? (
-              <Box
-                component="img"
-                loading="lazy"
-                width={64}
-                height={64}
-                src={`${imageURL}?width=64&fit=contain`}
-                sx={{
-                  flexShrink: 0,
-                  bgcolor: "grey.100",
-                  objectFit: "contain",
-                  overflow: "hidden",
-                }}
-              />
-            ) : (
-              <Stack
-                sx={{
-                  flexShrink: 0,
-                  bgcolor: "grey.100",
-                  width: 64,
-                  height: 64,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  overflow: "hidden",
-                }}
+      <>
+        <Stack
+          direction="row"
+          sx={{
+            bgcolor: "background.paper",
+            height: 64,
+            width: "100%",
+            border: 1,
+            borderColor: "border",
+            borderRadius: 2,
+            alignItems: "center",
+            overflow: "hidden",
+          }}
+        >
+          <Stack direction="row" alignItems="center" flexGrow={1}>
+            {draggable && (
+              <IconButton
+                disableRipple
+                disableFocusRipple
+                disableTouchRipple
+                size="xsmall"
+                sx={{ cursor: "grab", mx: 0.5 }}
               >
-                <ImageRounded color="action" />
-              </Stack>
-            ))}
-          <Stack
-            gap={0.5}
-            justifyContent="center"
-            flexGrow={1}
-            ml={
-              !!imageFields?.length || (!imageFields?.length && !draggable)
-                ? 2
-                : 0
-            }
-          >
-            <Typography
-              color="text.primary"
-              fontWeight={600}
-              variant="body2"
-              sx={{
-                display: "-webkit-box",
-                "-webkit-line-clamp": "1",
-                "-webkit-box-orient": "vertical",
-                wordBreak: "break-word",
-                wordWrap: "break-word",
-                hyphens: "auto",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
+                <DragIndicatorRounded fontSize="small" />
+              </IconButton>
+            )}
+            {!!imageFields?.length &&
+              (!!imageURL ? (
+                <Box
+                  component="img"
+                  loading="lazy"
+                  width={64}
+                  height={64}
+                  src={`${imageURL}?width=64&fit=contain`}
+                  sx={{
+                    flexShrink: 0,
+                    bgcolor: "grey.100",
+                    objectFit: "contain",
+                    overflow: "hidden",
+                  }}
+                />
+              ) : (
+                <Stack
+                  sx={{
+                    flexShrink: 0,
+                    bgcolor: "grey.100",
+                    width: 64,
+                    height: 64,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    overflow: "hidden",
+                  }}
+                >
+                  <ImageRounded color="action" />
+                </Stack>
+              ))}
+            <Stack
+              gap={0.5}
+              justifyContent="center"
+              flexGrow={1}
+              ml={
+                !!imageFields?.length || (!imageFields?.length && !draggable)
+                  ? 2
+                  : 0
+              }
             >
-              {itemTitle}
-            </Typography>
-            {contentItem?.web?.metaDescription && (
               <Typography
-                color="text.secondary"
+                color="text.primary"
+                fontWeight={600}
                 variant="body2"
                 sx={{
                   display: "-webkit-box",
@@ -255,54 +255,127 @@ export const ActiveItem = memo(
                   textOverflow: "ellipsis",
                 }}
               >
-                {contentItem?.web?.metaDescription}
+                {itemTitle}
               </Typography>
-            )}
+              {contentItem?.web?.metaDescription && (
+                <Typography
+                  color="text.secondary"
+                  variant="body2"
+                  sx={{
+                    display: "-webkit-box",
+                    "-webkit-line-clamp": "1",
+                    "-webkit-box-orient": "vertical",
+                    wordBreak: "break-word",
+                    wordWrap: "break-word",
+                    hyphens: "auto",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  {contentItem?.web?.metaDescription}
+                </Typography>
+              )}
+            </Stack>
+          </Stack>
+          <Stack direction="row" gap={2} mx={2} alignItems="center">
+            <Stack gap={0.25}>
+              {!!publishStatus?.draft && (
+                <VersionChip
+                  type="draft"
+                  version={publishStatus.draft.version}
+                  publisher={publishStatus.draft.publisher}
+                  dateTime={publishStatus.draft.dateTime}
+                />
+              )}
+              {!!publishStatus?.scheduled ? (
+                <VersionChip
+                  type="scheduled"
+                  version={publishStatus.scheduled.version}
+                  publisher={publishStatus.scheduled.publisher}
+                  dateTime={publishStatus.scheduled.dateTime}
+                />
+              ) : publishStatus?.published ? (
+                <VersionChip
+                  type="published"
+                  version={publishStatus.published.version}
+                  publisher={publishStatus.published.publisher}
+                  dateTime={publishStatus.published.dateTime}
+                />
+              ) : (
+                <></>
+              )}
+            </Stack>
+            <Stack direction="row" gap={1}>
+              <IconButton
+                size="xsmall"
+                onClick={() =>
+                  history.push(`/content/${relatedModelData?.ZUID}/${itemZUID}`)
+                }
+              >
+                <Edit fontSize="small" />
+              </IconButton>
+              <IconButton
+                size="xsmall"
+                onClick={(evt) => setAnchorEl(evt.currentTarget)}
+              >
+                <MoreHoriz fontSize="small" />
+              </IconButton>
+            </Stack>
           </Stack>
         </Stack>
-        <Stack direction="row" gap={2} mx={2} alignItems="center">
-          <Stack gap={0.25}>
-            {!!publishStatus?.draft && (
-              <VersionChip
-                type="draft"
-                version={publishStatus.draft.version}
-                publisher={publishStatus.draft.publisher}
-                dateTime={publishStatus.draft.dateTime}
-              />
-            )}
-            {!!publishStatus?.scheduled ? (
-              <VersionChip
-                type="scheduled"
-                version={publishStatus.scheduled.version}
-                publisher={publishStatus.scheduled.publisher}
-                dateTime={publishStatus.scheduled.dateTime}
-              />
-            ) : publishStatus?.published ? (
-              <VersionChip
-                type="published"
-                version={publishStatus.published.version}
-                publisher={publishStatus.published.publisher}
-                dateTime={publishStatus.published.dateTime}
-              />
-            ) : (
-              <></>
-            )}
-          </Stack>
-          <Stack direction="row" gap={1}>
-            <IconButton
-              size="xsmall"
-              onClick={() =>
-                history.push(`/content/${relatedModelData?.ZUID}/${itemZUID}`)
-              }
-            >
-              <Edit fontSize="small" />
-            </IconButton>
-            <IconButton size="xsmall">
-              <MoreHoriz fontSize="small" />
-            </IconButton>
-          </Stack>
-        </Stack>
-      </Stack>
+        {!!anchorEl && (
+          <Menu
+            open
+            anchorEl={anchorEl}
+            onClose={() => setAnchorEl(null)}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            <MenuItem>
+              <ListItemIcon>
+                <CloudUploadRounded />
+              </ListItemIcon>
+              <ListItemText primary="Publish Now" />
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <ScheduleRounded />
+              </ListItemIcon>
+              <ListItemText primary="Schedule Publish" />
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <DesignServicesRounded />
+              </ListItemIcon>
+              <ListItemText primary="Draft Preview - vXXX" />
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <LanguageRounded />
+              </ListItemIcon>
+              <ListItemText primary="Production Preview - vXXX" />
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <WidgetsRounded />
+              </ListItemIcon>
+              <ListItemText primary="Copy ZUID" />
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <CloseRounded />
+              </ListItemIcon>
+              <ListItemText primary="Remove" />
+            </MenuItem>
+          </Menu>
+        )}
+      </>
     );
   }
 );
