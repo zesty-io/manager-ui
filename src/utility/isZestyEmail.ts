cac9d843f7bbff0a2ba20167907958c3418e35c0
Andres <agalin920@gmail.com>
2025-03-10 09:54:06 -0700

    Whitelist kin domain for blocks usage

--- src/utility/isZestyEmail.ts
+++ src/utility/isZestyEmail.ts
@@ -1,12 +1,3 @@
-import { MD5 } from "./md5";
-
 export function isZestyEmail(email: string): boolean {
-  const hashedWhitelistedEmails = [
-    "f6b705ad0f149b40abe7ab939d6f2cc4",
-    "91de2eca00c7588dbd5b477751a68e39",
-  ];
-
-  return (
-    email.endsWith("@zesty.io") || hashedWhitelistedEmails.includes(MD5(email))
-  );
+  return email.endsWith("@zesty.io") || email.endsWith("@kin.com");
 }
