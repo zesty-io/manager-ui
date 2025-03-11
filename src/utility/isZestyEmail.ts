213270a5e8ac55ca86f4a133c36bc9868e53160e
Andres <agalin920@gmail.com>
2025-03-11 14:53:01 -0700

    white list geo blue domain

--- src/utility/isZestyEmail.ts
+++ src/utility/isZestyEmail.ts
@@ -1,3 +1,7 @@
 export function isZestyEmail(email: string): boolean {
-  return email.endsWith("@zesty.io") || email.endsWith("@kin.com");
+  return (
+    email.endsWith("@zesty.io") ||
+    email.endsWith("@kin.com") ||
+    email.endsWith("@geo-blue.com")
+  );
 }
