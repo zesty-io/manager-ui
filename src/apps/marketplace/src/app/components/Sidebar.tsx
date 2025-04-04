d554884e5918fb29f2bd932efea4fad2c322f3b0
Nar Cuenca <narc.ph@gmail.com>
2025-04-04 14:58:34 +0800

    Add sub app nav loading for apps

--- src/apps/marketplace/src/app/components/Sidebar.tsx
+++ src/apps/marketplace/src/app/components/Sidebar.tsx
@@ -8,9 +8,15 @@ import {
   AppSideBar,
   SubMenu,
 } from "../../../../../shell/components/AppSidebar";
+import { Box, Stack, Skeleton, Typography } from "@mui/material";
+import { IconButton as IconButtonCustom } from "@zesty-io/material";
+import { AddRounded } from "@mui/icons-material";
 
 export const Sidebar = () => {
   const installedApps = useSelector((state: AppState) => state.apps.installed);
+  const isLoadingApps = useSelector(
+    (state: AppState) => state.apps.isLoadingApps
+  );
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -34,6 +40,66 @@ export const Sidebar = () => {
 
     return [];
   }, [installedApps]);
+
+  if (isLoadingApps) {
+    return (
+      <Box
+        sx={{
+          backgroundColor: "grey.900",
+          height: "100%",
+          width: "inherit",
+          color: "common.white",
+          py: 1.5,
+        }}
+      >
+        <Stack sx={{ gap: 1.5, mb: 1.5 }}>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            px={1.5}
+          >
+            <Typography
+              variant="h6"
+              fontWeight={700}
+              lineHeight="24px"
+              fontSize={18}
+            >
+              Apps
+            </Typography>
+
+            <IconButtonCustom variant="contained" size="xsmall">
+              <AddRounded fontSize="small" />
+            </IconButtonCustom>
+          </Stack>
+        </Stack>
+        <Stack
+          sx={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: 36,
+            ml: 1.5,
+            mr: 2,
+            gap: 1,
+          }}
+        >
+          <Skeleton
+            variant="circular"
+            width={24}
+            height={24}
+            sx={{ backgroundColor: "grey.700", flexShrink: 0 }}
+          />
+          <Skeleton
+            variant="rounded"
+            width="100%"
+            height={24}
+            sx={{ backgroundColor: "grey.700" }}
+          />
+        </Stack>
+      </Box>
+    );
+  }
 
   return (
     <AppSideBar
--- src/shell/store/types.ts
+++ src/shell/store/types.ts
@@ -19,6 +19,7 @@ export type AppState = {
   apps: {
     frames: any;
     installed: InstalledApp[];
+    isLoadingApps: boolean;
   };
   auth: {
     checking: boolean;
