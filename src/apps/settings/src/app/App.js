b9d449c6789a39ba0dc025449880aaff4ac70664
Nar Cuenca <narc.ph@gmail.com>
2025-04-04 14:05:32 +0800

    Add sub app nav loading for settings

--- src/apps/settings/src/app/App.js
+++ src/apps/settings/src/app/App.js
@@ -3,7 +3,6 @@ import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 import { Box } from "@mui/material";
 
-import { WithLoader } from "@zesty-io/core/WithLoader";
 import { SettingsNav } from "./components/Nav";
 
 import { Instance } from "./views/Instance";
@@ -41,84 +40,71 @@ export default connect((state) => ({
   }, []);
 
   return (
-    <WithLoader
-      condition={
-        props.settings.catInstance.length &&
-        props.settings.catStyles.length &&
-        props.settings.catFonts.length
-      }
-      message="Starting Settings"
-      height="100vh"
-    >
-      <section className={styles.Settings}>
-        <div className={styles.AppWrap}>
-          <ResizableContainer
-            id="settingsNav"
-            defaultWidth={220}
-            minWidth={220}
-            maxWidth={360}
-          >
-            <SettingsNav />
-          </ResizableContainer>
+    <section className={styles.Settings}>
+      <div className={styles.AppWrap}>
+        <ResizableContainer
+          id="settingsNav"
+          defaultWidth={220}
+          minWidth={220}
+          maxWidth={360}
+        >
+          <SettingsNav />
+        </ResizableContainer>
+        <Box
+          className={styles.OverflowWrap}
+          sx={{
+            borderLeft: "1px solid",
+            borderColor: "border",
+          }}
+        >
           <Box
-            className={styles.OverflowWrap}
+            component="main"
+            className={
+              location.pathname === "/settings/instance/bynder"
+                ? ""
+                : styles.Content
+            }
             sx={{
-              borderLeft: "1px solid",
-              borderColor: "border",
+              height: "100%",
             }}
           >
-            <Box
-              component="main"
-              className={
-                location.pathname === "/settings/instance/bynder"
-                  ? ""
-                  : styles.Content
-              }
-              sx={{
-                height: "100%",
-              }}
-            >
-              <Switch>
-                <Route
-                  exact
-                  path="/settings/styles/:category"
-                  component={Styles}
-                />
-                <Redirect from="/settings/styles" to="/settings/styles/1" />
-                <Route
-                  path="/settings/instance/bynder"
-                  exact
-                  component={Bynder}
-                />
-                <Route
-                  path="/settings/instance/:category"
-                  component={Instance}
-                />
+            <Switch>
+              <Route
+                exact
+                path="/settings/styles/:category"
+                component={Styles}
+              />
+              <Redirect from="/settings/styles" to="/settings/styles/1" />
+              <Route
+                path="/settings/instance/bynder"
+                exact
+                component={Bynder}
+              />
+              <Route path="/settings/instance/:category" component={Instance} />
 
-                <Route path="/settings/fonts/browse" component={Browse} />
-                <Route path="/settings/fonts/installed" component={Installed} />
-                <Redirect from="/settings/fonts" to="/settings/fonts/browse" />
+              <Route path="/settings/fonts/browse" component={Browse} />
+              <Route path="/settings/fonts/installed" component={Installed} />
+              <Redirect from="/settings/fonts" to="/settings/fonts/browse" />
 
-                <Route path="/settings/robots" component={Robots} />
-                <Route
-                  path="/settings/head"
-                  render={() => (
-                    <div className={styles.InstanceHeadTags}>
-                      <Head resourceZUID={props.instance.ZUID} />
-                    </div>
-                  )}
-                />
+              <Route path="/settings/robots" component={Robots} />
+              <Route
+                path="/settings/head"
+                render={() => (
+                  <div className={styles.InstanceHeadTags}>
+                    <Head resourceZUID={props.instance.ZUID} />
+                  </div>
+                )}
+              />
 
-                <Redirect from="/settings" to="/settings/instance/general" />
-                <Redirect
-                  from="/settings/instance"
-                  to="/settings/instance/general"
-                />
-              </Switch>
-            </Box>
+              <Redirect from="/settings" to="/settings/instance/general" />
+              <Redirect
+                from="/settings/instance"
+                to="/settings/instance/general"
+              />
+            </Switch>
           </Box>
-        </div>
-      </section>
-    </WithLoader>
+        </Box>
+      </div>
+    </section>
   );
 });
