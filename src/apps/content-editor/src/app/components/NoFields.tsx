5017b7dfd76ccdf3f68607959462d21ee4d5c4be
Nar Cuenca <narc.ph@gmail.com>
2025-05-02 11:00:41 +0800

    Create no fields component

---
+++ src/apps/content-editor/src/app/components/NoFields.tsx
@@ -0,0 +1,50 @@
+import { Typography, Stack, Button, Box } from "@mui/material";
+import { AddRounded } from "@mui/icons-material";
+import { useParams, useHistory } from "react-router";
+
+import fieldsLoading from "../../../../../../public/images/fields-loading.png";
+
+export const NoFields = () => {
+  const history = useHistory();
+  const { modelZUID } = useParams<{
+    modelZUID: string;
+  }>();
+
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      width="100%"
+    >
+      <Stack gap={3}>
+        <Box maxWidth={540}>
+          <Typography variant="h3" fontWeight={700} color="text.primary">
+            Add Fields to Your Model
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            This model (Articles) doesn't have any fields yet. To define the
+            structure of your content items, go to your model and add fields.
+          </Typography>
+        </Box>
+        <Button
+          variant="contained"
+          startIcon={<AddRounded />}
+          size="small"
+          sx={{ width: "fit-content" }}
+          onClick={() => {
+            history.push(`/schema/${modelZUID}`);
+          }}
+        >
+          Add Fields in Schema
+        </Button>
+      </Stack>
+      <Box
+        component="img"
+        src={fieldsLoading}
+        alt="No fields available"
+        loading="lazy"
+      />
+    </Stack>
+  );
+};
