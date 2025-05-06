64d6005e0ab82a9461a50658588d6a0a18a1e447
Andres <agalin920@gmail.com>
2025-05-06 13:50:03 -0700

    fix prosemirror issue

--- src/shell/components/FieldTypeEditor/Editors/react-prosemirror-plugins/plugins.js
+++ src/shell/components/FieldTypeEditor/Editors/react-prosemirror-plugins/plugins.js
@@ -1,19 +1,18 @@
 import { history } from "prosemirror-history";
 import { dropCursor } from "prosemirror-dropcursor";
 import { gapCursor } from "prosemirror-gapcursor";
-// import { columnResizing, tableEditing } from "prosemirror-tables";
 import { placeholder } from "@aeaton/prosemirror-placeholder";
 import { footnotes } from "@aeaton/prosemirror-footnotes";
+
+import keys from "./keys";
+import rules from "./rules";
 
 import "prosemirror-tables/style/tables.css";
 import "prosemirror-gapcursor/style/gapcursor.css";
 import "@aeaton/prosemirror-footnotes/style/footnotes.css";
 import "@aeaton/prosemirror-placeholder/style/placeholder.css";
 
-import keys from "./keys";
-import rules from "./rules";
-
-export default [
+const plugins = [
   rules,
   keys,
   placeholder(),
@@ -21,10 +20,10 @@ export default [
   dropCursor(),
   gapCursor(),
   history(),
-  // columnResizing(),
-  // tableEditing()
 ];
 
-// for tables
+export { plugins };
+
+// (Optional) Keep this, but not part of plugin system
 document.execCommand("enableObjectResizing", false, false);
 document.execCommand("enableInlineTableEditing", false, false);
