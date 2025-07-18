65d04232f154ed8e493be20ee8e344b2e3605b0b
geodem.dev <74890703+geodem127@users.noreply.github.com>
2025-07-18 09:44:38 +0800

    [#3665]: Global - App Crashes When Rendering Login Page for Logged-Out Users (#3666)

--- src/shell/hooks/useSearchBlocksByKeyword.ts
+++ src/shell/hooks/useSearchBlocksByKeyword.ts
@@ -27,45 +27,8 @@ const toProperCase = (str?: string): string => {
     .trim();
 };
 
-const processData = (contentItems: ContentItem[], models: ContentModel[]) => {
-  const modelsMap = Object.fromEntries(
-    models.map((model) => [model.ZUID, model])
-  );
-
-  const processedModels = models.map((model) => ({
-    ZUID: model.ZUID,
-    label: model.label,
-    type: model.type,
-    contentModelLabel: null,
-    contentModelZUID: null,
-    updatedAt: model.updatedAt,
-    createdAt: model.createdAt,
-    createdByUserZUID: model.createdByUserZUID,
-    langID: null,
-  }));
-
-  const processedContent = contentItems.map((item) => {
-    const parentModel = modelsMap[item?.meta?.contentModelZUID];
-    return {
-      ZUID: item?.meta?.ZUID,
-      label: item?.web?.metaTitle || item?.web?.metaLinkText,
-      type: parentModel?.type,
-      contentModelZUID: item?.meta?.contentModelZUID,
-      contentModelLabel: parentModel?.label || parentModel?.metaTitle,
-      updatedAt: item?.meta?.updatedAt || item?.web?.updatedAt,
-      createdAt: item?.meta?.createdAt || item?.web?.createdAt,
-      createdByUserZUID:
-        item?.meta?.createdByUserZUID || item?.web?.createdByUserZUID,
-      langID: item?.meta?.langID,
-    };
-  });
-
-  return [...processedModels, ...processedContent];
-};
-
 export const useSearchBlocksByKeyword = (): SearchResult => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [blocks, setBlocks] = useState<BlockModel[]>([]);
 
   const { data: modelsRaw, isLoading: isLoadingModels } =
     useGetContentModelsQuery();
@@ -82,71 +45,107 @@ export const useSearchBlocksByKeyword = (): SearchResult => {
     isLoadingUsers ||
     isLoadingLanguages;
 
-  const allUsers = useMemo<UsersMap>(
-    () =>
-      Object.fromEntries(
-        users.map((user) => [
-          `${user.firstName} ${user.lastName}`.toLowerCase(),
-          user.ZUID,
-        ])
-      ),
-    [users]
-  );
-
   const usersMap = useMemo<UsersMap>(
     () =>
+      !!users?.length &&
       Object.fromEntries(
-        users.map((user) => [
-          user.ZUID,
-          `${user.firstName} ${user.lastName}`.toLowerCase(),
-        ])
+        users?.map((user) => [
+          user?.ZUID,
+          `${user?.firstName} ${user?.lastName}`.toLowerCase(),
+        ]) || []
       ),
     [users]
   );
 
   const languageMap = useMemo<LanguageMap>(
-    () => Object.fromEntries(languages.map((lang) => [lang.ID, lang])),
+    () =>
+      !!languages?.length &&
+      Object.fromEntries(languages?.map((lang) => [lang?.ID, lang])),
     [languages]
   );
 
+  const modelsMap = useMemo(
+    () =>
+      !!modelsRaw?.length &&
+      Object.fromEntries(modelsRaw?.map((model) => [model?.ZUID, model])),
+    [modelsRaw]
+  );
+
   const normalizedSearch = searchTerm.toLowerCase()?.trim();
-  const matchedUserZUID = allUsers[normalizedSearch];
 
   const parsedBlocks = useMemo(() => {
     if (isLoading) return [];
 
-    const parsedData = processData(contentsRaw, modelsRaw);
+    const processedModels = !modelsRaw?.length
+      ? []
+      : modelsRaw?.map((model) => ({
+          ZUID: model?.ZUID,
+          label: model?.label,
+          type: model?.type,
+          contentModelLabel: null,
+          contentModelZUID: null,
+          updatedAt: model?.updatedAt,
+          createdAt: model?.createdAt,
+          createdByUserZUID: model?.createdByUserZUID,
+          langID: null,
+        }));
 
-    return parsedData
-      ?.filter((item) => item?.type === "block")
-      ?.map((item) => {
-        return {
-          ...item,
-          lang: languageMap?.[item?.langID]?.code,
-          createdByUserName: !usersMap?.[item?.createdByUserZUID]
-            ? ""
-            : toProperCase(usersMap?.[item?.createdByUserZUID]),
-          title: item?.label,
-        };
+    const processedContent = !contentsRaw?.length
+      ? []
+      : contentsRaw?.map((item) => {
+          const parentModel = modelsMap?.[item?.meta?.contentModelZUID] || null;
+          return {
+            ZUID: item?.meta?.ZUID,
+            label: item?.web?.metaTitle || item?.web?.metaLinkText,
+            type: !parentModel ? null : parentModel?.type,
+            contentModelZUID: item?.meta?.contentModelZUID,
+            contentModelLabel: !parentModel
+              ? null
+              : parentModel?.label || parentModel?.metaTitle,
+            updatedAt: item?.meta?.updatedAt || item?.web?.updatedAt,
+            createdAt: item?.meta?.createdAt || item?.web?.createdAt,
+            createdByUserZUID:
+              item?.meta?.createdByUserZUID || item?.web?.createdByUserZUID,
+            langID: item?.meta?.langID,
+          };
+        });
+
+    return [...processedModels, ...processedContent]
+      .map((item) => ({
+        ...item,
+        lang: languageMap[item.langID]?.code,
+        createdByUserName: toProperCase(usersMap[item.createdByUserZUID] || ""),
+        title: item.label,
+      }))
+      .filter((item) => {
+        if (item.type !== "block") return false;
+        if (!normalizedSearch) return true;
+
+        const searchFields = [
+          item.ZUID,
+          item.label,
+          item.createdByUserName,
+          item.title,
+          item.contentModelZUID,
+          item.type,
+          item.contentModelLabel,
+        ]
+          .join("\n")
+          .toLowerCase();
+
+        return searchFields.includes(normalizedSearch);
       });
-  }, [isLoading, contentsRaw, modelsRaw, languageMap, usersMap]);
+  }, [
+    isLoading,
+    contentsRaw,
+    modelsRaw,
+    languageMap,
+    usersMap,
+    normalizedSearch,
+  ]);
 
-  useEffect(() => {
-    if (isLoading) return;
-    const filteredBlocks = parsedBlocks?.filter((item) => {
-      const searchString = `${item?.ZUID}\n
-        ${item?.label}\n
-        ${item?.createdByUserName}\n
-        ${item?.title}\n
-        ${item?.contentModelZUID}\n
-        ${item?.type}\n${item?.contentModelLabel}}`?.toLowerCase();
-      return searchString?.includes(normalizedSearch);
-    });
-
-    setBlocks(filteredBlocks);
-  }, [parsedBlocks, normalizedSearch, matchedUserZUID, isLoading]);
   return {
-    blocks: blocks,
+    blocks: parsedBlocks,
     setBlockKeyword: setSearchTerm,
     isLoading,
   };
