984def126f5e59331eeed361552ca6717708cb70
Nar Cuenca <narc.ph@gmail.com>
2025-07-21 14:23:07 +0800

    Typography parenting fix

--- src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/RenameItemDialog.tsx
+++ src/apps/content-editor/src/app/views/ItemEdit/components/ItemEditHeader/RenameItemDialog.tsx
@@ -43,7 +43,7 @@ export const RenameItemDialog = ({ onClose }: DuplicateItemProps) => {
 
   return (
     <Dialog open fullWidth maxWidth={"xs"} onClose={onClose}>
-      <DialogTitle>
+      <DialogTitle component="div">
         <Box
           sx={{
             backgroundColor: "blue.50",
