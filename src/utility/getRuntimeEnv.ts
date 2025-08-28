232d77bc26bf8e9c4310f8c60fc462221afafccc
Nar Cuenca <narc.ph@gmail.com>
2025-08-28 09:07:52 +0800

    Use __CONFIG__.ENV to determine current env app is running on

--- src/utility/getRuntimeEnv.ts
+++ src/utility/getRuntimeEnv.ts
@@ -1,10 +1,12 @@
 export default () => {
   const isContentOneDomain = window.location.hostname.includes(".content.one");
+  // @ts-expect-error untyped
+  const env = __CONFIG__.ENV;
 
   if (isContentOneDomain) {
     const COOKIE_DOMAIN = ".content.one";
 
-    switch (process.env.NODE_ENV) {
+    switch (env) {
       case "production":
         return {
           URL_MANAGER: ".cms.content.one",
@@ -30,12 +32,12 @@ export default () => {
         };
 
       default:
-        throw new Error(`Invalid NODE_ENV value: ${process.env.NODE_ENV}`);
+        throw new Error(`Invalid NODE_ENV value: ${env}`);
     }
   } else {
     const COOKIE_DOMAIN = ".zesty.io";
 
-    switch (process.env.NODE_ENV) {
+    switch (env) {
       case "production":
         return {
           URL_MANAGER: ".manager.zesty.io",
@@ -61,7 +63,7 @@ export default () => {
         };
 
       default:
-        throw new Error(`Unhandled NODE_ENV value: ${process.env.NODE_ENV}`);
+        throw new Error(`Unhandled NODE_ENV value: ${env}`);
     }
   }
 };
