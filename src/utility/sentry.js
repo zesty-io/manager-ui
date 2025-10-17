ee40e9a1c6e20f12b1f1f60b3f02ec57f6f10a22
Nar Cuenca <narc.ph@gmail.com>
2025-10-17 10:50:45 +0800

    Upgrade sentry to v7

--- src/utility/sentry.js
+++ src/utility/sentry.js
@@ -1,5 +1,4 @@
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import history from "utility/history";
 
 // window.CONFIG not available so we use the webpack injected variable
@@ -7,11 +6,7 @@ if (["stage", "production"].includes(__CONFIG__?.ENV)) {
   Sentry.init({
     release: __CONFIG__?.build?.data?.gitCommit,
     environment: __CONFIG__?.ENV,
-    integrations: [
-      new Integrations.BrowserTracing({
-        routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
-      }),
-    ],
+    integrations: [Sentry.reactRouterV5BrowserTracingIntegration({ history })],
     dsn: "https://2e83c3767c484794a56832affe2d26d9@o162121.ingest.sentry.io/5441698",
     autoSessionTracking: true,
     tracesSampleRate: 1.0,
