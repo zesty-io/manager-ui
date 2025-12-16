ff4410135ddf2054e98fe460774e818bdbb22bfa
geodem <geodem.dev@gmail.com>
2025-12-17 06:58:43 +0800

    implemented content seeding | updated element selector

--- cypress/e2e/content/item-list-table.spec.js
+++ cypress/e2e/content/item-list-table.spec.js
@@ -1,17 +1,13 @@
 const NOW = Date.now();
 
 describe("Content item list table", () => {
-  let MODEL = null;
-  let FIELDS = null;
   let ITEMS = null;
   before(() => {
     cy.task("seed:content", "fixtures/actions.json").then(
-      ({ model, fields, items }) => {
+      ({ model, items }) => {
         Cypress.env("modelZUID", model?.ZUID);
         Cypress.env("itemZUID", items[0]?.meta?.ZUID);
-        MODEL = model;
         ITEMS = items;
-        FIELDS = fields;
       }
     );
   });
@@ -23,10 +19,13 @@ describe("Content item list table", () => {
       });
     });
 
-    // cy.getBySelector("SingleRelationshipCell")
-    cy.get(".MuiDataGrid-row .MuiDataGrid-cell:eq(2)")
+    cy.getBySelector("sortByFilter_default").click();
+    cy.getBySelector(`"sort:text"`).click();
+    cy.getBySelector("listItemTable")
+      .find('[data-cy="itemListRow"]')
       .first()
-      .contains(ITEMS?.[0]?.web?.metaTitle, { matchCase: false });
+      .find('[data-field="text"]')
+      .contains(ITEMS?.[1]?.data?.text);
   });
 
   it("properly removes deleted content items from cache even after page reload", () => {
@@ -38,8 +37,10 @@ describe("Content item list table", () => {
 
     cy.intercept("/search/items*").as("searchItems");
     cy.intercept("/v1/content/models*").as("contentModels");
-
-    cy.get("input[name=title]").clear().type(`Delete me ${NOW}`);
+    cy.getBySelector(`"field:text"`)
+      .find("input")
+      .clear()
+      .type(`Delete me ${NOW}`);
     cy.getBySelector("ManualMetaFlow").click();
     cy.getBySelector("metaDescription")
       .find("textarea")
