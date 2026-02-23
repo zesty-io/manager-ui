ce58052558b117dc1016d6ed4408eb9bab62e550
geodem <geodem.dev@gmail.com>
2026-02-24 06:06:13 +0800

    added code-app sidebar e2e tests

--- cypress/e2e/code/sidebar.spec.js
+++ cypress/e2e/code/sidebar.spec.js
@@ -1,11 +1,83 @@
-describe("Code Editor Sidebar", () => {
+describe("Code Editor Sidebar", { defaultCommandTimeout: 50000 }, () => {
+  let SIDEBAR = null;
+  let FILENAME = null;
   before(() => {
-    cy.visit("/code");
+    cy.task("seed:code", "fixtures/code/sidebar.json").then((res) => {
+      console.debug("res: ", res);
+      SIDEBAR = res;
+      FILENAME = SIDEBAR?.fileName?.split("/")?.pop()?.trim() || "";
+      Cypress.env("fileZUID", res?.ZUID);
+    });
+
+    cy.visit(`/code`);
   });
 
-  it("can navigate code files in the sidebar", () => {
-    cy.getBySelector("appSidebarSearch").find("input").type("all_field_types");
-    cy.get(".MuiTreeItem-root").first().click();
-    cy.location("pathname").should("eq", "/code/file/views/11-98e7d0-148d5r");
+  it("Displays documents/files correctly", () => {
+    cy.get(
+      '[data-cy="html"] [id="html\\-\\/code\\/file\\/views\\/\\_\\_e2e\\_\\_"]'
+    ).as("e2eFolder");
+
+    cy.get("@e2eFolder").within(() => {
+      cy.get(".MuiTreeItem-content").click({ force: true });
+    });
+
+    cy.get("@e2eFolder")
+      .contains(Cypress.env("COMMIT_ID"))
+      .closest('[role="treeitem"]')
+      .as("commitFolder");
+
+    cy.get("@commitFolder").within(() => {
+      cy.get(".MuiTreeItem-content").click({ force: true });
+    });
+
+    expect(FILENAME).to.not.be.empty;
+
+    cy.get("@commitFolder")
+      .find(`[id="html\\-\\/code\\/file\\/views\\/${Cypress.env("fileZUID")}"]`)
+      .scrollIntoView()
+      .should("be.visible")
+      .click();
+  });
+
+  it("Show icon for unpublished files/documents", () => {
+    cy.get('[data-cy="CodeEditorContainer"]')
+      .find("textarea")
+      .eq(0)
+      .type("xxxx");
+
+    cy.getBySelector("SaveCodeButton")
+      .should("be.visible")
+      .should("be.enabled")
+      .click();
+
+    cy.get(`[id="html\\-\\/code\\/file\\/views\\/${Cypress.env("fileZUID")}"]`)
+      .scrollIntoView()
+      .should("be.visible")
+      .find('[data-cy="PublishIconButton"]')
+      .should("be.visible");
+  });
+
+  it("Publish file/document when clicking the publish icon", () => {
+    cy.get('[data-cy="CodeEditorContainer"]')
+      .find("textarea")
+      .eq(0)
+      .type("xxxx");
+
+    cy.getBySelector("SaveCodeButton")
+      .should("be.visible")
+      .should("be.enabled")
+      .click();
+
+    cy.get(`[id="html\\-\\/code\\/file\\/views\\/${Cypress.env("fileZUID")}"]`)
+      .scrollIntoView()
+      .should("be.visible")
+      .find('[data-cy="PublishIconButton"]')
+      .should("be.visible")
+      .should("be.enabled")
+      .click();
+
+    cy.getBySelector("toast").contains(`${FILENAME}`, {
+      matchCase: false,
+    });
   });
 });
