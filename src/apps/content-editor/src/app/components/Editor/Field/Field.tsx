2cfea4156adb9d1722905a77e6fab0957fe64099
Andres <agalin920@gmail.com>
2026-02-25 10:41:24 -0800

    reset image to replace on modal close

--- src/apps/content-editor/src/app/components/Editor/Field/Field.tsx
+++ src/apps/content-editor/src/app/components/Editor/Field/Field.tsx
@@ -188,6 +188,11 @@ export const Field = memo(
       }
     );
 
+    const closeImageModal = () => {
+      imageModal?.onClose?.();
+      setImageModal(null);
+    };
+
     const renderMediaModal = () => {
       return ReactDOM.createPortal(
         <MemoryRouter
@@ -207,7 +212,7 @@ export const Field = memo(
                 overflow: "hidden",
               },
             }}
-            onClose={() => setImageModal(null)}
+            onClose={closeImageModal}
           >
             <IconButton
               sx={{
@@ -215,7 +220,7 @@ export const Field = memo(
                 right: 15,
                 top: 10,
               }}
-              onClick={() => setImageModal(null)}
+              onClick={closeImageModal}
             >
               <CloseIcon sx={{ color: "common.white" }} />
             </IconButton>
@@ -224,7 +229,7 @@ export const Field = memo(
               isSelectDialog={true}
               addImagesCallback={(images) => {
                 imageModal.callback(images);
-                setImageModal(null);
+                closeImageModal();
               }}
             />
           </Dialog>
@@ -499,7 +504,7 @@ export const Field = memo(
                       overflow: "hidden",
                     },
                   }}
-                  onClose={() => setImageModal(null)}
+                  onClose={closeImageModal}
                 >
                   <IconButton
                     data-cy="closeMediaDialogBtn"
@@ -508,7 +513,7 @@ export const Field = memo(
                       right: 5,
                       top: 0,
                     }}
-                    onClick={() => setImageModal(null)}
+                    onClick={closeImageModal}
                   >
                     <CloseIcon sx={{ color: "common.white" }} />
                   </IconButton>
@@ -527,7 +532,7 @@ export const Field = memo(
                     }
                     addImagesCallback={(images) => {
                       imageModal.callback(images);
-                      setImageModal(null);
+                      closeImageModal();
                     }}
                     isReplace={imageModal.isReplace}
                   />
