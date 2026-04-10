593f77fcb79de434b55c3f03c03a2df0da108113
Nar Cuenca <narc.ph@gmail.com>
2026-04-10 14:52:41 +0800

    Resolve failing tests due to changes to the getBySelector command

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   video: false,
+  numTestsKeptInMemory: 0,
   defaultCommandTimeout: 15000,
   env: {
     API_AUTH: "https://auth.api.dev.zesty.io",
