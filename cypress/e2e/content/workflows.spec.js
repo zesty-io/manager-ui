51611842df8c888f7d0238f691d909aedd15df7d
George Demonteverde <74890703+geodem127@users.noreply.github.com>
2026-05-19 23:52:41 +0800

    Cypress: Setup Fixture Seeding - Content Workflows (#3970)
    - [x] added new task and definition for content labels cleanup.
    - [x] implemented data seeding

--- cypress/e2e/content/workflows.spec.js
+++ cypress/e2e/content/workflows.spec.js
@@ -1,15 +1,10 @@
 import ContentItemPage from "./pages/ContentItemPage";
-import CONFIG from "../../../src/shell/app.config";
-import instanceZUID from "../../../src/utility/instanceZUID";
+
 const NOW = Date.now();
 
-const INSTANCE_API = `${
-  CONFIG?.[process.env.NODE_ENV]?.API_INSTANCE_PROTOCOL
-}${instanceZUID}${CONFIG?.[process.env.NODE_ENV]?.API_INSTANCE}`;
 const TITLES = {
-  contentItem: `Content item workflow test ${NOW}`,
-  publishLabel: "Publish Approval",
-  testLabel: "Random Test Label",
+  publishLabel: `Publish Approval - ${NOW}`,
+  testLabel: `Random Test Label - ${NOW}`,
 };
 const LABEL_DATA = {
   publishLabel: {
@@ -31,75 +26,30 @@ const LABEL_DATA = {
 };
 const cleanUp = () => {
   // Delete test content item
-  cy.location("pathname").then((loc) => {
-    const [_, __, modelZUID, itemZUID] = loc?.split("/");
-    cy.apiRequest({
-      method: "DELETE",
-      url: `${INSTANCE_API}/content/models/${modelZUID}/items/${itemZUID}`,
-      failOnStatusCode: false, // This is only the cleanup so it doesn't matter if the content item exists or not
-    });
-  });
-
-  // Delete test labels
-  cy.apiRequest({
-    url: `${INSTANCE_API}/env/labels?showDeleted=true`,
-    failOnStatusCode: false, // This is only the cleanup so it doesn't matter if the labels exist or not
-  }).then((response) => {
-    response?.data
-      ?.filter(
-        (label) =>
-          !label?.deletedAt &&
-          [TITLES.publishLabel, TITLES.testLabel].includes(label?.name)
-      )
-      .forEach((label) => {
-        cy.apiRequest({
-          url: `${INSTANCE_API}/env/labels/${label.ZUID}`,
-          method: "DELETE",
-        });
-      });
-  });
+  cy.task("cleanup:labels", [TITLES.publishLabel, TITLES.testLabel]);
 };
 
 describe("Content Item Workflows", () => {
+  let ITEM = null;
   before(() => {
     cleanUp();
-    cy.intercept("POST", "**/labels").as("createLabel");
-    cy.intercept("GET", "**/labels*").as("getLabels");
 
     // Create allow publish workflow label
     Object.values(LABEL_DATA).forEach((data) => {
-      cy.apiRequest({
-        method: "POST",
-        url: `${INSTANCE_API}/env/labels`,
-        body: data,
-      });
+      cy.task("api:createLabel", data);
     });
 
-    // Visit test page
-    cy.apiRequest({
-      method: "POST",
-      url: `${INSTANCE_API}/content/models/6-b6cde1aa9f-wftv50/items`,
-      body: {
-        data: {
-          title: TITLES.contentItem,
-          description: TITLES.contentItem,
-          tc_title: TITLES.contentItem,
-          tc_description: TITLES.contentItem,
-          tc_image: null,
-        },
-        web: {
-          canonicalTagMode: 1,
-          parentZUID: "0",
-          metaLinkText: TITLES.contentItem,
-          metaTitle: TITLES.contentItem,
-          pathPart: TITLES.contentItem?.replaceAll(" ", "-")?.toLowerCase(),
-          metaDescription: TITLES.contentItem,
-        },
-        meta: { langID: 1, contentModelZUID: "6-b6cde1aa9f-wftv50" },
-      },
-    }).then((response) => {
-      cy.visit(`/content/6-b6cde1aa9f-wftv50/${response.data?.ZUID}`);
-    });
+    cy.task("seed:content", "fixtures/item.json")
+      .then(({ model, items }) => {
+        Cypress.env("modelZUID", model?.ZUID);
+        Cypress.env("itemZUID", items[0]?.meta?.ZUID);
+        ITEM = items?.[0];
+      })
+      .then(() => {
+        cy.visit(
+          `/content/${Cypress.env("modelZUID")}/${Cypress.env("itemZUID")}`
+        );
+      });
   });
 
   after(() => {
@@ -173,7 +123,7 @@ describe("Content Item Workflows", () => {
     ContentItemPage.elements
       .toast()
       .contains(
-        `Cannot Publish: "${TITLES.contentItem}". Does not have a status that allows publishing`
+        `Cannot Publish: "${ITEM?.web?.metaTitle}". Does not have a status that allows publishing`
       );
   });
 
